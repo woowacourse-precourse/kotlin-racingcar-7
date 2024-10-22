58e201818415425dce1df4d7b76898ad63e85ce9
지원 <cheerfulboys@naver.com>
2024-10-22 22:44:03 +0900

    feat(splitName): 입력된 이름을 나누어 list에 담는 함수 추가
    입력 받은 문자열을 콤마를 기준으로 나누어 List에 담는다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,9 +3,12 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+fun splitName(names: String): List<String> = names.split(",").map { it }
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val inputName = Console.readLine()
+    val names = splitName(inputName)
     println("시도할 횟수는 몇 회인가요?")
     val inputRound = Console.readLine()
 
