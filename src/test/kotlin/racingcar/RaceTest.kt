7f911321bb0920147360ce4cec564fefae8c8f17
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-23 00:33:17 +0900

    test(RaceTest): 우승자를 반환하는 기능 테스트
    - 우승자가 혼자일 경우 테스트
    - 우승자가 다수일 경우 테스트

--- src/test/kotlin/racingcar/RaceTest.kt
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -14,4 +14,28 @@ class RaceTest {
         val expect = listOf(Car("red", 0), Car("blue", 1))
         assertEquals(expect, result)
     }
+
+    @Test
+    fun `우승자가 혼자일 경우 테스트`() {
+        val cars = listOf(Car("red"), Car("blue"))
+        val race = Race(cars, NumbersGenerator(listOf(3, 4, 5, 3, 7, 2)))
+        repeat(3){
+            race.play()
+        }
+        val result = race.getWinnersName()
+        val expect = listOf("red")
+        assertEquals(expect, result)
+    }
+
+    @Test
+    fun `우승자가 다수일 경우 테스트`() {
+        val cars = listOf(Car("red"), Car("blue"))
+        val race = Race(cars, NumbersGenerator(listOf(3, 4, 5, 3, 7, 6)))
+        repeat(3){
+            race.play()
+        }
+        val result = race.getWinnersName()
+        val expect = listOf("red","blue")
+        assertEquals(expect, result)
+    }
 }
