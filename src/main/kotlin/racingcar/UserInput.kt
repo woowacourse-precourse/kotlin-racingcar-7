2437e9faebeb47e9d02d652390cd748c2f996200
dioo1461 <dioo1461@gmail.com>
2024-10-22 21:57:47 +0900

    feat(UserInput.kt): 사용자의 입력을 처리하는 UserInput 클래스 구현
    - 사용자의 입력을 요청하고, 이에 대해 중복 검사를 한 뒤 결과를 반환함
    - 반환 형식 : Outpu{carNameList: List<String>, totalAttempts: Int}
    - 중복된 자동차 이름에 대해서, index를 suffix로 붙여 식별 가능하게 만듦
    - UserInput 클래스에 대한 단위 테스트 또한 구현함

---
+++ src/main/kotlin/racingcar/UserInput.kt
@@ -0,0 +1,59 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console.readLine
+
+const val CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+const val TOTAL_ATTEMPTS_REQUEST_MESSAGE = "시도할 횟수는 몇 회인가요?"
+
+data class Output(val carNameList: List<String>, val totalAttempts: Long)
+
+class UserInput {
+
+    fun request(): Output {
+        println(CAR_NAMES_REQUEST_MESSAGE)
+        var carNamesInput = readLine()
+        println(TOTAL_ATTEMPTS_REQUEST_MESSAGE)
+        var totalAttemptsInput = readLine()
+
+        verifyCarNamesInput(carNamesInput)
+        verifyTotalAttemptsInput(totalAttemptsInput)
+        val carNameList = parseCarNamesInput(carNamesInput)
+        val totalAttempts = parseTotalAttemptsInput(totalAttemptsInput)
+
+        return Output(carNameList, totalAttempts)
+    }
+
+    private fun parseCarNamesInput(carNamesInput: String): List<String> {
+        val rawCarNameList = carNamesInput.split(",")
+        return appendIndexToDuplicatedCarNames(rawCarNameList)
+    }
+
+    private fun appendIndexToDuplicatedCarNames(rawCarNameList: List<String>): List<String> {
+        val map = mutableMapOf<String, Int>()
+        val refinedCarNameList = mutableListOf<String>()
+
+        for (carName in rawCarNameList) {
+            if (!map.containsKey(carName)) {
+                map[carName] = 0
+                refinedCarNameList.add(carName)
+            } else {
+                refinedCarNameList.add(carName+map[carName])
+                map[carName] = map[carName]!! + 1
+            }
+        }
+
+        return refinedCarNameList.toList()
+    }
+
+    private fun parseTotalAttemptsInput(totalAttemptsInput: String): Long {
+        return totalAttemptsInput.toLong()
+    }
+
+    private fun verifyCarNamesInput(carNamesInput: String) {
+        // TODO: 자동차 이름 입력에 대한 검증 구현
+    }
+
+    private fun verifyTotalAttemptsInput(totalAttemptsInput: String) {
+        // TODO: 총 이동 횟수에 대한 검증 구현
+    }
+}
---
+++ src/test/kotlin/racingcar/unitTest/UserInputTest.kt
@@ -0,0 +1,48 @@
+package racingcar
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class UserInputTest : NsTest() {
+    private val userInput = UserInput()
+
+    @Test
+    fun `정상적인 입력`() {
+        var carInput = "pobi,woni,jun"
+        var totalAttemptsInput = "5"
+        assertSimpleTest {
+            run(carInput, totalAttemptsInput)
+            assertThat(output()).contains("[pobi, woni, jun]", "5")
+        }
+    }
+
+    @Test
+    fun `중복된 자동차 이름 입력`() {
+        var carInput = "pobi,pobi,pobi"
+        var totalAttemptsInput = "5"
+            assertSimpleTest {
+            run(carInput, totalAttemptsInput)
+            assertThat(output()).contains("[pobi, pobi0, pobi1]", "5")
+        }
+    }
+
+    @Test
+    fun `공백이 포함된 자동차 이름 입력`() {
+        var carInput = "ju  n , woni,po bi"
+        var totalAttemptsInput = "5"
+        assertSimpleTest {
+            run(carInput, totalAttemptsInput)
+            assertThat(output()).contains("[ju  n ,  woni, po bi]", "5")
+        }
+    }
+
+    
+    override fun runMain() {
+        val userInput = UserInput()
+        val output = userInput.request()
+        println(output.carNameList)
+        println(output.totalAttempts)
+    }
+
+}
