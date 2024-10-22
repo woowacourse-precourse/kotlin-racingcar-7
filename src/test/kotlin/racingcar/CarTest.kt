511a4511b11baa3fc1280e45ee3a88cde38547f5
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-22 20:53:26 +0900

    test(CarTest): 테스트 생성
     - trymove 이동에 성공할 경우 위치 테스트
     - trymove 이동에 실패할 경우 위치 테스트
    
    이동을 시도했을때 받는 인수에 따라서 위치가 올라갔는지 테스트

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,25 @@
+package racingcar
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CarTest {
+
+    lateinit var car: Car
+
+    @Test
+    fun `이동 성공 테스트`(){
+        car = Car("white")
+        car.tryMove(6)
+        val expect = 1
+        assertEquals(expect,car.position)
+    }
+
+    @Test
+    fun `이동 실패 테스트`(){
+        car = Car("white")
+        car.tryMove(4)
+        val expect = 0
+        assertEquals(expect,car.position)
+    }
+}
