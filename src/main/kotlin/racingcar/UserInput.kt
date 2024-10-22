57eddf51350463b669446b600c78c5b60932b390
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-23 00:45:09 +0900

    feat(UserInput): 사용자 입력을 받는 기능 구현
    - 자동차의 이름들을 받는 기능 구현
    - 레이스 횟수를 구하는 기능 구현

---
+++ src/main/kotlin/racingcar/UserInput.kt
@@ -0,0 +1,14 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+class UserInput {
+
+    fun getCarsName() = Console.readLine()
+
+    fun getRaceCount(): Int {
+        val count = Console.readLine().toIntOrNull()
+        requireNotNull(count) { println("숫자를 입력해주세요") }
+        return count
+    }
+}
