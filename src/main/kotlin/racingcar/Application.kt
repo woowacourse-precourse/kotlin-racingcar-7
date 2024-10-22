71c6aa342313b27cb5ab2b4fc99a30c12356f240
지원 <cheerfulboys@naver.com>
2024-10-22 22:23:33 +0900

    feat(main): 사용자 입력 및 출력 추가
    화면에 이름과 횟수를 입력 받는 부분 및 안내 문구 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val inputName = Console.readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    val inputRound = Console.readLine()
+
+    println("실행 결과")
+
+
+    println("최종 우승자 : ")
 }
