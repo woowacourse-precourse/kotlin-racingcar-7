16646d68535d8d5d2c9ed5accdacfecfbc9cd16c
MEDA-DESKTOP\Meda <med4ndro@gmail.com>
2024-10-23 14:37:26 +0900

    feat: 문자열 입력을 받아 파싱하는 메서드 구현
    - 사용자로부터 입력받은 자동차 이름들과 시도 횟수를 각각 파싱하여 반환

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -10,17 +10,29 @@ class RacingGame {
         // TODO: 게임 진행 로직 구현
     }
 
+    /**
+     * 자동차 이름들을 사용자로부터 입력받기
+     * @return 자동차 이름들과 0으로 초기화된 LinkedHashMap 값
+     */
     fun inputCarNames(): LinkedHashMap<String, Int> {
-        TODO("입력 스트링 처리 코드 구현")
+        println(Messages.GAME_START)
+        val inputString = readLine()
+        return splitCarNames(inputString)
     }
 
-    fun splitCarNames(input: String): LinkedHashMap<String, Int> {
-        TODO("입력 스트링 분할 코드 구현")
+    private fun splitCarNames(input: String): LinkedHashMap<String, Int> {
+        val names = input.split(",")
+        val value = List(names.size) { 0 }
+        return names.zip(value).toMap(LinkedHashMap())
     }
 
+    /**
+     * 자동차 경주 횟수를 사용자로 부터 입력받기
+     * @return 자동차 경주 횟수를 나타내는 Int 값
+     */
     fun inputTryCount(): Int {
-        TODO("시도할 횟수 입력 코드 구현")
+        println(Messages.GAME_INPUT_COUNT)
+        val inputCountString = readLine()
+        return inputCountString!!.toInt()
     }
-
-
 }
