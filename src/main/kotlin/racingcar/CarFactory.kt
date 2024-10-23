4063c2ada8b75cd8d5e4c46adf8bcf4e01f5a195
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-23 22:48:58 +0900

    refactor(CarFactory): 하드코딩 값 상수로 추출
    어떤 의미를 가지고 있는지 가독성을 높이기 위해 추출

--- src/main/kotlin/racingcar/CarFactory.kt
+++ src/main/kotlin/racingcar/CarFactory.kt
@@ -3,12 +3,18 @@ package racingcar
 class CarFactory {
 
     fun buildCars(input: String): List<Car> {
-        val carsName = input.split(",")
+        val carsName = input.split(DELIMITER)
         validateCarsName(carsName)
         return carsName.map { name -> Car(name) }
     }
 
     private fun validateCarsName(carsName: List<String>) = carsName.forEach { name ->
-        require(name.length <= 5) { println("자동차 이름을 5글자 이하로 작성해주세요!") }
+        require(name.length <= NAME_COUNT_LIMIT) { println(OVER_NAME_COUNT_MESSAGE) }
+    }
+
+    companion object {
+        private const val DELIMITER = ","
+        private const val NAME_COUNT_LIMIT = 5
+        private const val OVER_NAME_COUNT_MESSAGE = "자동차 이름을 5글자 이하로 작성해주세요!"
     }
 }
