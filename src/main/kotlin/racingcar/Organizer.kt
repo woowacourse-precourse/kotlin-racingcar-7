07fbf56fa5b8884ceff829184eb5356ef6e3ef6b
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-23 14:26:45 +0900

    feat: 자동차 전진 조건 판별 및 각 자동차의 전진 횟수 저장
    - Car.kt: 기존 class 를 data class 로 변경하여 자동차 이름과 전진 횟수를 저장할 수 있도록 합니다.
    - Rules.kt: 랜덤값에 대한 전진조건을 판별합니다.
    - Organizer: 경주 횟수만큼 자동차들의 랜덤값을 뽑고, 전진 가능할 시 forward 값을 +1 합니다.

--- src/main/kotlin/racingcar/Organizer.kt
+++ src/main/kotlin/racingcar/Organizer.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
 class Organizer {
     private val input = Input()
     private val rules = Rules()
@@ -9,11 +11,21 @@ class Organizer {
         val appliedCars: List<String> = input.inputCars()
         val isDuplicatedCar = appliedCars.distinct().count() != appliedCars.count()
         if (isDuplicatedCar) throw IllegalArgumentException("경주에 등록할 자동차 이름은 중복될 수 없습니다.")
-        val raceCars: List<Car> = appliedCars.map { Car.validateCarName(it) }
+        val raceCars: MutableList<Car> = appliedCars.map { Car.validateCarName(it) }.toMutableList()
         println("시도할 횟수는 몇 회인가요?")
         val raceCount = input.inputRaceCount()
         if (!rules.isValidRaceCount(raceCount)) {
-            throw IllegalArgumentException("경주 횟수는 최소 1회 이상이며, 정수형만 입력 가능합니다.")
+            throw IllegalArgumentException("경주 횟수는 최소 1회 이상이며, 정수만 입력 가능합니다.")
+        }
+
+        for (i in 0..<raceCount.toInt()) {
+            for (j in 0..<raceCars.size) {
+                val randomValue = pickNumberInRange(0, 9)
+                if (rules.isPossibleForward(randomValue)) {
+                    val car = raceCars[j]
+                    raceCars[j] = car.copy(forward = car.forward + 1)
+                }
+            }
         }
     }
 }
--- src/main/kotlin/racingcar/Rules.kt
+++ src/main/kotlin/racingcar/Rules.kt
@@ -5,4 +5,8 @@ class Rules {
     fun isValidRaceCount(raceCount: String): Boolean {
         return raceCount.toIntOrNull() != null && raceCount.toInt() > 0
     }
+
+    fun isPossibleForward(randomValue: Int): Boolean {
+        return randomValue >= 4
+    }
 }
