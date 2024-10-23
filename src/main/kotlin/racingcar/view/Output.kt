c4dfbdfd35ad79fcccf7f5e0ffc9bdd3655e4df0
박지민 <pjm5489@naver.com>
2024-10-23 21:21:46 +0900

    feat: view 구현

---
+++ src/main/kotlin/racingcar/view/Output.kt
@@ -0,0 +1,22 @@
+package racingcar.view
+
+import racingcar.constants.Constants.COLON
+import racingcar.constants.Constants.RESULT_MSG
+import racingcar.constants.Constants.WINNERS_MSG
+
+class Output {
+    fun resultMsg(carNameList: List<String>, moveCountList: List<List<String>>) {
+        println(RESULT_MSG)
+        for (j in moveCountList.indices) {
+            for (i in carNameList.indices) {
+                println(carNameList[i] + COLON + moveCountList[i].take(j + 1).joinToString(""))
+            }
+            println()
+        }
+    }
+
+    fun winnerMsg(winnerList: List<String>) {
+        val winners = winnerList.joinToString(", ")
+        println(WINNERS_MSG + winners)
+    }
+}
