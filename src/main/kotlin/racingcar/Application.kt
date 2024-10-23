b54ce2319863579de108cdd9a04aac942d880ff1
지원 <cheerfulboys@naver.com>
2024-10-23 14:58:21 +0900

    feat(.): 우승자를 판별하고 출력하는 기능 추가
    가장 높은 위치를 가진 우승자(들)를 판별하여 최종 우승자를 출력한다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,6 +3,11 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+data class Car(
+    var location: Int,
+    var number: Int,
+)
+
 fun isValidName(name: String) : Boolean {
     if (name.length > 5) {
         return false
@@ -19,15 +24,69 @@ fun isValidRound(round: Int): Boolean {
 
 fun splitName(names: String): List<String> = names.split(",").map { it }
 
+fun createCar(names: List<String>) : Map<String, Car> {
+    val cars = mutableMapOf<String, Car>()
+    for (name in names) {
+        cars[name] = Car(0, 0)
+    }
+    return cars
+}
+
+fun getRandomNumber() : Int {
+    return Randoms.pickNumberInRange(0,9)
+}
+
+fun moveCar(car: Map.Entry<String, Car>) {
+    car.value.location += 1
+}
+
+fun stopCar(){ }
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val inputName = Console.readLine()
     val names = splitName(inputName)
     println("시도할 횟수는 몇 회인가요?")
-    val inputRound = Console.readLine()
+    val inputRound = Console.readLine().toInt()
+    val cars = createCar(names)
 
     println("실행 결과")
 
+    var randomNumber = 0
+    repeat(inputRound) {
+        for (car in cars) {
+            randomNumber = getRandomNumber()
+            if (randomNumber >= 4) {
+                moveCar(car)
+            } else {
+                stopCar()
+            }
+            val currentlocation: Int = car.value.location
+            val line = "-"
+            println("${car.key} : ${line.repeat(currentlocation)}")
 
-    println("최종 우승자 : ")
+        }
+        println()
+    }
+
+    var top = 0
+    var winner = ""
+
+    for (car in cars) {
+        if (car.value.location > 0) {
+            top = car.value.location
+        }
+    }
+
+    for (car in cars) {
+        if (car.value.location == top) {
+            if (winner == "") {
+                winner = car.key
+            } else {
+                winner += ", ${car.key}"
+            }
+        }
+    }
+
+    println("최종 우승자 : ${winner}")
 }
