eb899429e8f1924b17c39bf20758028c823a5fa8
Tnalxmsk <alstnwkd990@naver.com>
2024-10-24 02:21:04 +0900

    test: 자동차 기능 테스트
    자동차 특정 거리 이동 시 거리가 올바르게 저장되는지 확인하는 테스트 로직 작성

---
+++ src/test/kotlin/racingcar/model/CarTest.kt
@@ -0,0 +1,18 @@
+package racingcar.model
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class CarTest {
+
+    @Test
+    fun `전진 기능 테스트`() {
+        val car = Car("test")
+        car.moveForward()
+        car.moveForward()
+        car.stop()
+        car.moveForward()
+
+        assertThat(car.getDistance()).isEqualTo("---")
+    }
+}
