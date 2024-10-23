6a2c29bd20be3a7be1673807ba13787ddff12576
yeobeomhwi <yeobh0828@naver.com>
2024-10-23 16:43:25 +0900

    feat(car): add car to data class

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,12 @@
 package racingcar
 
+// 자동차를 나타내는 클래스, 이름과 이동 거리, 순위를 속성으로 가짐
+data class Car(
+    var name: String,
+    var move: Int = 0,
+    var rank: Int = 0,
+)
+
 fun main() {
     // TODO: 프로그램 구현
 }
