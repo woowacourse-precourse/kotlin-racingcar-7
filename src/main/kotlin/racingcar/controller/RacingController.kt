a72d6ca0f67754993aba79eea1afb6ef9ae42ad0
choiminjun <mj010504@naver.com>
2024-10-23 22:59:29 +0900

    feat: 차수별 자동차 레이싱 구현

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -1,7 +1,9 @@
 package racingcar.controller
 
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 import racingcar.model.Car
 import racingcar.view.InputView
+import java.nio.file.Files.move
 
 class RacingController {
     private val inputView = InputView()
@@ -10,11 +12,21 @@ class RacingController {
         val cars = createCars()
         val moveCount = inputView.readMoveCount()
 
+        repeat(moveCount) {
+            racingCars(cars)
+        }
+
     }
 
     private fun createCars() : List<Car> {
         val carNames = inputView.readCarNames()
         return carNames.map {name -> Car(name)}
+    }
+
+    private fun racingCars(cars : List<Car>) {
+        cars.forEach { car ->
+            if(pickNumberInRange(0, 9) >= 4) car.move()
+        }
     }
 
 }
