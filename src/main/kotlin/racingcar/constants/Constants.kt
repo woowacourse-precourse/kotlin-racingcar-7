e589f810030ba11b8e8e2a11d2e84d8e2555e710
박지민 <pjm5489@naver.com>
2024-10-23 21:21:17 +0900

    feat: 상수 정의

---
+++ src/main/kotlin/racingcar/constants/Constants.kt
@@ -0,0 +1,12 @@
+package racingcar.constants
+
+object Constants {
+    const val MOVE = "-"
+    const val STOP = ""
+    const val INPUT_CARNAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val INPUT_COUNT_MSG = "시도할 횟수는 몇 회인가요?"
+    const val RESULT_MSG = "실행 결과"
+    const val COLON = " : "
+    const val WINNERS_MSG = "최종 우승자 : "
+
+}
