c7bc4453ebb10be0f67f2781d689569f17d0dc0d
jhseo12 <jhseo1778@gmail.com>
2024-10-25 02:34:19 +0900

    refactor: 쉼표와 숫자 사이에 여백 추가

--- src/main/kotlin/racingcar/model/Move.kt
+++ src/main/kotlin/racingcar/model/Move.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 class Move {
     fun move(infoOfCar: MutableMap<String, Int>): MutableMap<String, Int> {
         for ((key, value) in infoOfCar) {
-            val randomNumber = Randoms.pickNumberInRange(0,9)
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
             if (randomNumber >= 4) {
                 infoOfCar[key] = value + 1
             }
