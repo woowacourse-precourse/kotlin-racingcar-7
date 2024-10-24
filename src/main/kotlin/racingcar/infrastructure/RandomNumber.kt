25c269fd36b5042acb4e4a2cb37e9b2e2213fc83
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 02:09:14 +0900

    feat(randomNumber): 랜덤 숫자 반환
    0에서 9사이의 랜덤 숫자 고르는 함수 구현

---
+++ src/main/kotlin/racingcar/infrastructure/RandomNumber.kt
@@ -0,0 +1,11 @@
+package racingcar.infrastructure
+
+import camp.nextstep.edu.missionutils.Randoms
+
+object RandomNumber {
+    fun pick(){
+        Randoms.pickNumberInRange(START_NUMBER, END_NUMBER)
+    }
+    private const val START_NUMBER: Int = 0
+    private const val END_NUMBER: Int = 9
+}
