fd5bedbbe8a19c49b787a7a4e5a9d847d809f2e1
bbangiee <yoojin1117@gmail.com>
2024-10-25 00:39:25 +0900

    fix(입력받기): 입력받은 시도 횟수가 숫자가 아닐 때 예외 발생하는 것으로 변경

--- src/main/kotlin/racingcar/IOHandler.kt
+++ src/main/kotlin/racingcar/IOHandler.kt
@@ -1,6 +1,8 @@
 package racingcar
 
 class IOHandler {
+    val validation = Validation()
+
     fun startMatch(): Registaration {
         return Registaration(cars = registerCars(), decideNumberOfRound())
     }
@@ -8,20 +10,20 @@ class IOHandler {
     private fun registerCars(): String {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 
-        val registeredCars = readLine()
-        if (registeredCars.isNullOrEmpty()) {
-            throw IllegalArgumentException()
-        }
+        val registeredCars = readLine() ?: throw IllegalArgumentException()
+        validation.isNotEmpty(registeredCars)
         return registeredCars
     }
 
     private fun decideNumberOfRound(): Int {
         println("시도할 횟수는 몇 회인가요?")
 
-        val numberOfRound = readLine()
-        if (numberOfRound.isNullOrEmpty()) {
-            throw IllegalArgumentException()
-        }
+        val numberOfRound = readLine() ?: throw IllegalArgumentException()
+        validation.isNotEmpty(numberOfRound)
+        validation.isDigit(numberOfRound)
+//        if (numberOfRound.isNullOrEmpty()) {
+//            throw IllegalArgumentException()
+//        }
         return numberOfRound.toInt()
     }
 }
--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -1,6 +1,20 @@
 package racingcar
 
 class Validation() {
+    fun isNotEmpty(string: String) {
+        if (string.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun isDigit(string: String) {
+        string.map {
+            if (!it.isDigit()) {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
     // 자동차 이름은 문자, 숫자 모두 허용
     // split에 사용할 구분자는 ","만 허용
     fun isDelimitersCorrect(registaration: Registaration): Boolean {
