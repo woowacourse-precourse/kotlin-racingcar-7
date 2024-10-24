67cb91a23479132c02898e77b2c32945611ffe02
hwsg0912@naver.com <hwsg0912@naver.com>
2024-10-24 18:28:51 +0900

    feat: InputView 구현 및 입력 요청 메서드 이동
    - 사용자 입력 메서드 구현
    - OutputView에서 입력 요청 출력 메서드(printInputRacingCarNameRequst(), printInputAttemptCountRequest())  InputView로 이동
    - README.md 수정

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,14 +3,6 @@ package racingcar.view
 import racingcar.constant.PrintMessage
 
 class OutputView {
-    fun printInputRacingCarNameRequest() {
-        println(PrintMessage.INPUT_RACING_CAR_NAME_REQUEST)
-    }
-
-    fun printInputAttemptCountRequest() {
-        println(PrintMessage.INPUT_ATTEMPT_COUNT_REQUEST)
-    }
-
     fun printRaceResult() {
         println(PrintMessage.RACE_RESULT)
     }
