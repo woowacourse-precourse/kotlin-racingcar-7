8133f86f1de5904a060e2d83404c3d7b365e2085
JCTA0125 <jcta0125@duksung.ac.kr>
2024-10-24 18:53:45 +0900

    feat: 입력 (이름 입력)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+fun inputCar(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val nameList = Console.readLine()
+    val splitNameList = nameList.split(",")
+
+    return requireNotNull(splitNameList)
+}
+
 fun main() {
     // TODO: 프로그램 구현
+    val nameList = inputCar()
+
 }
