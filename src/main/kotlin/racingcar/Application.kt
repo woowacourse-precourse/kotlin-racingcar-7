82438e1c83b84ddf396e4e4229ed6419f5445dd4
baekcii <bsy8470@gmail.com>
2024-10-25 00:51:32 +0900

    Fix: 시도 횟수를 실수형으로 입력할 시 발생하는 오류 수정

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,4 +1,5 @@
 package racingcar
+
 import camp.nextstep.edu.missionutils.*
 
 fun main() {
@@ -13,53 +14,51 @@ fun main() {
     check.secondInput(tryCount)
 
     val raceGame = RaceGame()
-    raceGame.play(cars, tryCount.toInt())
-
+    raceGame.play(cars, tryCount.toFloat().toInt())
 }
 
-class RaceGame{
+class RaceGame {
 
-    fun play(cars : String, tryCount : Int){
+    fun play(cars: String, tryCount: Int) {
         var carMap = splitCars(cars)
 
         println("실행 결과")
-        for(i in 0 until tryCount){
+        for (i in 0 until tryCount) {
             carMap = goForward(carMap)
             result(carMap)
         }
         winner(carMap)
     }
-    private fun splitCars(cars : String) : MutableMap<String, Int>{
-        val carMap : MutableMap<String,Int> = mutableMapOf()
-        val carList = cars.split(",").map{it.trim()}
-        carList.forEach{
-            carMap[it]=0
+    private fun splitCars(cars: String): MutableMap<String, Int> {
+        val carMap: MutableMap<String, Int> = mutableMapOf()
+        val carList = cars.split(",").map { it.trim() }
+        carList.forEach {
+            carMap[it] = 0
         }
         return carMap
     }
-    private fun goForward(carMap : MutableMap<String, Int>) : MutableMap<String,Int>{
-        carMap.forEach{(name, score)->
-            if(Randoms.pickNumberInRange(0,9)>=4){
-                carMap[name]=score+1
+    private fun goForward(carMap: MutableMap<String, Int>): MutableMap<String, Int> {
+        carMap.forEach { (name, score) ->
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                carMap[name] = score + 1
             }
         }
         return carMap
     }
-
-    private fun result(gameScore : MutableMap<String, Int>){
-        gameScore.forEach{
+    private fun result(gameScore: MutableMap<String, Int>) {
+        gameScore.forEach {
             println("${it.key} : ${"-".repeat(it.value)}")
         }
         println()
     }
-    private fun winner(gameScore: MutableMap<String, Int>){
+    private fun winner(gameScore: MutableMap<String, Int>) {
         val check = CheckException()
         check.result(gameScore)
 
         val maxScore = gameScore.values.max()
-        val winnerList : MutableList<String> = mutableListOf()
-        gameScore.forEach{(name,score)->
-            if(score == maxScore){
+        val winnerList: MutableList<String> = mutableListOf()
+        gameScore.forEach { (name, score) ->
+            if (score == maxScore) {
                 winnerList.add(name)
             }
         }
@@ -67,34 +66,32 @@ class RaceGame{
     }
 }
 
-class CheckException{
+class CheckException {
 
-    fun firstInput(car : String){
-        if(car=="" || !car.contains(",")) {
+    fun firstInput(car: String) {
+        if (car == "" || !car.contains(",")) {
             throw IllegalArgumentException()
         }
-        val carList = car.split(",").map{it.trim()}
-        if(carList.size != carList.distinct().size){
+        val carList = car.split(",").map { it.trim() }
+        if (carList.size != carList.distinct().size) {
             throw IllegalArgumentException()
         }
-        carList.forEach{
-            if(it.isBlank() || it.length>5){
+        carList.forEach {
+            if (it.isBlank() || it.length > 5) {
                 throw IllegalArgumentException()
             }
         }
     }
-
-    fun secondInput(count : String){
-        val num = count.toIntOrNull()
-        if(num==null || num <= 0 ) throw IllegalArgumentException()
+    fun secondInput(count: String) {
+        val num = count.toFloatOrNull()
+        if (num == null || num <= 0 || num % 1 != 0f) throw IllegalArgumentException()
     }
-
-    fun result(scores : MutableMap<String,Int>){
+    fun result(scores: MutableMap<String, Int>) {
         var check = 0
-        for (score in scores){
-            if(score.value!=0)
-                check+=1
+        for (score in scores) {
+            if (score.value != 0)
+                check += 1
         }
-        if(check==0) throw IllegalArgumentException()
+        if (check == 0) throw IllegalArgumentException()
     }
 }
