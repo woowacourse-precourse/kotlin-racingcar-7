f9e7fd995450eb0ec2be1ab3b04577cb8c94a416
HamBeomJoon <hbj0802@naver.com>
2024-10-25 01:22:16 +0900

    feat(RacingController): 차수별 실행 결과 출력
    - 차수별로 각 자동차의 이름과 전진 상황을 "-"로 출력한다.
    - 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -1,6 +1,7 @@
 package racingcar.controller
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 class RacingController {
     private var carState = mutableMapOf<String, Int>()
@@ -15,9 +16,31 @@ class RacingController {
         printInputRaceCount()
         val raceCount = Console.readLine().toInt()
 
+        printOutputRaceResult()
+        repeat(raceCount) {
+            race(carState)
+            println()
+        }
+
     }
 
     private fun printInputCarName() = println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     private fun printInputRaceCount() = println("시도할 횟수는 몇 회인가요?")
 
+    private fun printOutputRaceResult() = println("\n실행 결과")
+
+    private fun race(carState: MutableMap<String, Int>) {
+        for (car in carState.keys) {
+            if (checkForward()) {
+                // !!써서 carState[car]!! + 1  or  carState[car]!!.plus(1) 쓸지 얘로 쓸지 고민..
+                carState[car] = carState.getValue(car) + 1
+            }
+
+            println("$car : " + "-".repeat(carState.getValue(car)))
+        }
+    }
+
+    private fun checkForward(): Boolean {
+        return Randoms.pickNumberInRange(0, 9) >= 4
+    }
 }
