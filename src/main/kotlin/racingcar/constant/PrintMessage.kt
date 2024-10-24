e1460412a4ab45def8d66770eddfed1c89260867
hwsg0912@naver.com <hwsg0912@naver.com>
2024-10-24 17:38:06 +0900

    feat: PrintMessage 구현

---
+++ src/main/kotlin/racingcar/constant/PrintMessage.kt
@@ -0,0 +1,9 @@
+package racingcar.constant
+
+object PrintMessage {
+    val INPUT_RACING_CAR_NAME_REQUEST = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    val INPUT_ATTEMPT_COUNT_REQUEST = "시도할 횟수는 몇 회인가요?"
+    val RACING_RESULT = "실행 결과"
+    val FINAL_WINNERS = "최종 우승자 : "
+    val MOVE_SYMBOL = "-"
+}
