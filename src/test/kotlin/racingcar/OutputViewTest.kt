5633af8d43664ac30250e6ce2604170ddc17c185
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-24 17:25:39 +0900

    test: 에러 메시지 출력 / 에러 발생 테스트를 구분
    - OutputViewTest.kt:
    에러 발생 시 출력메시지와 매개변수 메시지가 같은지 확인하는 테스트를 추가합니다.
    에러를 띄울 메시지를 매개변수를 통해 넘겨줄 경우 IllegalArgumentException 을 발생한다

--- src/test/kotlin/racingcar/OutputViewTest.kt
+++ src/test/kotlin/racingcar/OutputViewTest.kt
@@ -10,19 +10,38 @@ import org.junit.jupiter.params.provider.ValueSource
 class OutputViewTest {
     private val outputView = OutputView()
 
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            "경주 횟수는 최소 1회 이상이며, 정수만 입력 가능합니다.",
-            "경주에 등록할 자동차 이름은 중복될 수 없습니다.",
-            "경주에 등록할 자동차 이름은 공백을 포함할 수 없으며, 5자 이하여야 합니다.",
-        ]
-    )
-    fun `에러를 띄울 메시지를 매개변수를 통해 넘겨줄 경우 그 메시지를 포함해 IllegalArgumentException 을 발생시킨다`(message: String) {
-        val exception = assertThrows<IllegalArgumentException> {
-            throw IllegalArgumentException(message)
+    class OutputViewTest {
+
+        private val outputView = OutputView()
+
+        @ParameterizedTest
+        @ValueSource(
+            strings = [
+                "경주 횟수는 최소 1회 이상이며, 정수만 입력 가능합니다.",
+                "경주에 등록할 자동차 이름은 중복될 수 없습니다.",
+                "경주에 등록할 자동차 이름은 공백을 포함할 수 없으며, 5자 이하여야 합니다.",
+            ]
+        )
+        fun `에러를 띄울 메시지를 매개변수를 통해 넘겨줄 경우 IllegalArgumentException 발생 시 에러메시지가 같은지 확인한다`(message: String) {
+            val exception = assertThrows<IllegalArgumentException> {
+                outputView.printErrorMessage(message)
+            }
+
+            assertEquals(message, exception.message)
         }
 
-        assertEquals(message, exception.message)
+        @ParameterizedTest
+        @ValueSource(
+            strings = [
+                "경주 횟수는 최소 1회 이상이며, 정수만 입력 가능합니다.",
+                "경주에 등록할 자동차 이름은 중복될 수 없습니다.",
+                "경주에 등록할 자동차 이름은 공백을 포함할 수 없으며, 5자 이하여야 합니다.",
+            ]
+        )
+        fun `에러를 띄울 메시지를 매개변수를 통해 넘겨줄 경우 IllegalArgumentException 을 발생한다`(message: String) {
+            assertThrows<IllegalArgumentException> {
+                outputView.printErrorMessage(message)
+            }
+        }
     }
 }
