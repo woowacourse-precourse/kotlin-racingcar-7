9b78697aef1c1c82e78235144978d291a53ebccd
UiHyeon <hyunkim6790@gmail.com>
2024-10-24 13:09:25 +0900

    feat: 기본 입력 틀 생성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNameList = Console.readLine().split(",")
+    println("시도할 횟수는 몇 회인가요?")
+    val moveCount = Console.readLine().toInt()
+
+    Console.close()
+
 }
