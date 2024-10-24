71db527d8f9f0dc61f65abc4db44866736715143
bbangiee <yoojin1117@gmail.com>
2024-10-24 18:19:45 +0900

    feat(입력받기): 경주할 자동차 이름과 시도할 횟수 입력받기
    - 경주할 자동차 이름 입력하는 함수 생성
    - 시도 횟수 입력하는 함수 생성

---
+++ src/main/kotlin/racingcar/IOHandler.kt
@@ -0,0 +1,18 @@
+package racingcar
+
+class IOHandler {
+    fun startMatch() {
+        registerCars()
+        decideNumberOfRound()
+    }
+
+    private fun registerCars(): String? {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        return readLine()
+    }
+
+    private fun decideNumberOfRound(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readLine()?.toInt() ?: throw IllegalArgumentException()
+    }
+}
