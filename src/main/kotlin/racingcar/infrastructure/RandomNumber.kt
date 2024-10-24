8a31475cfc05d5961ff002140f528107a5f852f2
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 02:13:51 +0900

    fix(randomNumber): pick함수 반환 타입 Int로 지정
    RandomNumber에 pick()함수 반환 타입을 Unit으로 잘못 지정
    반환 타입 Int로 변경함

--- src/main/kotlin/racingcar/infrastructure/RandomNumber.kt
+++ src/main/kotlin/racingcar/infrastructure/RandomNumber.kt
@@ -3,8 +3,8 @@ package racingcar.infrastructure
 import camp.nextstep.edu.missionutils.Randoms
 
 object RandomNumber {
-    fun pick(){
-        Randoms.pickNumberInRange(START_NUMBER, END_NUMBER)
+    fun pick(): Int {
+        return Randoms.pickNumberInRange(START_NUMBER, END_NUMBER)
     }
     private const val START_NUMBER: Int = 0
     private const val END_NUMBER: Int = 9
