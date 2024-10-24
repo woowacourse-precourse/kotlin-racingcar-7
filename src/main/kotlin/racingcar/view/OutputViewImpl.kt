63bc7e26d74415733f896ac6f64b3aef9fbd0b12
Tnalxmsk <alstnwkd990@naver.com>
2024-10-24 21:35:44 +0900

    refactor: 상수 관리 개선
    companion object 를 통해 상수 관리 코드 추가 구현

--- src/main/kotlin/racingcar/view/OutputViewImpl.kt
+++ src/main/kotlin/racingcar/view/OutputViewImpl.kt
@@ -6,13 +6,19 @@ class OutputViewImpl : OutputView {
 
     override fun printNewLine() = println()
 
-    override fun printRaceResult() = println("실행 결과")
+    override fun printRaceResult() = println(MESSAGE_RACE_RESULT)
 
     override fun printCarRoundInfo(car: Car) {
-        println("${car.getName()} : ${car.currentDistance}")
+        println(MESSAGE_ROUND_INFO.format(car.getName(), car.currentDistance))
     }
 
     override fun printWinners(names: String) {
-        println("최종 우승자 : $names")
+        println(MESSAGE_WINNERS.format(names))
+    }
+
+    companion object {
+        private const val MESSAGE_RACE_RESULT = "실행 결과"
+        private const val MESSAGE_ROUND_INFO = "%s : %s"
+        private const val MESSAGE_WINNERS = "최종 우승자 : %s"
     }
 }
