cd98dbae61a689bf73bf780a3477a44bd55ba8b2
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-24 14:22:25 +0900

    test: 우승자 판단에 대한 테스트 추가
    - JudgeTest.kt/WinnerFinderTest:
    가장 높은 전진 횟수의 우승자 이름을 반환하는 테스트를 추가합니다.
    가장 높은 전진 횟수가 여러 대일 경우 여러 우승자를 반환하는 테스트를 추가합니다.
    경기에 참여한 자동차가 1대일 경우 그 자동차의 이름을 반환하는 테스트를 추가합니다.

--- src/test/kotlin/racingcar/JudgeTest.kt
+++ src/test/kotlin/racingcar/JudgeTest.kt
@@ -1,7 +1,6 @@
 package racingcar
 
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -153,6 +152,47 @@ class JudgeTest {
             val result = judge.isPossibleForward(randomValue)
 
             assertFalse(result)
+        }
+    }
+
+    @Nested
+    @DisplayName("우승자 판단")
+    inner class WinnerFinderTest {
+
+        @Test
+        fun `경기에 참여한 자동차의 전진 횟수가 2로 모두 같을 경우 경기에 참여한 모든 자동차의 이름을 반환한다`() {
+            val raceCars = listOf(Car("pobi", 2), Car("woni", 2))
+
+            val result = judge.findWinnerName(raceCars)
+
+            assertEquals(listOf("pobi", "woni"), result)
+        }
+
+        @Test
+        fun `세 자동차에서 각 전진 횟수가 1,2,3으로 상이할 경우 세 자동차 중 전진 횟수가 3으로 가장 높은 자동차의 이름을 반환한다`() {
+            val raceCars = listOf(Car("pobi", 1), Car("woni", 2), Car("jason", 3))
+
+            val result = judge.findWinnerName(raceCars)
+
+            assertEquals(listOf("jason"), result)
+        }
+
+        @Test
+        fun `세 자동차에서 각 전진 횟수가 3,3,2일 경우 세 자동차 중 전진 횟수가 3으로 가장 높은 두 자동차의 이름을 반환한다`() {
+            val raceCars = listOf(Car("pobi", 3), Car("woni", 3), Car("jason", 2))
+
+            val result = judge.findWinnerName(raceCars)
+
+            assertEquals(listOf("pobi", "woni"), result)
+        }
+
+        @Test
+        fun `경기에 참여한 자동차가 1대일 경우 그 자동차의 이름을 반환한다`() {
+            val raceCars = listOf(Car("pobi", 7))
+
+            val result = judge.findWinnerName(raceCars)
+
+            assertEquals(listOf("pobi"), result)
         }
     }
 }
