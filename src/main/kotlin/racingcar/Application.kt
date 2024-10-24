a4f77cedc4f1f7f09cb9657e8bb53ef851701f1c
Tnalxmsk <alstnwkd990@naver.com>
2024-10-24 16:44:44 +0900

    feat: OutputView 객체 의존성 주입

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,10 +2,12 @@ package racingcar
 
 import racingcar.presenter.RacingCarPresenter
 import racingcar.view.InputViewImpl
+import racingcar.view.OutputViewImpl
 
 fun main() {
     val inputView = InputViewImpl()
+    val outputView = OutputViewImpl()
     val racingCarPresenter = RacingCarPresenter()
 
-    RacingCarGameApp(inputView, racingCarPresenter).run()
+    RacingCarGameApp(inputView, outputView, racingCarPresenter).run()
 }
