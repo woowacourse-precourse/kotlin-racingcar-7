3854b85e4803d322e17af854df469deeda92ffe0
jhseo12 <jhseo1778@gmail.com>
2024-10-25 01:53:36 +0900

    feat(View): 중복된 이름일 경우 예외 처리 추가

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -7,6 +7,7 @@ class InputView {
         val input = Console.readLine()
         require(input.isNotEmpty()) {ERROR_EMPTY_STRING}
         val nameList = input.split(",")
+        require(nameList.size == nameList.distinct().size) {ERROR_SAME_NAME}
         for (name in nameList) {
             require(name.isNotEmpty()) {ERROR_EMPTY_NAME}
             require(name.all{ it.isLetter() }) {ERROR_INVALID_NAME}
@@ -27,6 +28,7 @@ class InputView {
 
     companion object {
         private const val ERROR_EMPTY_STRING = "[경고] 이름을 입력해야 합니다."
+        private const val ERROR_SAME_NAME = "[경고] 중복된 이름이 있습니다."
         private const val ERROR_EMPTY_NAME = "[경고] 비어있는 이름이 있습니다."
         private const val ERROR_INVALID_NAME = "[경고] 알파벳으로 이뤄진 이름만 입력 가능합니다."
         private const val ERROR_NAME_LENGTH = "[경고] 이름은 5글자를 초과할 수 없습니다."
