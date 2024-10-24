528b116741eac05b4afdf25b9aff58b21d215440
ijh1298 <imjh0417@gmail.com>
2024-10-24 19:57:15 +0900

    refactor: =을 return으로 대체

--- src/main/kotlin/racingcar/Model.kt
+++ src/main/kotlin/racingcar/Model.kt
@@ -3,7 +3,9 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 class Model {
-    private fun getRandomNumber() = Randoms.pickNumberInRange(0, 9)
+    private fun getRandomNumber(): Int {
+        return Randoms.pickNumberInRange(0, 9)
+    }
 
     private fun move(car: Car) {
         if (getRandomNumber() >= 4)
@@ -12,5 +14,9 @@ class Model {
 
     fun moveAll(cars: List<Car>) {
         cars.forEach { move(it) }
+    }
+
+    private fun getStatus(cars: List<Car>): List<Int> {
+        return cars.map { it.getPos() }
     }
 }
