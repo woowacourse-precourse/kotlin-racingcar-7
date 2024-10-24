c14cf355cd7eb6258689b41fa3f8ba8c1a539bbf
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-24 22:56:28 +0900

    test(CarFactoryTest): 자동차 익명 생성 테스트 추가
    - 자동차 이름이 공백이거나 없을 경우 익명이 만들어지는지 테스트
    - 익명에 접미사가 순서대로 추가되는지 테스트

--- src/test/kotlin/racingcar/CarFactoryTest.kt
+++ src/test/kotlin/racingcar/CarFactoryTest.kt
@@ -16,6 +16,13 @@ class CarFactoryTest {
     }
 
     @Test
+    fun `자동차 익명 생성 테스트`() {
+        val cars = carFactory.buildCars("red,  ,   ,")
+        val expect = listOf(Car("red"), Car("익명1"), Car("익명2"), Car("익명3"))
+        assertEquals(expect, cars)
+    }
+
+    @Test
     fun `자동차를 생성할때 이름이 5자 이상일 경우 예외 처리 테스트`() {
         val input = "red,blue,white,yellow"
         assertThrows<IllegalArgumentException> {
