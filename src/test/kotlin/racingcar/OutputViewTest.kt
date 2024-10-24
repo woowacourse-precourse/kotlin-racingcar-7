3bc889fffe119d0c4d1c2f9bc6d3243f6408f0a5
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-24 15:57:10 +0900

    test: 출력값에 대한 테스트 추가
    - OutputViewTest.kt: 매개변수를 통해 넘겨 받은 메시지를 출력하거나, 에러 메시지에 포함시키는 테스트를 추가합니다.

---
+++ src/test/kotlin/racingcar/OutputViewTest.kt
@@ -0,0 +1,49 @@
+package racingcar
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+@DisplayName("출력값에 대한 테스트")
+class OutputViewTest {
+    private val outputView = OutputView()
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+            "시도할 횟수는 몇 회인가요?",
+            "최종 우승자 : jason",
+        ]
+    )
+    fun `공지할 메시지를 매개변수를 통해 넘겨줄 경우 그 메시지를 출력한다`(message: String) {
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        outputView.printNotice(message)
+
+        val printedOutput = outputStream.toString().trim()
+
+        assertEquals(message, printedOutput)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "경주 횟수는 최소 1회 이상이며, 정수만 입력 가능합니다.",
+            "경주에 등록할 자동차 이름은 중복될 수 없습니다.",
+            "경주에 등록할 자동차 이름은 공백을 포함할 수 없으며, 5자 이하여야 합니다.",
+        ]
+    )
+    fun `에러를 띄울 메시지를 매개변수를 통해 넘겨줄 경우 그 메시지를 포함해 IllegalArgumentException 을 발생시킨다`(message: String) {
+        val exception = assertThrows<IllegalArgumentException> {
+            throw IllegalArgumentException(message)
+        }
+
+        assertEquals(message, exception.message)
+    }
+}
