4d1dfa0c815f27926c0cce34852e40cf06cb2fb9
Tnalxmsk <alstnwkd990@naver.com>
2024-10-24 14:38:10 +0900

    feat: 자동차 경주 게임 시도 횟수 입력 단계 구현
    시도횟수 입력 및 객체 생성 로직 구현

--- src/main/kotlin/racingcar/RacingCarGameApp.kt
+++ src/main/kotlin/racingcar/RacingCarGameApp.kt
@@ -11,5 +11,8 @@ class RacingCarGameApp(
     fun run() {
         val carNames = inputView.readNames()
         val cars = racingCarPresenter.createRacingCars(carNames)
+
+        val number = inputView.readAttemptCount()
+        val attemptCount = racingCarPresenter.createAttemptCount(number)
     }
 }
