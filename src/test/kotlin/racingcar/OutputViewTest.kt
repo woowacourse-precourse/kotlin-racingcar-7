89ea4cbb4c66c781df27d2aeb54db83d939a3bbe
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-24 17:10:15 +0900

    test: Java 코드를 사용한 테스트 제거

--- src/test/kotlin/racingcar/OutputViewTest.kt
+++ src/test/kotlin/racingcar/OutputViewTest.kt
@@ -5,31 +5,10 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 
 @DisplayName("출력값에 대한 테스트")
 class OutputViewTest {
     private val outputView = OutputView()
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-            "시도할 횟수는 몇 회인가요?",
-            "최종 우승자 : jason",
-        ]
-    )
-    fun `공지할 메시지를 매개변수를 통해 넘겨줄 경우 그 메시지를 출력한다`(message: String) {
-        val outputStream = ByteArrayOutputStream()
-        System.setOut(PrintStream(outputStream))
-
-        outputView.printNotice(message)
-
-        val printedOutput = outputStream.toString().trim()
-
-        assertEquals(message, printedOutput)
-    }
 
     @ParameterizedTest
     @ValueSource(
