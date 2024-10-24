af0ed5c0fbf09106dfb3bb67f7a260c7ea6d0d95
박지민 <pjm5489@naver.com>
2024-10-24 16:51:45 +0900

    feat: 전체 동작기능 구현

---
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -0,0 +1,36 @@
+package racingcar.controller
+
+import camp.nextstep.edu.missionutils.Console
+import racingcar.model.Error
+import racingcar.model.Make
+import racingcar.view.ErrorMsg
+import racingcar.view.Input
+import racingcar.view.Output
+
+class RaceController {
+    private val input = Input()
+    private val output = Output()
+    private val errorMsg = ErrorMsg()
+
+    private fun result(carNames: String, count: String) {
+        if (!Error(carNames, count).totalError()) {
+            val make = Make(carNames, count.toInt())
+            val result = make.moveByNameList()
+            val carNameList = make.carNameList()
+
+            output.resultMsg(carNameList, result)
+            output.winnerMsg(make.winnerList())
+            return
+        }
+        errorMsg.errorMsg()
+    }
+
+    fun run() {
+        input.carNameMsg()
+        val carNames = Console.readLine()
+        input.countMsg()
+        val count = Console.readLine()
+
+        result(carNames, count)
+    }
+}
