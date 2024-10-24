bf650ccb77197e5864b1edb846cac7c03dd50bf6
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-10-24 23:58:18 +0900

    feat: 상수 파일 생성

---
+++ src/main/kotlin/racingcar/Constants.kt
@@ -0,0 +1,6 @@
+package racingcar
+
+object Constants {
+    const val MESSAGE_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val MESSAGE_INPUT_ATTEMPT_NUMBER = "시도할 횟수는 몇 회인가요?"
+}
