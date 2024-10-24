7237fa31c60132824d5eec967d7d5a44b9f6c5b1
이용인 <yongin0517@gmail.com>
2024-10-25 00:53:33 +0900

    feat: Race 객체 생성

---
+++ src/main/kotlin/racingcar/Race.kt
@@ -0,0 +1,27 @@
+package racingcar
+
+class Race(
+    private val numberGenerator: NumberGenerator,
+    private val outputView: OutputView
+) {
+    private var cars: List<Car> = listOf()
+    private var tryCount: Int = 0
+
+    fun initializeRace(cars: List<Car>, tryCount: Int) {
+        this.cars = cars
+        this.tryCount = tryCount
+    }
+
+    fun startRace() {
+        repeat(tryCount) {
+            cars.forEach { car -> car.move(numberGenerator.generate()) }
+            outputView.printRaceResult(cars)
+        }
+    }
+
+
+    fun getRaceWinners(): List<Car> {
+        val farthestDistance = cars.maxOf { it.position }
+        return cars.filter { it.position == farthestDistance }
+    }
+}
