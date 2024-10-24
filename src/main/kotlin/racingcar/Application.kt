918fee48d2589bbf649d615d640e1217feb3e858
baekcii <bsy8470@gmail.com>
2024-10-24 22:53:27 +0900

    Feat(main): 사용자 입력 받기 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,16 @@
 package racingcar
+import camp.nextstep.edu.missionutils.*
 
 fun main() {
+    val check = CheckException()
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val cars = Console.readLine()
+    check.firstInput(cars)
+
+    println("시도할 횟수는 몇 회인가요?")
+    val tryCount = Console.readLine()
+    check.secondInput(tryCount)
 
 }
 
