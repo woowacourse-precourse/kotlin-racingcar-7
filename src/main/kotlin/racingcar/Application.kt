5b504e4dffaf5cd2ebb827225be16c650bdfe162
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-26 04:17:11 +0900

    feat: 게임 전체 로직 작성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,27 @@
 package racingcar
 
+import racingcar.domain.Car
+import racingcar.infrastructure.ConsoleInput
+import racingcar.infrastructure.ConsoleOutput
+import racingcar.interfaces.InputAdapter
+import racingcar.useCases.RaceManger
+
 fun main() {
     // TODO: 프로그램 구현
+    val consoleInput = ConsoleInput()
+    val consoleOutput = ConsoleOutput()
+
+    val inputCarNames: String = consoleInput.getCarNames()
+    val inputMatches: String = consoleInput.getMatches()
+
+    val inputAdapter = InputAdapter(inputCarNames, inputMatches)
+
+    val car: List<String> = inputAdapter.splitCarName()
+    val racingCars = inputAdapter.validateCarName(car)
+    val readyRacingCars: List<Car> = inputAdapter.changeTypeOfCar(racingCars)
+    val matches: Int = inputAdapter.changeTypeOfMatches()
+
+    val raceManger = RaceManger(readyRacingCars, matches, consoleOutput)
+    raceManger.playRace()
+    raceManger.findWinners()
 }
