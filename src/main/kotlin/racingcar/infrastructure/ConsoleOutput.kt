47dce36d77e3c7b1f7bc78161b60fb7cfe38a42a
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 13:36:17 +0900

    feat(ConsoleOutput): 매라운드 결과와 우승자 출력 UI 구현
    매 라운드 결과와 게임의 결과를 출력할 UI 구현

---
+++ src/main/kotlin/racingcar/infrastructure/ConsoleOutput.kt
@@ -0,0 +1,22 @@
+package racingcar.infrastructure
+
+import racingcar.domain.Car
+
+class ConsoleOutput {
+    fun showRoundResult(cars: List<Car>) {
+        for (car in cars) {
+            val carName: String = car.name
+            val carDistance: String = ONE_STEP.repeat(car.distance)
+            println("$carName : $carDistance")
+        }
+    }
+
+    fun showWinners(winners: List<String>) {
+        println(winners.joinToString(DELIMITER))
+    }
+
+    companion object {
+        private const val ONE_STEP = "-"
+        private const val DELIMITER = ","
+    }
+}
