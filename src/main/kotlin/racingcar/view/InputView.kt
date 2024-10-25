e8247b0fbea16a1870d0778bb5b0041693011834
moondev03 <moondev03@gmail.com>
2024-10-26 00:15:14 +0900

    feat(InputView): 시도 횟수를 입력받고 유효성 검사하는 기능 구현
    - 입력이 빈 문자열인지 검사하는 기능
    - 입력이 숫자가 아니거나 음수인지 검사하는 기능
    - 위 2가지 함수에 대한 테스트 코드 작성
    - 구현 완료한 기능 README.md에 표시

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -17,21 +17,43 @@ class InputView {
         return carNames
     }
 
+    fun getRacingTryCount(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+
+        val input = Console.readLine().trim()
+        checkInputIsEmpty(input)
+        validateTryCount(input)
+
+        return input.toInt()
+    }
+
     private fun checkInputIsEmpty(input: String) {
         if (input.isEmpty()) throw IllegalArgumentException(INVALID_INPUT_EMPTY)
     }
 
     private fun validateCarNameLength(carNames: List<String>) {
-        carNames.forEach { carName -> if (carName.length > 5 || carName.isEmpty()) throw IllegalArgumentException(INVALID_INPUT_CAR_NAME_LENGTH) }
+        carNames.forEach { carName ->
+            if (carName.length > 5 || carName.isEmpty()) throw IllegalArgumentException(
+                INVALID_INPUT_CAR_NAME_LENGTH
+            )
+        }
     }
 
     private fun validateCarNameDuplicate(carNames: List<String>) {
-        if(carNames.size != carNames.distinct().size) throw IllegalArgumentException(INVALID_INPUT_CAR_NAME_DUPLICATE)
+        if (carNames.size != carNames.distinct().size) throw IllegalArgumentException(INVALID_INPUT_CAR_NAME_DUPLICATE)
+    }
+
+    private fun validateTryCount(tryCount: String) {
+        val count = tryCount.toIntOrNull()
+        if (count == null || count < 0) {
+            throw IllegalArgumentException(INVALID_INPUT_TRY_COUNT)
+        }
     }
 
     companion object {
         const val INVALID_INPUT_EMPTY = "입력이 필요합니다."
         const val INVALID_INPUT_CAR_NAME_LENGTH = "자동차 이름의 길이가 유효하지 않습니다."
         const val INVALID_INPUT_CAR_NAME_DUPLICATE = "중복된 자동차 이름이 존재합니다."
+        const val INVALID_INPUT_TRY_COUNT = "유효하지 않는 입력 형식입니다."
     }
 }
--- src/test/kotlin/racingcar/InputViewTest.kt
+++ src/test/kotlin/racingcar/InputViewTest.kt
@@ -7,6 +7,7 @@ import racingcar.view.InputView
 import racingcar.view.InputView.Companion.INVALID_INPUT_CAR_NAME_DUPLICATE
 import racingcar.view.InputView.Companion.INVALID_INPUT_CAR_NAME_LENGTH
 import racingcar.view.InputView.Companion.INVALID_INPUT_EMPTY
+import racingcar.view.InputView.Companion.INVALID_INPUT_TRY_COUNT
 import java.lang.reflect.InvocationTargetException
 
 class InputViewTest {
@@ -93,6 +94,38 @@ class InputViewTest {
             e.printStackTrace()
         } catch (e: InvocationTargetException) {
             assertThat(e.cause?.message).isEqualTo(INVALID_INPUT_CAR_NAME_DUPLICATE)
+        }
+    }
+
+    @DisplayName("시도 횟수 입력이 문자인 경우 예외 발생 테스트")
+    @Test
+    fun notNumberTryCountInputTest() {
+        val input = "칠"
+        val method = inputView.javaClass.getDeclaredMethod("validateTryCount", String::class.java)
+        method.setAccessible(true)
+
+        try {
+            method.invoke(inputView, input.trim())
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        } catch (e: InvocationTargetException) {
+            assertThat(e.cause?.message).isEqualTo(INVALID_INPUT_TRY_COUNT)
+        }
+    }
+
+    @DisplayName("시도 횟수 입력이 음수인 경우 예외 발생 테스트")
+    @Test
+    fun negativeTryCountInputTest() {
+        val input = "-1"
+        val method = inputView.javaClass.getDeclaredMethod("validateTryCount", String::class.java)
+        method.setAccessible(true)
+
+        try {
+            method.invoke(inputView, input.trim())
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        } catch (e: InvocationTargetException) {
+            assertThat(e.cause?.message).isEqualTo(INVALID_INPUT_TRY_COUNT)
         }
     }
 }
