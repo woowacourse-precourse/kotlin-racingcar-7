f58104f376d7337fcd9d386b74a9356a5d60416d
Min Seon <cbdhoho1@gmail.com>
2024-10-26 00:45:31 +0900

    feat: add exceptions

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,8 +4,17 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
     val cars = carNameInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    ifCarNameIsBlank(cars)
+
     val carList = splitCarName(cars)
+    ifCarNameIsSame(carList)
+    ifCarNameOverFive(carList)
+    ifCarIsSingle(carList)
+
     val tryCount = tryCountInput("시도할 횟수는 몇 회인가요?").toInt()
+    ifTryCountIsNegative(tryCount)
+    ifTryCountIsntInt(tryCount)
+
     var carRacingResult = initTryCountList(carList.size)
 
     println("실행 결과")
@@ -83,4 +92,42 @@ fun findFinalWinner(carList: List<String>, carRacingResult: MutableList<String>)
 
 fun printFinalWinner(promt: String, finalWinners: MutableList<String>) {
     println(promt+finalWinners.joinToString(", "))
+}
+
+fun ifCarNameIsBlank(carNames: String) {
+    if(carNames.isBlank()){
+        throw IllegalArgumentException("Cars must be filled.")
+    }
+}
+
+fun ifCarNameIsSame(carList: List<String>) {
+    if(carList.distinct().size != carList.size){
+        throw IllegalArgumentException("Cars names must be distinct.")
+    }
+}
+
+fun ifCarNameOverFive(carList: List<String>) {
+    for(i in carList.indices){
+        if(carList[i].length > 5){
+            throw IllegalArgumentException("Car name's length must not over 5.")
+        }
+    }
+}
+
+fun ifTryCountIsNegative(tryCount: Int) {
+    if(tryCount < 0){
+        throw IllegalArgumentException("Try Count must not be a negative number.")
+    }
+}
+
+fun ifTryCountIsntInt(tryCount: Any) {
+    if(tryCount !is Int){
+        throw IllegalArgumentException("Try Count must be Integer Type.")
+    }
+}
+
+fun ifCarIsSingle(carList: List<String>) {
+    if(carList.size == 1){
+        throw IllegalArgumentException("Car racing starts when racers are more than two.")
+    }
 }
