8185d19d05b2b4daf9fe1e4ee47832331960550a
choiminjun <mj010504@naver.com>
2024-10-25 17:40:13 +0900

    feat: 자동차 전진 결과 출력, 우승자 출력 테스트 추가

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -1,30 +1,16 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
-import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import racingcar.model.Car
 import racingcar.utils.Messages
-import racingcar.utils.Validator
 
 class ApplicationTest : NsTest() {
-    @Test
-    fun `우승자 출력하기`() {
-        assertRandomNumberInRangeTest(
-            {
-                run("pobi,woni", "1")
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
-            },
-            MOVING_FORWARD, STOP
-        )
-    }
-
     @ParameterizedTest
     @ValueSource(strings = ["carcar", "carcarcarcar"])
     fun `자동차 이름 길이 수는 5 이하여야 한다`(carName: String) {
@@ -59,7 +45,6 @@ class ApplicationTest : NsTest() {
     @ParameterizedTest
     @ValueSource(ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     fun `무작위 값이 4이상인 경우 자동차는 전진하고 4미만인 경우 정지한다`(randomInt: Int) {
-
         // given
         val car = Car("a")
 
@@ -71,13 +56,35 @@ class ApplicationTest : NsTest() {
         else assertThat(car.movedDistance).isEqualTo(0)
     }
 
+    @Test
+    fun `자동차가 전진할 때 차수별로 진행 상황을 출력한다`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("a,b,c", "2")
+                assertThat(output()).contains("a : -", "b : ", "c : -", "a : --", "b : -", "c : --")
+            },
+            MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    fun `가장 많이 전진한 자동차들은 우승하고 최종 우승자를 출력한다, 공동 우승자는 쉼표(,)로 구분한다`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("a,b,c", "1")
+                assertThat(output()).contains("최종 우승자 : a, c")
+            },
+            MOVING_FORWARD, STOP, MOVING_FORWARD
+        )
+
+    }
 
     override fun runMain() {
         main()
     }
 
     companion object {
-        private const val MOVING_FORWARD: Int = 4
-        private const val STOP: Int = 3
+        private const val MOVING_FORWARD = 4
+        private const val STOP = 3
     }
 }
