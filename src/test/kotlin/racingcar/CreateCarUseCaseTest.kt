3cd7af066bdab5bbf4a94ca594844b8f044eaf50
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-26 01:19:41 +0900

    test(CreateCarUseCase): CarFactoryTest -> CreateCarUseCaseTest로 변경
    - CarFactoryTest를 CreateCarUseCase 테스트로 변경
    
    생성하는 로직이 필요해서 Factory라고 만들었지만
    Factory의 역할은 여러 형태의 Car의 초기화 방식을 캡슐화하고 관리해서
    새로운 형태를 추가할때 Factory만 수정할 수 있기 위한 목적이라 수정

--- src/test/kotlin/racingcar/CreateCarUseCaseTest.kt
+++ src/test/kotlin/racingcar/CreateCarUseCaseTest.kt
@@ -5,21 +5,22 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import racingcar.domain.entity.Car
 import racingcar.domain.factory.CarFactory
+import racingcar.domain.usecase.CreateCarUseCase
 
-class CarFactoryTest {
+class CreateCarUseCaseTest {
 
-    private val carFactory = CarFactory()
+    private val createCarUseCase = CreateCarUseCase()
 
     @Test
     fun `자동차 생성 테스트`() {
-        val cars = carFactory.buildCars("red,blue")
+        val cars = createCarUseCase.execute("red,blue")
         val expect = listOf(Car("red"), Car("blue"))
         assertEquals(expect, cars)
     }
 
     @Test
     fun `자동차 익명 생성 테스트`() {
-        val cars = carFactory.buildCars("red,  ,   ,")
+        val cars = createCarUseCase.execute("red,  ,   ,")
         val expect = listOf(Car("red"), Car("익명1"), Car("익명2"), Car("익명3"))
         assertEquals(expect, cars)
     }
@@ -28,7 +29,7 @@ class CarFactoryTest {
     fun `자동차를 생성할때 이름이 5자 이상일 경우 예외 처리 테스트`() {
         val input = "red,blue,white,yellow"
         assertThrows<IllegalArgumentException> {
-            carFactory.buildCars(input)
+            createCarUseCase.execute(input)
         }
     }
 }
