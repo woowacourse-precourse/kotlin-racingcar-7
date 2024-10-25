76dc9ec51be666d0ae7267e11e07656847a9b021
eunmyeungjun <audwns419@gmail.com>
2024-10-25 22:46:13 +0900

    feat: 경주를 위한 자동차 이름과 시도할 횟수 입력받기 기능추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,4 +1,11 @@
 package racingcar
-fun main() {
 
+import camp.nextstep.edu.missionutils.Console
+fun main() {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+
+    val carNames = Console.readLine().split(",") ?: emptyList()
+
+    println("시도할 횟수는 몇 회인가요?")
+    val times = readln().toInt() ?: 0
 }
