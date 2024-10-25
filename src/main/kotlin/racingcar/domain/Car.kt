ba263e3efdee90c5c501b148b5612429bd97cfaa
wjdrjs00 <kimjk3409@gmail.com>
2024-10-26 03:33:28 +0900

    feat(car): 차 전진 여부 결정 기능 추가
    moveToForward():
    - 전진을 위한 메서드를 추가한다.
    - 전진, 스탑 값이 참일경우 거리를 증가시킨다.
    - 재사용성을 위해 증가값을 상수화한다.
    
    checkMoveOrStop():
    - 전진, 스탑 여부 체크를 위한 메서드를 추가한다.
    - 지정된 범위를 벗어난 경우 예외를 발생한다.
    - 재사용성을 위해 범위값을 상수화 한다.

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -11,11 +11,31 @@ class Car(
         validateCarNameLength(carName)
     }
 
+    fun moveToForward() {
+        val randomNumber = randomNumber.generate()
+        if (checkMoveOrStop(randomNumber)) {
+            distance += DASH
+        }
+    }
+
+    private fun checkMoveOrStop(randomNumber: Int): Boolean {
+        return when (randomNumber) {
+            in STOP_START_RANGE..STOP_END_RANGE -> false
+            in MOVE_START_RANGE..MOVE_END_RANGE -> true
+            else -> throw IllegalArgumentException("지정된 범위를 벗어난 랜덤숫자 입니다.")
+        }
+    }
+
     private fun validateCarNameLength(carName: String) =
         require(carName.length <= MAX_CAR_NAME_LENGTH) { "자동차 이름은 다섯글자를 넘을 수 없습니다." }
 
     companion object {
         private const val INITIAL_DISTANCE_VALUE = ""
         private const val MAX_CAR_NAME_LENGTH = 5
+        private const val STOP_START_RANGE = 0
+        private const val STOP_END_RANGE = 3
+        private const val MOVE_START_RANGE = 4
+        private const val MOVE_END_RANGE = 9
+        private const val DASH = "-"
     }
 }
