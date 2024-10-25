4498a061a6055b6d0f71e2157c29d9bca1e9ffbe
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 18:59:15 +0900

    fix(raceManger): 생성자에 matches 파라미터 추가

--- src/main/kotlin/racingcar/useCases/RaceManger.kt
+++ src/main/kotlin/racingcar/useCases/RaceManger.kt
@@ -5,18 +5,19 @@ import racingcar.domain.RaceResult
 import racingcar.infrastructure.RandomNumber
 
 class RaceManger (
-    private val cars: List<Car>
+    private val readyRacingCars: List<Car>,
+    private val matches: Int
 ){
-    fun playRace(matches: Int) {
+    fun playRace() {
         repeat(matches) {
-            cars.forEach { car -> car.goOneStep(RandomNumber.pick()) }
+            readyRacingCars.forEach { readyRacingCar -> readyRacingCar.goOneStep(RandomNumber.pick()) }
         }
         findWinners()
     }
 
     private fun findWinners(): RaceResult {
-        val maxDistance: Int = cars.maxOf { it.distance }
-        val winners: List<Car> = cars.filter { it.distance == maxDistance }
+        val maxDistance: Int = readyRacingCars.maxOf { it.distance }
+        val winners: List<Car> = readyRacingCars.filter { it.distance == maxDistance }
         val winnersName: List<String> = winners.map{ it.name }
         return RaceResult(winnersName)
     }
