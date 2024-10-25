635fe79fae8cb35ab704a612aa2f488d25c54cbd
joonhokil <kiljh@me.com>
2024-10-25 16:26:19 +0900

    refactor: 필요없는 assertSimpleTest 제거

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -10,26 +10,20 @@ import org.junit.jupiter.api.assertThrows
 class ApplicationTest : NsTest() {
     @Test
     fun `차 하나만 참여`() {
-        assertSimpleTest {
-            val carRacing = CarRacing("pobi")
-            assertThat(carRacing.getNamesOfCars()).isEqualTo(listOf("pobi"))
-        }
+        val carRacing = CarRacing("pobi")
+        assertThat(carRacing.getNamesOfCars()).isEqualTo(listOf("pobi"))
     }
 
     @Test
     fun `차 두대가 참여`() {
-        assertSimpleTest {
-            val carRacing = CarRacing("pobi,woni")
-            assertThat(carRacing.getNamesOfCars()).isEqualTo(listOf("pobi","woni"))
-        }
+        val carRacing = CarRacing("pobi,woni")
+        assertThat(carRacing.getNamesOfCars()).isEqualTo(listOf("pobi","woni"))
     }
 
     @Test
     fun `차 세대가 참여`() {
-        assertSimpleTest {
-            val carRacing = CarRacing("pobi,woni,jun")
-            assertThat(carRacing.getNamesOfCars()).isEqualTo(listOf("pobi","woni","jun"))
-        }
+        val carRacing = CarRacing("pobi,woni,jun")
+        assertThat(carRacing.getNamesOfCars()).isEqualTo(listOf("pobi","woni","jun"))
     }
 
     @Test
