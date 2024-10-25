9e6d618ea3c9eb6f5f7490e818374373f7a9acba
choiminjun <mj010504@naver.com>
2024-10-25 16:51:21 +0900

    feat: 자동차, 시도할 횟수 입력 관련 테스트 추가

--- src/main/kotlin/racingcar/utils/Validator.kt
+++ src/main/kotlin/racingcar/utils/Validator.kt
@@ -2,9 +2,11 @@ package racingcar.utils
 
 object Validator {
     fun isValidCarName(carName: String) {
+        // 자동차 이름의 길이는 5이하여야 한다.
         if (carName.length > Values.CAR_NAME_LENGTH_LIMIT) throwInvalidStringException(Messages.INVALID_CAR_NAME_LENGTH)
+
         // 자동차 이름은 문자열 또는 문자열 + 숫자의 형태만 가능하다.
-        val regex = Regex("^[A-Za-z]+[0-9]*$")
+        val regex = Regex("^[A-Za-z가-힣]+[0-9]*+[A-Za-z가-힣]*$")
         if (!regex.matches(carName)) throwInvalidStringException(Messages.INVALID_CAR_NAME)
     }
 
--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -4,27 +4,47 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import racingcar.model.Car
+import racingcar.utils.Messages
+import racingcar.utils.Validator
 
 class ApplicationTest : NsTest() {
-    @Test
-    fun `기능 테스트`() {
-        assertRandomNumberInRangeTest(
-            {
-                run("pobi,woni", "1")
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
-            },
-            MOVING_FORWARD, STOP
-        )
+
+    @ParameterizedTest
+    @ValueSource(strings = ["carcar", "carcarcarcar"])
+    fun `자동차 이름 길이 수는 5 이하여야 한다`(carName: String) {
+        assertThatThrownBy {
+            run("$carName, $carName")
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.INVALID_CAR_NAME_LENGTH)
     }
 
-    @Test
-    fun `예외 테스트`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+    @ParameterizedTest
+    @ValueSource(strings = ["", "5", "-", "k-5", "5k", "5가"])
+    fun `자동차 이름은 문자열 또는 문자열 + 숫자 또는 문자열 + 숫자 + 문자열의 형태만 가능하다`(carName: String) {
+        assertThatThrownBy {
+            run("$carName, $carName")
         }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.INVALID_CAR_NAME)
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["+", "a", "가"])
+    fun `시도할 횟수는 정수이어야 한다`(moveCount: String) {
+        assertThatThrownBy {
+            run("car,car2,car3", moveCount)
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(Messages.INVALID_MOVE_COUNT)
+    }
+
 
     override fun runMain() {
         main()
