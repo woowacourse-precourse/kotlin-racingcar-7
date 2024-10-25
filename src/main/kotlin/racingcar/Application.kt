92c02e881879bb6cd52350f8b916571d31cc1245
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2024-10-25 18:08:10 +0900

    feat: 자동차 이름 및 횟수 입력 받기 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+
+    val names = try {
+        Console.readLine()
+    } catch (e: Exception) {
+        ""
+    }
+
+    println("시도할 횟수는 몇 회인가요?")
+
+    val tryNumber = try {
+        Console.readLine()
+    } catch (e: Exception) {
+        ""
+    }
+
+    println("실행 결과")
+}
+
+fun race(names: String, tryNumber: Int) {
+
 }
