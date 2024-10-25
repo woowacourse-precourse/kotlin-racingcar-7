f604fce793226de424b16f5d14369ea3c1cf095e
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-26 04:09:17 +0900

    feat(Output): output interface 생성
    consoleouput과 raceManger 2개 파일 모두 같은 함수를 사용해야하기 때문에 interface 생성

---
+++ src/main/kotlin/racingcar/infrastructure/Output.kt
@@ -0,0 +1,9 @@
+package racingcar.infrastructure
+
+import racingcar.domain.Car
+
+interface Output {
+    fun showMessage()
+    fun showRoundResult(cars: List<Car>)
+    fun showWinners(winners: List<String>)
+}
