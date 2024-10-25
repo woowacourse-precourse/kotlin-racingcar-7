5a3a689bf2c0d893e9a64c0e64aac5791dc5e4c3
jklom0326 <jklom0326@gmail.com>
2024-10-26 03:36:29 +0900

    feat: 자동차 이름 유효성 검사(이름이 공백일 경우)

--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -5,8 +5,14 @@ class Validation {
     fun checkCarNameLength(carName: List<Car>) {
         carName.forEach {
             if (it.name.length > 5) {
-                throw IllegalArgumentException("자동차 이름은 5자 이하만 입력 가능합니다.")
+                throw IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.")
             }
+        }
+    }
+
+    fun checkCarNameBlank(carName: List<Car>) {
+        if (carName.any { it.name.isBlank() }) {
+            throw IllegalArgumentException("자동차 이름이 공백일 수 없습니다.")
         }
     }
 }
