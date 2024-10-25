1d384c169d6426e99625977351c8653c59b47bbd
jburnoo <6pizz@naver.com>
2024-10-25 19:50:04 +0900

    feat: 사용자 입력 유효성 검사
    사용자 입력을 쉼표로 구분하여 검사한다.
    이름 체크 사항(길이, 공백, 중복)
    횟수 체크 사항(1 이상)
    차량 리스트 반환을 위한 Car data class 정의

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,4 +1,52 @@
 package racingcar.controller
 
+import racingcar.model.Car
+import racingcar.view.InputView
+
 class RaceController {
+    private val inputView = InputView()
+
+    fun run() {
+        val validCarList = getValidCarList()
+        val validRounds = getValidRounds()
+
+
+        inputView.close()
+    }
+
+    private fun getValidCarList(): List<Car> {
+        val carNames: String = inputView.getCarNames()
+        val tmpCarList = carNames.split(",")
+
+        isAllValid(tmpCarList)
+
+        return tmpCarList.map { carName -> Car(name = carName) }
+    }
+
+    private fun isAllValid(tmpCarList: List<String>) {
+        tmpCarList.map { carName ->
+            isValidCarNameLength(carName)
+            hasBlankInCarName(carName)
+        }
+        hasDuplicatedCarName(tmpCarList)
+    }
+
+    private fun isValidCarNameLength(carName: String) {
+        val len = carName.length
+        if (1 > len || len > 5) throw IllegalArgumentException("차량 이름의 길이는 1-5자 입니다.")
+    }
+
+    private fun hasBlankInCarName(carName: String) {
+        if (carName.contains(" ")) throw IllegalArgumentException("차량 이름에 공백을 제거해 주세요.")
+    }
+
+    private fun hasDuplicatedCarName(carNameList: List<String>) {
+        if (carNameList.toSet().size != carNameList.size) throw IllegalArgumentException("차량 이름은 중복될 수 없습니다.")
+    }
+
+    private fun getValidRounds(): Int {
+        val rounds = inputView.getRounds()
+        if (rounds < 1) throw IllegalArgumentException("1 이상의 횟수를 입력해 주세요.")
+        return rounds
+    }
 }
--- src/main/kotlin/racingcar/model/Race.kt
+++ src/main/kotlin/racingcar/model/Race.kt
@@ -2,3 +2,10 @@ package racingcar.model
 
 class Race {
 }
+
+data class Car(val name: String) {
+    var moves = 0
+        private set
+
+    fun move() = moves++
+}
