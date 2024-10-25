1ebcacadf409bf9d6000748abfca009d759af318
yewon-yw <kimyewon0401@gmail.com>
2024-10-25 22:39:21 +0900

    feat(Application): 시도할 횟수 입력받기
    시도할 횟수 입력받는 부분 추가
    - 숫자로 변환이 불가능하면 IllegalArgumentException 발생

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,4 +5,11 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val cars = Console.readLine().split(",").map { it.trim() }
     if (cars.any { it.length > 5 }) throw IllegalArgumentException()
+
+    val times: Int
+    try {
+        times = Console.readLine().toInt()
+    } catch (e: Exception) {
+        throw IllegalArgumentException()
+    }
 }
