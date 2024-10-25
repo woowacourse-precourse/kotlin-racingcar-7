87bb48d5cc25bb75a38a4269a5d5607043b3e8bf
unknown <yangjk9203@naver.com>
2024-10-25 15:47:15 +0900

    feat: 이동 횟수 입력, 이동 횟수 입력 오류 처리 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,7 +7,8 @@ fun main() {
 
     CarsNameInput()
     CarsNameChecker().carsNameChecker()
-
+    Count()
+    CountChecker().countChecker()
 
 }
 
@@ -37,5 +38,29 @@ class CarsNameChecker {
                 throw IllegalArgumentException("이름이 다섯 글자를 초과했습니다.")
             }
         }
+    }
+}
+
+class Count {
+
+    fun count(): Any {
+        println("시도할 횟수는 몇 회인가요?")
+        return Console.readLine()
+    }
+}
+
+class CountChecker {
+    private val count = Count().count().toString()
+    fun countChecker(): Int {
+
+        if (count.isBlank()) {
+            throw IllegalArgumentException("횟수를 입력하지 않았습니다.")
+        }
+
+        if (!count.contains(Regex("^[0-9]*$"))) {
+            throw IllegalArgumentException("숫자만 입력 가능합니다.")
+        }
+
+        return count.toInt()
     }
 }
