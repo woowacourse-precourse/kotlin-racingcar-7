23c70f068a0291ccd9486530dcd24fa238f15ed7
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 12:44:57 +0900

    feat(raceManger): 자동차 게임 진행 로직 생성
    RaceManger에 자동차 경기 진행 로직 구현
    경기가 끝나면 우승자 판별 로직 호출

--- src/main/kotlin/racingcar/useCases/RaceManger.kt
+++ src/main/kotlin/racingcar/useCases/RaceManger.kt
@@ -2,9 +2,19 @@ package racingcar.useCases
 
 import racingcar.domain.Car
 import racingcar.domain.RaceResult
+import racingcar.infrastructure.RandomNumber
 
-class RaceManger (private val cars: List<Car>){
-    fun findWinners(): RaceResult {
+class RaceManger (
+    private val cars: List<Car>
+){
+    fun playRace(matches: Int) {
+        repeat(matches) {
+            cars.forEach { car -> car.goOneStep(RandomNumber.pick()) }
+        }
+        findWinners()
+    }
+
+    private fun findWinners(): RaceResult {
         val maxDistance: Int = cars.maxOf { it.distance }
         val winners: List<Car> = cars.filter { it.distance == maxDistance }
         val winnersName: List<String> = winners.map{ it.name }
