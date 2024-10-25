19bfd263d598382e4d52b6e46095698e8619d0d1
wjdrjs00 <kimjk3409@gmail.com>
2024-10-26 03:18:14 +0900

    feat(car): 차 이름 길이 검증 기능 추가
    - 5글자를 넘는지 확인을 위한 검증 기능 추가한다.
    - 초기화시 검증을 수행하도록 init 블럭을 활용한다.
    - 재사용성을 위한 최대 문자길이 상수화

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -7,7 +7,15 @@ class Car(
 
     var distance: String = INITIAL_DISTANCE_VALUE
 
+    init {
+        validateCarNameLength(carName)
+    }
+
+    private fun validateCarNameLength(carName: String) =
+        require(carName.length <= MAX_CAR_NAME_LENGTH) { "자동차 이름은 다섯글자를 넘을 수 없습니다." }
+
     companion object {
         private const val INITIAL_DISTANCE_VALUE = ""
+        private const val MAX_CAR_NAME_LENGTH = 5
     }
 }
