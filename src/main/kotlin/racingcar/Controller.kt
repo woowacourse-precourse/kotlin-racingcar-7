fd92ef273188ac9c3b55d6e60fd720ecaccd1041
ijh1298 <imjh0417@gmail.com>
2024-10-25 20:38:06 +0900

    feat: Controller 추가 및 run 메소드의 입력 부분 구현

---
+++ src/main/kotlin/racingcar/Controller.kt
@@ -0,0 +1,19 @@
+package racingcar
+
+class Controller(private val model: Model, private val view: View) {
+    fun run() {
+        val inputCarNames = view.getCars()
+        val carNames = try {
+            inputCarNames.split(',')
+        } catch (_: Exception) {
+            throw IllegalArgumentException("잘못된 자동차 입력입니다.")
+        }
+
+        val inputTryNum = view.getTryNumber()
+        val tryNum = try {
+            inputTryNum.toInt()
+        } catch (_: Exception) {
+            throw IllegalArgumentException("잘못된 시도 횟수 입력입니다.")
+        }
+    }
+}
