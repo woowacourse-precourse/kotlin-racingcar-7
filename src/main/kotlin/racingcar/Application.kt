eaaf1ad84b6a7613d84fd0d69ef4d335ea56e8a7
yewon-yw <kimyewon0401@gmail.com>
2024-10-25 22:33:43 +0900

    feat(Application): 자동차 이름 입력받기
    자동차 이름 입력받는 부분 추가
    - 쉼표(,)를 기준으로 구분
    - 공백 제거
    - 이름이 5자를 초과하면 IllegalArgumentException 발생

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,8 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val cars = Console.readLine().split(",").map { it.trim() }
+    if (cars.any { it.length > 5 }) throw IllegalArgumentException()
 }
