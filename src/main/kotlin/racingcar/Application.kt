04057267edf665b3b3a72fe310e31628b0fcac73
Min Seon <cbdhoho1@gmail.com>
2024-10-25 21:53:53 +0900

    feat: add initTryCountList

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,9 +5,8 @@ import camp.nextstep.edu.missionutils.Console.readLine
 fun main() {
     val cars = carNameInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carList = splitCarName(cars)
-    val tryCount = tryCountInput("시도할 횟수는 몇 회인가요?")
-    var tryCountList: List<Int>
-
+    val tryCount = tryCountInput("시도할 횟수는 몇 회인가요?").toInt()
+    val tryCountList = initTryCountList(tryCount)
 }
 fun carNameInput(prompt: String): String{
     println(prompt)
@@ -22,4 +21,9 @@ fun tryCountInput(promt: String): String{
 fun splitCarName(carNames: String): List<String>{
     val splitedCarNames = carNames.split(",")
     return splitedCarNames
+}
+
+fun initTryCountList(tryCount: Int): MutableList<Int>{
+    val tryCountList = MutableList(tryCount) { 0 }
+    return tryCountList
 }
