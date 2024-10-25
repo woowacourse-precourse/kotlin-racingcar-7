3bf59ba893489d0d7330b4ca39fb27085b2bd5db
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 19:23:12 +0900

    fix(input): console.close() 삭제
    console.readLine()에서 값을 받아야하는 데, Console.close()로 에러 발생
    임시로 삭제 후, 다시 고칠 예정

--- src/main/kotlin/racingcar/infrastructure/Input.kt
+++ src/main/kotlin/racingcar/infrastructure/Input.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 interface Input {
     fun getInput(): String {
         val input = Console.readLine()
-        Console.close()
+
         return input
     }
 }
