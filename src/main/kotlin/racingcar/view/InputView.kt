d62fef55884914495266bf6ce3cfa5a93f158b0a
wjdrjs00 <kimjk3409@gmail.com>
2024-10-25 12:36:11 +0900

    feat(view): 반복 랩 횟수 입력 기능 구현
    랩 횟수를 반환하는 메서드를 추가한다:
    - 랩 횟수 입력 안내메세지 출력을 위한 메서드를 추가한다.
    - 재사용성을 위해 랩 횟수 입력 안내메세지를 상수화 한다.
    - readLine메서드를 통해 랩 횟수를 입력받는다.
    - 입력된 랩 횟수를 반환단다.

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -10,11 +10,20 @@ class InputView {
         return carNames
     }
 
+    fun inputLapCount(): String {
+        printInputLapCountMessage()
+        val lapCount = readLine()
+        return lapCount
+    }
+
     private fun readLine() = Console.readLine()
 
     private fun printInputCarNamesMessage() = println(INPUT_CAR_NAME_MESSAGE)
 
+    private fun printInputLapCountMessage() = println(INPUT_LAP_COUNT_MESSAGE)
+
     companion object {
         private const val INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val INPUT_LAP_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
     }
 }
