73122b92c7da3d0e01030f81bc24d0b3c0530a4b
unknown <yangjk9203@naver.com>
2024-10-25 15:24:09 +0900

    feat: 자동차 이름 입력, 자동차 이름 입력 오류 처리 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,41 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    CarsNameInput()
+    CarsNameChecker().carsNameChecker()
+
+
+}
+
+class CarsNameInput {
+
+    fun carsNameInput(): String {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        return Console.readLine()
+    }
+}
+
+
+class CarsNameChecker {
+
+    private val carsNameInput = CarsNameInput().carsNameInput()
+
+    fun carsNameChecker() {
+
+        if (carsNameInput.isBlank()) {
+            throw IllegalArgumentException("이름을 입력하지 않았습니다.")
+        }
+
+        val carName = carsNameInput.split(",")
+
+        for (i in carName) {
+            if (i.count() > 5) {
+                throw IllegalArgumentException("이름이 다섯 글자를 초과했습니다.")
+            }
+        }
+    }
 }
