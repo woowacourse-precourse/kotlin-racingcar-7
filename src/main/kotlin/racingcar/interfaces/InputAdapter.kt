25bcb968a469d21bbd6d60eacfc9384b1735631f
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-25 17:49:37 +0900

    feat(inputAdapter): 사용자로 받은 값에 대한 유효성 검사와 타입 변환
    자동차 이름 유효성 검사 조건
        - 5자 초과하는 문자열이 들어왔을 경우
        - 공백이 들어왔을 경우
        - 동일한 이름을 넣는 경우
        - 빈 문자로 이루어진 경우
    
    시도 횟수 유효성 검사 조건
        - 숫자가 아닌 경우
        - 0이거나 음수인 경우
        - 소수인 경우
    
    위 유효성 검사를 통과하면 UseCase에서 사용할 수 있도록 타입 변환

---
+++ src/main/kotlin/racingcar/interfaces/InputAdapter.kt
@@ -0,0 +1,61 @@
+package racingcar.interfaces
+
+import racingcar.domain.Car
+
+class InputAdapter (
+    private val carNames: String,
+    private val matches: String
+) {
+    fun splitCarName(): List<String> {
+        val cars = carNames.split(DELIMITER)
+        return cars
+    }
+
+    fun validateCarName(cars: List<String>): List<String> {
+        var racingCars = mutableListOf<String>()
+        for (car in cars) {
+            when {
+                car.length > 5 -> throw IllegalArgumentException(INPUT_ERROR_MSG + CARNAME_LESS_THAN_5_EXCEPTION_MSG)
+                car.isEmpty() -> throw IllegalArgumentException(INPUT_ERROR_MSG + CARNAME_EMPTY_EXCEPTION_MSG)
+                car.isBlank() -> throw IllegalArgumentException(INPUT_ERROR_MSG + CARNAME_WHITESPACE_EXCEPTION_MSG)
+                racingCars.contains(car) -> throw IllegalArgumentException(INPUT_ERROR_MSG + CARNAME_SAME_CARNAME_EXCEPTION_MSG)
+            }
+
+            racingCars.add(car)
+        }
+        return racingCars
+    }
+
+    fun changeTypeOfCar(racingCars: List<String>): List<Car> {
+        val readyRacingCars: List<Car> = racingCars.map{ Car(it) }
+        return readyRacingCars
+    }
+
+    fun changeTypeOfMatches(): Int {
+        return matches.toInt()
+    }
+
+    fun validateMatches(): Boolean {
+        when {
+            matches.toIntOrNull() == null -> throw IllegalArgumentException(INPUT_ERROR_MSG + MATCHES_NOT_INT_EXCEPTION_MSG)
+            matches.toInt() == 0 -> throw IllegalArgumentException(INPUT_ERROR_MSG + MATCHES_ZERO_EXCEPTION_MSG)
+            matches.toInt() < 0 -> throw IllegalArgumentException(INPUT_ERROR_MSG + MATCHES_NEGATIVE_NUMBER_EXCEPTION_MSG)
+        }
+        return true
+    }
+
+    companion object {
+        private const val DELIMITER = ","
+
+        const val INPUT_ERROR_MSG: String = "잘못된 입력입니다: "
+
+        private const val CARNAME_LESS_THAN_5_EXCEPTION_MSG: String = "자동차 이름에 이름은 5자 이하만 가능합니다."
+        private const val CARNAME_WHITESPACE_EXCEPTION_MSG = "자동차 이름에 공백을 입력했습니다."
+        private const val CARNAME_EMPTY_EXCEPTION_MSG = "자동차 이름에 빈 문자을 입력했습니다."
+        private const val CARNAME_SAME_CARNAME_EXCEPTION_MSG = "자동차 이름에 동일한 이름을 입력했습니다."
+
+        private const val MATCHES_NOT_INT_EXCEPTION_MSG: String = "시도 횟수에 정수가 아닌 값을 입력했습니다."
+        private const val MATCHES_ZERO_EXCEPTION_MSG: String = "시도 횟수에 0을 입력했습니다."
+        private const val MATCHES_NEGATIVE_NUMBER_EXCEPTION_MSG: String = "시도 횟수에 음수를 입력했습니다."
+    }
+}
