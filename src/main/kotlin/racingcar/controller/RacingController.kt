4c5930b7eba45979df574d0f35e92d2d13cce710
yrsel <xxknu88@gmail.com>
2024-10-25 14:58:51 +0900

    feat(RacingController): 레이싱 게임 전반적 흐름 담당
    게임의 전체적 흐름을 위해 생성
    유효성 검사 위치에 대한 고민을 하다 컨트롤러에서 처리하는 방식을 채택

---
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -0,0 +1,12 @@
+package racingcar.controller
+
+import racingcar.domain.numbergenerator.NumberGenerator
+
+class RacingController(
+    private val numberGenerator: NumberGenerator
+) {
+
+    fun play() {
+
+    }
+}
