be6290558c3eb005d7f19b9b8d399875805b7de5
jhseo12 <jhseo1778@gmail.com>
2024-10-25 17:03:04 +0900

    refactor: 무작위 값 발생 함수 추가 분리

--- src/main/kotlin/racingcar/model/Move.kt
+++ src/main/kotlin/racingcar/model/Move.kt
@@ -5,16 +5,25 @@ import camp.nextstep.edu.missionutils.Randoms
 class Move {
     fun move(infoOfCar: MutableMap<String, Int>): MutableMap<String, Int> {
         for ((name, move) in infoOfCar) {
-            getRandomNumber(infoOfCar, name, move)
+            moveCar(infoOfCar, name, move, getRandomNumber())
         }
         return infoOfCar
     }
 
-    private fun getRandomNumber(infoOfCar: MutableMap<String, Int>, name: String, move: Int): Int {
-        val randomNumber = Randoms.pickNumberInRange(0, 9)
+    private fun moveCar(
+        infoOfCar: MutableMap<String, Int>,
+        name: String,
+        move: Int,
+        randomNumber: Int,
+    ): MutableMap<String, Int> {
         if (randomNumber >= 4) {
             infoOfCar[name] = move + 1
         }
+        return infoOfCar
+    }
+
+    private fun getRandomNumber(): Int {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
         return randomNumber
     }
 }
