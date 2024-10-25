a8daac9b0771370db47700c637f8d69ea9c4c7ad
yrsel <xxknu88@gmail.com>
2024-10-25 21:33:37 +0900

    feat(RoundValidator, RoundValidatorTest): 라운드 횟수에 대한 검증

---
+++ src/main/kotlin/racingcar/validator/RoundValidator.kt
@@ -0,0 +1,25 @@
+package racingcar.validator
+
+import racingcar.exception.ExceptionMessage
+
+private const val ROUND_LOWER_BOUND = 1
+private const val ROUND_UPPER_BOUND = 10_000
+
+object RoundValidator {
+
+    fun validate(numberOfRoundInput: String): Int {
+        require(isNumeric(numberOfRoundInput)) { ExceptionMessage.INVALID_NUMERIC_INPUT.message }
+        require(isValidNumberOfRound(numberOfRoundInput)) { ExceptionMessage.INVALID_NUMBER_OF_ROUND }
+
+        return numberOfRoundInput.toInt()
+    }
+
+    private fun isNumeric(input: String): Boolean {
+        return input.all { it.isDigit() }
+    }
+
+    private fun isValidNumberOfRound(input: String): Boolean {
+        return input.toInt() in ROUND_LOWER_BOUND..ROUND_UPPER_BOUND
+    }
+
+}
