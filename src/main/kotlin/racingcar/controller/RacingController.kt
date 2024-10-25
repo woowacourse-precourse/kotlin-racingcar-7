365417269acd0a05c37c1df6c9315566530f2891
yrsel <xxknu88@gmail.com>
2024-10-25 22:28:46 +0900

    feat(RacingController): 레이싱 게임 전반적 흐름 완성

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -1,12 +1,42 @@
 package racingcar.controller
 
+import racingcar.domain.car.Cars
 import racingcar.domain.numbergenerator.NumberGenerator
+import racingcar.validator.CarNameValidator
+import racingcar.validator.RoundValidator
+import racingcar.view.*
 
 class RacingController(
     private val numberGenerator: NumberGenerator
 ) {
 
     fun play() {
-
+        val carNames = getCarNames()
+        val rounds = getNumberOfRound()
+        val racingCarsStatus = startRace(carNames, rounds)
+        raceWinnerView(racingCarsStatus)
     }
+
+    private fun getCarNames(): String {
+        val carNamesInput = carNamesView()
+        return CarNameValidator.validate(carNamesInput)
+    }
+
+    private fun getNumberOfRound(): Int {
+        val numberOfRoundInput = numberOfRoundView()
+        return RoundValidator.validate(numberOfRoundInput)
+    }
+
+    private fun startRace(carNames: String, rounds: Int): Cars {
+        val racingCars = Cars(carNames)
+
+        printResultComment()
+        for (round in 1..rounds) {
+            racingCars.lap(numberGenerator)
+            printRacingStatus(racingCars)
+        }
+
+        return racingCars
+    }
+
 }
