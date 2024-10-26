7cebcd2e819422902991f572d29073d3fefa83f3
eunmyeungjun <audwns419@gmail.com>
2024-10-26 17:53:28 +0900

    feat: 무작위 숫자(0 ~9)를 생성하여 전진 여부 결정 기능구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,8 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 
@@ -8,4 +10,17 @@ fun main() {
 
     println("시도할 횟수는 몇 회인가요?")
     val times = readln().toInt() ?: 0
+
+    val distance = MutableList(carNames.size) {0}
+
+    move(distance)
+
+}
+
+private fun move (moveDistance: MutableList<Int>) {
+    moveDistance.indices.forEach { i ->
+        if (Randoms.pickNumberInRange(0 ,9) >= 4) {
+            moveDistance[i]++
+        }
+    }
 }
