ea519e481ec0e9c8dbe889bff30bd4ae99b6bbee
chanho0908 <uni070@naver.com>
2024-10-26 22:38:15 +0900

    feat(DependencyInjector.kt) : StateReducer, Playground 의존성 주입 추가

--- src/main/kotlin/racingcar/di/DependencyInjector.kt
+++ src/main/kotlin/racingcar/di/DependencyInjector.kt
@@ -1,12 +1,14 @@
 package racingcar.di
 
+import racingcar.PlayGround
+import racingcar.reducer.PlayerStateReducer
 import racingcar.delegate.ValidationDelegator
 import racingcar.delegate.ValidationDelegatorImpl
+import racingcar.delegate.playCount.PlayCountErrorDelegator
+import racingcar.viewModel.RacingViewModel
+import racingcar.delegate.playCount.PlayCountErrorDelegatorImpl
 import racingcar.delegate.name.NameErrorDelegator
 import racingcar.delegate.name.NameErrorDelegatorImpl
-import racingcar.delegate.playCount.PlayCountErrorDelegator
-import racingcar.delegate.playCount.PlayCountErrorDelegatorImpl
-import racingcar.viewModel.RacingViewModel
 
 class DependencyInjector {
 
@@ -16,7 +18,7 @@ class DependencyInjector {
         val validationDelegator =
             injectValidationDelegator(nameErrorDelegator, playCountErrorDelegator)
 
-        return RacingViewModel(validationDelegator)
+        return RacingViewModel(validationDelegator, injectPlayGround())
     }
 
     private fun injectValidationDelegator(
@@ -25,6 +27,10 @@ class DependencyInjector {
     ): ValidationDelegator {
         return ValidationDelegatorImpl(injectNameErrorDelegator, injectPlayCountErrorDelegator)
     }
+
+    private fun injectStateReducer(): PlayerStateReducer = PlayerStateReducer()
+
+    private fun injectPlayGround(): PlayGround = PlayGround(injectStateReducer())
 
     private fun injectNameErrorDelegator(): NameErrorDelegator = NameErrorDelegatorImpl()
 
