2a5ace6564571a9ba3b3580e9f2b9de0deaaa4ab
sorongosdev <dosora9@gmail.com>
2024-10-27 06:54:59 +0900

    feat(controller): 차 이동

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 import racingcar.model.Car
 import racingcar.utils.Validator
 import racingcar.view.InputView
+import racingcar.view.OutputView
 
 class RaceController {
     private val cars = mutableListOf<Car>()
@@ -14,9 +15,24 @@ class RaceController {
         createCars(carNames)
 
         val moveCount = InputView.getMoveCount()
+        moveCars(moveCount)
     }
 
     private fun createCars(carNames: List<String>) {
         cars.addAll(carNames.map { Car(it) })
+    }
+
+    private fun moveCars(moveCount: Int) {
+        for (i in 0 until moveCount) {
+            moveCarsForRound()
+        }
+    }
+
+    private fun moveCarsForRound() {
+        cars.forEach { car ->
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.move()
+            }
+        }
     }
 }
