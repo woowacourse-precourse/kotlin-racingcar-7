5c24a4ec0a9278ca97137cc45bfd212261e0d1c0
hwsg0912@naver.com <hwsg0912@naver.com>
2024-10-26 19:08:45 +0900

    feat: 자동차 이름 중복 시 예외 처리 기능 구현
    - 중복된 이름에 대한 예외 처리 함수 추가
    - 내부 함수에 private 적용하여 코드 구조 개선

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -5,11 +5,23 @@ import racingcar.model.RacingCar
 class RacingCarController {
     fun createRacingCars(input: String): List<RacingCar> {
         val racingCarNames = splitRacingCarNames(input)
-        return racingCarNames.map { racingCarName ->
-            RacingCar(racingCarName)
+        if (validateRacingCarNames(racingCarNames)) {
+            return racingCarNames.map { racingCarName ->
+                RacingCar(racingCarName)
+            }
+        }
+        throw IllegalArgumentException("레이싱카 생성이 실패했습니다.")
+    }
+
+    private fun splitRacingCarNames(input: String) = input.split(",")
+
+    private fun validateRacingCarNames(racingCarNames: List<String>): Boolean {
+        return when {
+            hasDuplicateNames(racingCarNames) -> throw IllegalArgumentException("자동차 이름은 중복될 수 없습니다.")
+            else -> true
         }
     }
 
-    fun splitRacingCarNames(input: String) = input.split(",")
+    private fun hasDuplicateNames(names: List<String>) = names.size != names.distinct().size
 
 }
