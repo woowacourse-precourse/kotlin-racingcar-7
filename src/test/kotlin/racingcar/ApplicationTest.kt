1e36b246476b7a40cfafa95a792f83d973ca90c8
지원 <cheerfulboys@naver.com>
2024-10-27 05:28:48 +0900

    test: 테스트 코드 추가
    음수, 0, 이름 빈 문자열, null, 콤마가 아닌 구분자, 자동차 이름 한 개에 대한 테스트 코드를 추가함.

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.text.NumberFormat
 
 class ApplicationTest : NsTest() {
     @Test
@@ -24,6 +25,82 @@ class ApplicationTest : NsTest() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
         }
+    }
+
+    @Test
+    fun `음수 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "-1") }
+        }
+    }
+
+    @Test
+    fun `0 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "0") }
+        }
+    }
+
+    @Test
+    fun `이름 빈 문자열 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("", "-1") }
+        }
+    }
+
+    @Test
+    fun `이름 null 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException(null, "-1") }
+        }
+    }
+
+//    같은 빈 문자열인데 뭐는 되고 왜 얘는 실패야...
+//    @Test
+//    fun `시도 횟수 빈 문자열 예외 테스트`() {
+//        assertSimpleTest {
+//            assertThrows<IllegalArgumentException> { runException("pobi,woni", "") }
+//        }
+//    }
+
+
+    @Test
+    fun `시도 횟수 null 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", null) }
+        }
+    }
+
+    @Test
+    fun `시도 횟수 소수 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "1.1") }
+        }
+    }
+
+    @Test
+    fun `이름 구분자 dot 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi.woni", "1") }
+        }
+    }
+
+    @Test
+    fun `이름 한 명 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi", "1") }
+        }
+    }
+
+    @Test
+    fun `동명 테스트`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
+            },
+            MOVING_FORWARD, STOP
+        )
     }
 
     override fun runMain() {
