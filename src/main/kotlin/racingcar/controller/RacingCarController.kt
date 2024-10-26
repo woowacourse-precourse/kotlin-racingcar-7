bdb602b05b61da9255a7f7770a4b0a6cdd41ed04
jerry8282 <ka06196@naver.com>
2024-10-27 02:26:26 +0900

    feat: 사용자로부터 시도 횟수 입력받기 및 예외출력

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -4,21 +4,33 @@ import racingcar.view.ErrorView
 import racingcar.view.InputView
 
 class RacingCarController {
-    private val view = InputView
-    private val errorview = ErrorView
+    private val inputView = InputView
+    private val errorView = ErrorView
 
     fun start() {
         try {
             val carNames = getCarNames()
+            val numberOfAttempts = getNumberOfAttempts()
         } catch (e: IllegalArgumentException) {
-            errorview.errorMessage(e.message ?: "오류가 발생했습니다.")
+            errorView.errorMessage(e.message ?: "오류가 발생했습니다.")
         }
     }
 
     private fun getCarNames(): List<String> {
-        val input = view.askForCarNames()
+        val input = inputView.askForCarNames()
+
         val carNames = input.split(",").map { it.trim() }
         require(carNames.all { it.length <= 5 }) { "자동차 이름은 5자 이하만 가능합니다." }
+
         return carNames
+    }
+
+    private fun getNumberOfAttempts(): Int {
+        val input = inputView.askForNumberOfAttempts()
+
+        val numberOfAttempts = input.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
+        require(numberOfAttempts > 0) { "시도 횟수는 1회 이상이어야 합니다." }
+
+        return numberOfAttempts
     }
 }
--- src/main/kotlin/racingcar/view/ErrorView.kt
+++ src/main/kotlin/racingcar/view/ErrorView.kt
@@ -1,6 +1,5 @@
 package racingcar.view
 
-
 object ErrorView{
     fun errorMessage(message: String) {
         println(message)
