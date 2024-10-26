f7312a71cd2dd21f04599b76b3e96233b836cec7
jklom0326 <jklom0326@gmail.com>
2024-10-27 01:13:06 +0900

    feat: 시도 횟수 문구 출력

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -5,15 +5,20 @@ class RacingGame {
     private val validation = Validation()
 
     fun gameStart() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        var cars = initCar()
+        val cars = initCar()
+        val roundCounts = initRound()
     }
 
     private fun initCar(): List<Car> {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val inputCarNames = computer.inputCarName()
         val carName = splitCarName(inputCarNames)
         validation.validateCarName(carName)
         return carName
+    }
+
+    private fun initRound() {
+        println("시도할 횟수는 몇 회인가요?")
     }
 
     private fun splitCarName(carName: String): List<Car> {
