14b89e50e0e557acd0ddb2c909956850b0893932
rkqls4764 <rkqls4764@naver.com>
2024-10-26 22:12:16 +0900

    feat: 전진 결정 함수 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import kotlin.random.Random
+
 fun main() {
 
 }
@@ -34,4 +36,16 @@ fun inputName(carList: MutableList<Car>) {
 fun inputTimes(): Int {
     println("시도할 횟수는 몇 회인가요?")
     return readLine()?.toInt() ?: throw IllegalArgumentException("숫자를 입력해야 합니다.")
+}
+
+/* 전진을 결정하는 함수 */
+fun isMoving(): Boolean {
+    val randomNum = Random.nextInt(10) //0에서 9 사이의 랜덤 값 생성
+
+    if (randomNum >= 4) { //랜덤 값이 4 이상이면 전진
+        return true
+    }
+    else { //아니면 멈춤
+        return false
+    }
 }
