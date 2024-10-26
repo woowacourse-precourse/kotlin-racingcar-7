ab4515598162b12cde2bda0c48027adef4651077
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-26 18:46:20 +0900

    test(raceManger): raceManger 테스트 로직 추가
    - 자동차 게임 진행 테스트
    - 위너 찾기 테스트

--- src/test/kotlin/racingcar/useCases/RaceMangerTest.kt
+++ src/test/kotlin/racingcar/useCases/RaceMangerTest.kt
@@ -23,13 +23,15 @@ class RaceMangerTest {
     }
 
     @Test
+    @DisplayName("자동차 게임 진행 테스트")
     fun `자동차 게임 진행 테스트`() {
         raceManger.playRace()
         readyRacingCars.forEach { assert(it.distance >= 0) }
     }
 
     @Test
-    fun `위너 찾기 로직 테스트`() {
+    @DisplayName("우승자 판별 로직 테스트")
+    fun `우승자 판별 로직 테스트`() {
         readyRacingCars = listOf(Car("car1", 5), Car("car2", 3))
         output = ConsoleOutput()
         raceManger = RaceManger(readyRacingCars, 2, output)
