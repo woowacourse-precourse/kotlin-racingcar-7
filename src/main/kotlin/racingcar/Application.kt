f47cfd06c181ea01565ddf5660ee5186fd34728f
joonhokil <kiljh@me.com>
2024-10-26 13:37:20 +0900

    refactor: 상수명을 더 명확하게 개선

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,7 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
     print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
-    val input = readLine()
-    val carRacing = CarRacing(input)
+    val inputCarNames = readLine()
+    val carRacing = CarRacing(inputCarNames)
     carRacing.racing()
 }
