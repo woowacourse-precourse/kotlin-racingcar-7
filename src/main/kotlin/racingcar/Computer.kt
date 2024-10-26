8ff856a85d11e6262097a73167ec20c1669422fa
jklom0326 <jklom0326@gmail.com>
2024-10-27 02:12:11 +0900

    feat: 무작위 값 생성 기능

--- src/main/kotlin/racingcar/Computer.kt
+++ src/main/kotlin/racingcar/Computer.kt
@@ -1,10 +1,13 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 class Computer {
 
-    fun inputCarName(): String {
-        return Console.readLine()
-    }
+    fun inputCarName(): String = Console.readLine()
+
+    fun inputRoundCount(): String = Console.readLine()
+
+    fun generate() = Randoms.pickNumberInRange(1, 9)
 }
