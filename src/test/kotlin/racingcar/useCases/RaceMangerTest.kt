0d368adc599aa65f6c6c8b16e87a297eb745ca67
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-10-26 18:44:12 +0900

    test(raceManger): raceManger 테스트 로직 추가
    - 자동차 게임 진행 테스트
    - 위너 찾기 테스트

---
+++ src/test/kotlin/racingcar/useCases/RaceMangerTest.kt
@@ -0,0 +1,42 @@
+package racingcar.useCases
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import racingcar.domain.Car
+import racingcar.domain.RaceResult
+import racingcar.infrastructure.ConsoleOutput
+import racingcar.infrastructure.Output
+
+class RaceMangerTest {
+
+    private lateinit var readyRacingCars: List<Car>
+    private lateinit var output: ConsoleOutput
+    private lateinit var raceManger: RaceManger
+
+    @BeforeEach
+    fun setObject() {
+        readyRacingCars = listOf(Car("car1"), Car("car2"))
+        output = ConsoleOutput()
+        raceManger = RaceManger(readyRacingCars, 2, output)
+    }
+
+    @Test
+    fun `자동차 게임 진행 테스트`() {
+        raceManger.playRace()
+        readyRacingCars.forEach { assert(it.distance >= 0) }
+    }
+
+    @Test
+    fun `위너 찾기 로직 테스트`() {
+        readyRacingCars = listOf(Car("car1", 5), Car("car2", 3))
+        output = ConsoleOutput()
+        raceManger = RaceManger(readyRacingCars, 2, output)
+
+        val testWinner = raceManger.findWinners()
+        val exceptWinner = RaceResult(listOf("car1"))
+
+        assertEquals(exceptWinner, testWinner)
+    }
+}
