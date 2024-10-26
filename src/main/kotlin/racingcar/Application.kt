e3ea0ead756bdc10767575cd425393bc56480aa0
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-10-27 01:38:45 +0900

    feat: 자동차 이름 입력 기능

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carMap = mutableMapOf<String, Int>()
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    // TODO : trim() ? 으로 공백 제거? // 커밋 body에 올렸다 PR 설명에 적기
+    for (i in readLine().split(",")) {
+        carMap.put(i, 0)
+    }
 }
