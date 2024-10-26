a178293559931bdb178cff93e0d190c0a2ee8f6d
Tnalxmsk <alstnwkd990@naver.com>
2024-10-26 16:51:27 +0900

    refactor: Car 객체 상수 처리
    companion object를 통해 최대 이름 길이, 이동 기준점 상수화

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,5 +1,6 @@
 package racingcar.model
 
+import racingcar.enum.RacingRule.*
 import racingcar.utils.NumberGenerator
 
 class Car(private val name: String) {
@@ -7,19 +8,25 @@ class Car(private val name: String) {
     private var distance = 0
 
     val currentDistance: String
-        get() = "-".repeat(distance)
+        get() = DISTANCE.value.repeat(distance)
 
     init {
-        require(name.isNotEmpty() && name.length <= 5)
+        require(name.isNotEmpty() && name.length <= MAX_NAME_LENGTH)
     }
 
     fun getName() = name
 
     fun canMove(): Boolean {
-        return NumberGenerator.generate() >= 4
+        return NumberGenerator.generate() >= MOVE_THRESHOLD
     }
 
     fun moveForward() {
         distance++
+    }
+
+
+    companion object {
+        private const val MAX_NAME_LENGTH = 5
+        private const val MOVE_THRESHOLD = 4
     }
 }
