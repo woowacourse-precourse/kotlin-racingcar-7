59f118fcca119d287707179eed066bec7451b37c
unknown <yangjk9203@naver.com>
2024-10-27 00:19:49 +0900

    feat(SettingValue): 세팅에 필요한 입력값이 정리된 클래스 추가

---
+++ src/main/kotlin/racingcar/control/SettingValue.kt
@@ -0,0 +1,9 @@
+package racingcar.control
+
+class SettingValue {
+    val startNumber = 0
+    val endNumber = 9
+    val minimumNumberToMoveForward = 4
+    val delimiters = ","
+    val maximumNumberOfCharacters = 5
+}
