a7ec3bbdcd2d34c39e0a8fb832ca9b3548c31777
Heejun-0219 <heejun0219@gmail.com>
2024-10-26 16:49:01 +0900

    chore: mockito 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ repositories {
 
 dependencies {
     implementation("com.github.woowacourse-projects:mission-utils:1.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("org.mockito:mockito-all:1.10.19")
 }
 
 tasks {
