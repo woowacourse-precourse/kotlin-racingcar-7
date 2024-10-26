2d63bef70514cafc7e2d4b69416abf76f7d8468c
jklom0326 <jklom0326@gmail.com>
2024-10-26 23:12:35 +0900

    feat: 자동차 생성 기능

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -1,8 +1,22 @@
 package racingcar
 
 class RacingGame {
+    private val computer = Computer()
+    private val validation = Validation()
 
     fun gameStart() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        var cars = initCar()
+    }
+
+    private fun initCar(): List<Car> {
+        val inputCarNames = computer.inputCarName()
+        val carName = splitCarName(inputCarNames)
+        validation.validateCarName(carName)
+        return carName
+    }
+
+    private fun splitCarName(carName: String): List<Car> {
+        return carName.split(",").map { Car(it) }
     }
 }
