0f70d83c04263da0daee64889ce645a4af07791b
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-10-26 16:32:28 +0900

    feat: 최종 우승자

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,6 +3,25 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+
+fun getWinnerIndex(carsMovings : Array<Int>): MutableList<Int> {
+    val winners  = mutableListOf<Int>()
+    var maxNumber = 0
+
+    carsMovings.forEachIndexed { idx, it ->
+        if(it == maxNumber){
+            winners.addLast(idx)
+        }
+        if(it > maxNumber){
+            maxNumber = it
+            winners.clear()
+            winners.addLast(idx)
+        }
+    }
+
+    return winners
+}
+
 fun main() {
     // TODO: 프로그램 구현
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
@@ -28,4 +47,7 @@ fun main() {
         }
         println()
     }
+
+    val result = getWinnerIndex(carsMovings).map { cars[it] }
+    println("최종 우승자 : ${result.joinToString(", ")}")
 }
