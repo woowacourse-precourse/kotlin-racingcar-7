69f88c0f1591d3a98e1cd960210d3fb05f26721a
eunmyeungjun <audwns419@gmail.com>
2024-10-26 23:27:46 +0900

    feat: 최종 우승자 계산과 출력기능구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -17,7 +17,24 @@ fun main() {
         racingRound(carNames, distance)
     }
 
+    printWinner(carNames,distance)
+
+    Console.close()
 }
+
+private fun printWinner(carNames: List<String>, moveDistance: List<Int>) {
+    val winners = mutableListOf<String>()
+    var maxDistance = moveDistance.maxOrNull()
+
+    moveDistance.forEachIndexed { index, distance ->
+        if (distance == maxDistance) {
+            winners.add(carNames[index])
+        }
+    }
+    println("최종 우승자 : ${winners.joinToString(", ")}")
+}
+
+
 
 private fun racingRound(carNames: List<String>, moveDistance: MutableList<Int>) {
     isItMove(moveDistance)
