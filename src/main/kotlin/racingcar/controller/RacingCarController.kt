2026195c221912d8e4373bda7661d8726d78ae38
hwsg0912@naver.com <hwsg0912@naver.com>
2024-10-26 19:28:41 +0900

    feat: 입력한 이름이 빈 문자열일 경우 예외 처리 기능 추가

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -4,7 +4,7 @@ import racingcar.model.RacingCar
 
 class RacingCarController {
     fun createRacingCars(input: String): List<RacingCar> {
-        val racingCarNames = splitRacingCarNames(input)
+        val racingCarNames = parseRacingCarNames(input)
         if (validateRacingCarNames(racingCarNames)) {
             return racingCarNames.map { racingCarName ->
                 RacingCar(racingCarName)
@@ -13,15 +13,19 @@ class RacingCarController {
         throw IllegalArgumentException("레이싱카 생성이 실패했습니다.")
     }
 
-    private fun splitRacingCarNames(input: String) = input.split(",")
+    private fun parseRacingCarNames(input: String) = input.split(",").map { it.trim() }
 
     private fun validateRacingCarNames(racingCarNames: List<String>): Boolean {
         return when {
             hasDuplicateNames(racingCarNames) -> throw IllegalArgumentException("자동차 이름은 중복될 수 없습니다.")
+            isNameEmpty(racingCarNames) -> throw IllegalArgumentException("자동차의 이름이 입력되지 않았습니다.")
             else -> true
         }
     }
 
     private fun hasDuplicateNames(names: List<String>) = names.size != names.distinct().size
 
+    private fun isNameEmpty(names: List<String>): Boolean {
+        return names.any { it.isEmpty() }
+    }
 }
