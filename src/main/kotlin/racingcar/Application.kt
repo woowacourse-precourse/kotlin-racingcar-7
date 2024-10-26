1b564f9e1dcddc6dda5f589037f4023785a148ce
yewon-yw <kimyewon0401@gmail.com>
2024-10-27 00:50:19 +0900

    refactor(RacingCarController, Application):  컨트롤러 클래스 분리
    Application에 작성되어있던 로직을 Controller로 분리
    - InputView, OutputView로 입출력 실행
    - 전진 여부 결정 함수 Controller로 이동
    - 이동 거리를 갱신하는 함수 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,42 +1,7 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
-import racingcar.model.Car
+import racingcar.controller.RacingCarController
 
 fun main() {
-    val cars = Console.readLine().split(",").map { Car(it.trim(), StringBuilder()) }
-    if (cars.any { it.name.length > 5 }) throw IllegalArgumentException()
-
-    val times: Int
-    try {
-        times = Console.readLine().toInt()
-    } catch (e: Exception) {
-        throw IllegalArgumentException()
-    }
-
-    repeat(times) {
-        cars.forEach { car ->
-            if (canMove()) {
-                car.move.append("-")
-            }
-        }
-        printResult(cars)
-    }
-    print(getWinner(cars))
-}
-
-fun canMove(): Boolean = Randoms.pickNumberInRange(0, 9) >= 4
-
-fun printResult(cars: List<Car>) {
-    cars.forEach { car ->
-        println("${car.name} : ${car.move}")
-    }
-    println()
-}
-
-fun getWinner(car: List<Car>): String {
-    val maxMove = car.maxOf { it.move.length }
-    val winner = car.filter { it.move.length == maxMove }.map { it.name }
-    return winner.joinToString()
+    RacingCarController().startRace()
 }
