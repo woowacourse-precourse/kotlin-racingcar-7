c8a6055e468558b9ac5e894c6560cc2c5f352abf
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-10-26 15:16:59 +0900

    feat: 시도할 횟수 사용자 입력 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,7 +2,13 @@ package racingcar
 
 fun main() {
     // TODO: 프로그램 구현
-    val inputValue = readLine()
-    val cars = inputValue?.split(',')
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val inputCar = readLine()
+    val cars = inputCar?.split(',')
+
+
+    println("시도할 횟수는 몇 회인가요?")
+    val inputCount = readLine()
+    val count = inputCount
 
 }
