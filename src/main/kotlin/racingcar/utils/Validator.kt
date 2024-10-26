8d9187d8ff52e006f141462423d3aceca82545a0
sorongosdev <dosora9@gmail.com>
2024-10-27 07:41:06 +0900

    refactor(utils): 유효성 검사 로직 함수로 분리

--- src/main/kotlin/racingcar/utils/Validator.kt
+++ src/main/kotlin/racingcar/utils/Validator.kt
@@ -1,22 +1,34 @@
 package racingcar.utils
-
 object Validator {
     fun validateCarName(carList: List<String>) {
-        carList.forEach {
-            if (it.length > 5) {
-                throw IllegalArgumentException("이름은 5자 이내여야합니다.")
-            } else if(it.isBlank()) {
-                throw IllegalArgumentException("이름에 공백이 포함되어 있습니다.")
-            }
-            if (!it.all { it.isLetter() || it == ',' }) {
-                throw IllegalArgumentException("콤마(,)를 제외한 기호는 입력할 수 없습니다.")
-            }
+        carList.forEach { carName ->
+            validateCarNameLength(carName)
+            validateCarNameBlank(carName)
+            validateCarNameCharacters(carName)
         }
     }
 
-    fun validateMoveCount(moveCount: Int){
-        if(moveCount <= 0){
+    fun validateMoveCount(moveCount: Int) {
+        if (moveCount <= 0) {
             throw IllegalArgumentException("시도 횟수는 0이 아닌 양의 정수여야합니다.")
+        }
+    }
+
+    private fun validateCarNameLength(carName: String) {
+        if (carName.length > 5) {
+            throw IllegalArgumentException("이름은 5자 이내여야합니다.")
+        }
+    }
+
+    private fun validateCarNameBlank(carName: String) {
+        if (carName.isBlank()) {
+            throw IllegalArgumentException("이름에 공백이 포함되어 있습니다.")
+        }
+    }
+
+    private fun validateCarNameCharacters(carName: String) {
+        if (!carName.all { it.isLetter() || it == ',' }) {
+            throw IllegalArgumentException("콤마(,)를 제외한 기호는 입력할 수 없습니다.")
         }
     }
 }
