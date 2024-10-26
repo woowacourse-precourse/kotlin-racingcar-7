5cccbbfae8241e4e1e848a64926f748cdd5ab717
조형석 <chs9902@naver.com>
2024-10-26 15:55:48 +0900

    :sparkles: feat: 자동차 이름 입력 기능 추가

--- src/main/kotlin/racingcar/utils/Constants.kt
+++ src/main/kotlin/racingcar/utils/Constants.kt
@@ -1,4 +1,10 @@
 package racingcar.utils
 
 class Constants {
+    val INSERT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    val INSERT_COUNT = "시도할 횟수는 몇 회인가요?"
+    val RESULT = "실행 결과"
+    val WINNER = "최종 우승자 : "
+    val DASH = "-"
+    val COMMA = ","
 }
--- src/main/kotlin/racingcar/view/Input.kt
+++ src/main/kotlin/racingcar/view/Input.kt
@@ -1,4 +1,10 @@
 package racingcar.view
 
+import racingcar.utils.Constants
+
 class Input {
+    fun getCars():List<String>{
+        println(Constants().INSERT_CAR_NAMES)
+        return camp.nextstep.edu.missionutils.Console.readLine().split(",")
+    }
 }
