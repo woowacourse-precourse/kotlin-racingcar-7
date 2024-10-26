e841d287ac04ba64ef7f84a4610ad6b4189f3c91
oungsi2000 <oungsi1000@gmail.com>
2024-10-26 21:32:10 +0900

    feat: 첫 번째 기능 구현 완료
    자동차 이름과 시도 횟수를 입력받는 기능을 구현하였습니다
    추가된 함수 목록
    1. getCarNames() : 자동차 이름을 입력받는 기능입니다
    2. getTryCounts() : 시도핧 횟수를 입력받는 기능입니다
    3. argumentChecker() : 자동차 이름의 유효성을 검사합니다
    불일치 시 IllegalArgumentException 를 throw 합니다

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,46 @@
 package racingcar
 
+var carNames:List<String> = mutableListOf<String>()
+var tryCounts:Int? = 0
+
+fun argumentChecker(carName:String) {
+	if (carNames.size < 2) {
+		throw IllegalArgumentException("2개 이상의 자동차를 입력해주세요")
+	}
+	if (carName.trimIndent().length == 0) {
+		throw IllegalArgumentException("자동차 이름은 공백이 될 수 없습니다")
+	}
+	if (carName.length > 5) {
+		throw IllegalArgumentException("자동차 이름은 5자 이하로 입력해주세요")
+	}
+}
+
+fun getCarNames():List<String> {
+	println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+	carNames = camp.nextstep.edu.missionutils.Console.readLine().split(',')
+
+	for (carName in carNames) {
+		argumentChecker(carName)
+	}
+
+	return carNames
+}
+
+fun getTryCounts():Int? {
+	try {
+		println("시도할 횟수는 몇 회인가요?")
+		tryCounts = camp.nextstep.edu.missionutils.Console.readLine().toInt()
+	} catch (exception:NumberFormatException) {
+		throw IllegalArgumentException("숫자를 입력해주세요")
+	}
+
+	return tryCounts
+
+}
+
 fun main() {
-    // TODO: 프로그램 구현
+	getCarNames()
+	getTryCounts()
+
+	// TODO: 프로그램 구현
 }
