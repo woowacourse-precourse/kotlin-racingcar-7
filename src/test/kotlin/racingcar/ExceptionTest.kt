f63046e46bc4ac879723486cffa26c75e097c30c
jhseo12 <jhseo1778@gmail.com>
2024-10-26 09:41:32 +0900

    fix: 에러 문구 수정

--- src/test/kotlin/racingcar/ExceptionTest.kt
+++ src/test/kotlin/racingcar/ExceptionTest.kt
@@ -80,7 +80,7 @@ class ExceptionTest {
     companion object {
         private const val ERROR_SAME_NAME = "[경고] 중복된 이름이 있습니다."
         private const val ERROR_EMPTY_NAME = "[경고] 비어있는 이름이 있습니다."
-        private const val ERROR_INVALID_NAME = "[경고] 알파벳으로 이뤄진 이름만 입력 가능합니다."
+        private const val ERROR_INVALID_NAME = "[경고] 유니코드에 해당되는 문자로 이뤄진 이름만 입력 가능합니다."
         private const val ERROR_NAME_LENGTH = "[경고] 이름은 5글자를 초과할 수 없습니다."
         private const val ERROR_COUNT_SIZE = "[경고] 1이상이어야 합니다."
         private const val ERROR_ONLY_DIGIT = "[경고] 숫자만 입력 가능합니다."
