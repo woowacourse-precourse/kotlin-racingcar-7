ee5f0896ab7e16cf982aeb78373acfdf28158042
taegn <taegyunn123@gmail.com>
2024-10-27 02:00:11 +0900

    refactor: RacingCar, Input - carName의 validation check 위치를 Input class -> RacingCar class로 이전

--- src/main/kotlin/racingcar/Input.kt
+++ src/main/kotlin/racingcar/Input.kt
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console
 
 class Input {
     companion object {
-        private val CAR_NAME_LENGTH_RANGE = 1..5
         private val COUNT_RANGE = 0..1_000_000
         private val INPUT_EXCEPTION = IllegalArgumentException()
     }
@@ -12,22 +11,15 @@ class Input {
     fun carNamesAndCount(): Pair<List<String>, Int> = carNames() to count()
     private fun carNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        val carNames = readLine().split(",")
-        if (carNames.all {
-                it.length in CAR_NAME_LENGTH_RANGE &&
-                        it.all { c -> c.isUpperCase() || c.isLowerCase() || c.isDigit() }
-            }) {
-            return carNames
-        }
-        throw INPUT_EXCEPTION
+        return readLine().split(",").let { if (it.all(RacingCar::checkCarName)) it else throw INPUT_EXCEPTION }
     }
 
     private fun count(): Int {
         try {
             println("시도할 횟수는 몇 회인가요?")
-            val count =
-                readLine().let { if (it.length <= 7 && it.toInt() in COUNT_RANGE) it.toInt() else throw INPUT_EXCEPTION }
-            return count
+            val input = readLine()
+            require(input.length <= COUNT_RANGE.last.toString().length && input.toInt() in COUNT_RANGE)
+            return input.toInt()
         } catch (e: Exception) {
             throw INPUT_EXCEPTION
         }
