cad1327b90b14bd8fa767e08e058b21647931341
Tnalxmsk <alstnwkd990@naver.com>
2024-10-26 19:21:56 +0900

    feat: 시도 횟수 검증 에러 메세지 추가

--- src/main/kotlin/racingcar/enum/ValidationError.kt
+++ src/main/kotlin/racingcar/enum/ValidationError.kt
@@ -7,5 +7,7 @@ enum class ValidationError(val message: String) {
     INCLUDED_GAP(ERROR_FORMAT.format("공백이 포함되어 있습니다.")),
     INVALID_NAME(ERROR_FORMAT.format("잘못된 이름 형식입니다.")),
     DUPLICATION_NAME(ERROR_FORMAT.format("중복된 이름이 포함되어 있습니다.")),
-    EXCEEDED_PLAYER_LIMIT(ERROR_FORMAT.format("출전 가능한 선수는 최대 5명입니다."))
+    EXCEEDED_PLAYER_LIMIT(ERROR_FORMAT.format("출전 가능한 선수는 최대 5명입니다.")),
+    INVALID_ATTEMPT_COUNT(ERROR_FORMAT.format("시도 횟수는 1 이상의 정수만 입력 가능합니다.")),
+    EXCEEDED_ATTEMPT_COUNT(ERROR_FORMAT.format("최대 시도 횟수는 10번입니다."))
 }
