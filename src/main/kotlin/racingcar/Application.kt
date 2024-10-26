0e7e01a6943af491ac5b64e636b10b7f13105cb2
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2024-10-26 23:03:12 +0900

    feat: 이름을 쉼표 기준으로 구분하고 5자 이하로 제한하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,6 +2,7 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.IllegalArgumentException
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
@@ -24,4 +25,16 @@ fun main() {
 }
 
 fun race(names: String, tryNumber: Int) {
+    val delimiter = ","
+
+    val finalParticipants = names.split(delimiter.toRegex()).map { name ->
+        try {
+            if (name.length > 5) {
+                throw IllegalArgumentException("이름은 5자 이하여야 합니다.")
+            }
+            name
+        } catch (e: IllegalArgumentException) {
+            println("잘못된 값이 입력되었습니다: ")
+        }
+    }
 }
