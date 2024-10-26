13c9e6a57a15e72b8d61a5d3d1cea70309574170
unknown <yangjk9203@naver.com>
2024-10-26 23:07:52 +0900

    feat: 자동차 이름 입력 오류 메시지 추가

--- src/main/kotlin/racingcar/view/CarNameError.kt
+++ src/main/kotlin/racingcar/view/CarNameError.kt
@@ -3,6 +3,8 @@ package racingcar.view
 class CarNameError {
 
     val carNameBlank = "이름을 입력하지 않았습니다."
+    val onlyDelimiters = "구분자만 입력 되었습니다."
     val carNameOverFive = "이름은 다섯글자를 초과할 수 없습니다."
     val notCompetitor = "혼자 경주할 수 없습니다."
+
 }
