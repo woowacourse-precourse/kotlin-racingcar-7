7b50a313659172c33edf1311b67434c5aaa534bb
changzin <changzin0126@gmail.com>
2024-10-26 15:52:51 +0900

    feat(main): 경주가 완료되었을 때, 우승자를 뽑고 출력하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,27 +5,32 @@ import camp.nextstep.edu.missionutils.Randoms
 import java.util.StringTokenizer
 
 fun main() {
+    /** 자동차 이름 문자열 입력 받기 */
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    val input:String =  Console.readLine()
+    val input:String = Console.readLine()
 
     val carNameList: List<String> = makeCarNames(input)
     var carScore: MutableList<Int> = mutableListOf()
     var carString: MutableList<String> = mutableListOf()
 
+    /** 자동차 갯수로 데이터 초기화 */
     initListData(carNameList, carScore, carString)
 
+    /** 경주횟수 입력받기 */
     println("시도할 횟수는 몇 회인가요?")
     val totalCount:Int = makeInteger(Console.readLine())
 
+
+    /** 게임진행 */
+    println("실행 결과")
     for(i in 1..totalCount){
         executeGame(carScore, carString)
         printScore(carNameList, carString)
     }
 
+    /** 승자 출력 */
+    printWinner(carNameList, carScore)
 }
-
-
-
 
 fun makeCarNames(input: String): List<String>{
     val arr: MutableList<String> = mutableListOf()
@@ -39,6 +44,7 @@ fun makeCarNames(input: String): List<String>{
     }
     return arr
 }
+
 
 fun validateName(carName: String): Boolean{
     if (carName.length > 5){
@@ -66,6 +72,7 @@ fun makeInteger(totalCount: String): Int{
 fun executeGame(carScore: MutableList<Int>, carString: MutableList<String>){
     for(i in 0..<carScore.size){
         val rand = Randoms.pickNumberInRange(0, 9)
+        println(rand)
         if (rand >= 4){
             carScore[i] += 1
             carString[i] += "-"
@@ -75,9 +82,25 @@ fun executeGame(carScore: MutableList<Int>, carString: MutableList<String>){
 
 fun printScore(carNameList: List<String>, carString: MutableList<String>) {
     for(i in 0..<carNameList.size){
-        println(carNameList[i] + " : " + carString[i]);
+        println(carNameList[i] + " : " + carString[i])
     }
     println()
 }
 
+fun printWinner(carNameList: List<String>, carScore: MutableList<Int>) {
+    var maxScore = carScore.max()
+    var count = 0
+
+    print("최종 우승자 : ")
+
+    for(i in 0..<carScore.size){
+        if (carScore[i] == maxScore && count == 0){
+            print(carNameList[i])
+            count++
+        }
+        else if (carScore[i] == maxScore && count > 0){
+            print(", " + carNameList[i])
+        }
+    }
+}
 
