ded2e70cdbc880a9c73ed71223f8a0ceba958d93
unknown <yangjk9203@naver.com>
2024-10-26 23:15:55 +0900

    feat(CarNameValidator): 추가된 오류조건 처리 기능 추가

--- src/main/kotlin/racingcar/control/CarNameValidator.kt
+++ src/main/kotlin/racingcar/control/CarNameValidator.kt
@@ -9,11 +9,20 @@ class CarNameValidator {
         if (carNameInput.isBlank()) {
             throw IllegalArgumentException(CarNameError().carNameBlank)
         }
-        val carName: List<String> = carNameInput.split(",")
+        if (carNameInput.all{it.toString() == SettingValue().delimiters }) {
+            throw IllegalArgumentException(CarNameError().onlyDelimiters)
+        }
+
+        val carName = CarNameSeparator().carNameSeparator(carNameInput)
+
         for (i in carName) {
-            if (i.length > 5) {
+            if (i.length > SettingValue().maximumNumberOfCharacters) {
                 throw IllegalArgumentException(CarNameError().carNameOverFive)
             }
+        }
+
+        if (!carNameInput.contains(SettingValue().delimiters) || carName.count() == 1) {
+            throw IllegalArgumentException(CarNameError().notCompetitor)
         }
         return carName
     }
