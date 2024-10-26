99667684afad5bc3ff1550a4a7883395cc83db2c
jburnoo <6pizz@naver.com>
2024-10-27 04:24:14 +0900

    feat: Race 모델 구현
    조건(무작위 값 4 이상) 처리 및 전진 수행
    라운드 반복

--- src/main/kotlin/racingcar/model/Race.kt
+++ src/main/kotlin/racingcar/model/Race.kt
@@ -1,6 +1,19 @@
 package racingcar.model
 
-class Race {
+import camp.nextstep.edu.missionutils.Randoms
+
+class Race(private val carList: List<Car>, private val rounds: Int) {
+    fun play() {
+        repeat(rounds) {
+            raceCars()
+        }
+    }
+
+    private fun raceCars() {
+        for (car in carList) if (shouldMove()) car.move()
+    }
+
+    private fun shouldMove() = Randoms.pickNumberInRange(0, 9) >= 4
 }
 
 data class Car(val name: String) {
