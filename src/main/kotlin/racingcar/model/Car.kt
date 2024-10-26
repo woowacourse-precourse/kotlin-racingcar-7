63ca01dd04488dc9b09627ada36de6a3685962af
Tnalxmsk <alstnwkd990@naver.com>
2024-10-26 19:13:42 +0900

    feat: 예외 처리 메세지 추가
    이름 길이 안내 메세지 추가

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,5 +1,6 @@
 package racingcar.model
 
+import racingcar.enum.ERROR_FORMAT
 import racingcar.enum.RacingRule.*
 import racingcar.utils.NumberGenerator
 
@@ -11,7 +12,9 @@ class Car(private val name: String) {
         get() = DISTANCE.value.repeat(distance)
 
     init {
-        require(name.isNotEmpty() && name.length <= MAX_NAME_LENGTH)
+        require(name.isNotEmpty() && name.length <= MAX_NAME_LENGTH) {
+            ERROR_FORMAT.format("이름 길이는 최소 1자 이상 최대 5자 이하 입니다.")
+        }
     }
 
     fun getName() = name
