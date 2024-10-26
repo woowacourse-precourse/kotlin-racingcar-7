c2984d8a133aeb616f846d57c4f104190b461530
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-26 22:40:40 +0900

    feat(OutputView): 출력 기능 추가
    - "실행 결과" 출력
    - 각 자동차들의 진행 상황 출력
    - 최종 우승자 출력

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,24 @@
+package racingcar.view
+
+import racingcar.model.RacingCar
+
+class OutputView {
+    fun printDefaultMessage() {
+        println(OutputMessage.DEFAULT_MESSAGE.message)
+    }
+
+    fun printResult(carList: List<RacingCar>) {
+        carList.map { println(it) } // 각 자동차의 상태 출력
+    }
+
+    fun printWinners(winners: List<RacingCar>) {
+        println(OutputMessage.WINNERS.message + winners.joinToString(", "))
+    }
+
+    enum class OutputMessage(
+        val message: String,
+    ) {
+        DEFAULT_MESSAGE("실행 결과"),
+        WINNERS("최종 우승자 : "),
+    }
+}
