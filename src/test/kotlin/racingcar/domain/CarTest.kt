7ed18e0d555568b5f62e7a39869d1273c66baafb
Heejun-0219 <heejun0219@gmail.com>
2024-10-26 16:49:48 +0900

    test: CarTest
    Car 도메인 클래스 단위 테스트 추가

---
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -0,0 +1,58 @@
+package racingcar.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.mockStatic
+
+class CarTest {
+    @Test
+    fun `자동차 이름 길이 비정상 테스트`() {
+        assertThrows<IllegalArgumentException> { Car("") }
+        assertThrows<IllegalArgumentException> { Car("javaji") }
+    }
+
+    @Test
+    fun `자동차 이동 테스트`() {
+        val car = Car("pobi")
+
+        val movedCar = car.move()
+
+        assert(car.position + 1 == movedCar.position)
+    }
+
+    @Test
+    fun `값 이상이면 전진한다`() {
+        mockStatic(Randoms::class.java).use { mockedStatic ->
+            // given
+            val car = Car("pobi")
+            mockedStatic
+                .`when`<Int> { Randoms.pickNumberInRange(0, 9) }
+                .thenReturn(4)
+
+            // when
+            val result = car.isMove()
+
+            // then
+            assertThat(result).isTrue()
+        }
+    }
+
+    @Test
+    fun `값 이하면 전진하지 않는다`() {
+        mockStatic(Randoms::class.java).use { mockedStatic ->
+            // given
+            val car = Car("pobi")
+            mockedStatic
+                .`when`<Int> { Randoms.pickNumberInRange(0, 9) }
+                .thenReturn(3)
+
+            // when
+            val result = car.isMove()
+
+            // then
+            assertThat(result).isFalse()
+        }
+    }
+}
