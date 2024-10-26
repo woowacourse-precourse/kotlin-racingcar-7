6fc3c53b489d8846f781ba3f78c6e42122d0c46a
changzin <changzin0126@gmail.com>
2024-10-26 15:07:25 +0900

    feat(main): 자동차 이름을 받고 검증, 필요한 데이터를 초기화하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,55 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import java.util.StringTokenizer
+
 fun main() {
     // TODO: 프로그램 구현
+    val input:String =  Console.readLine()
+
+    val carNameList: List<String> = makeCarNames(input)
+    var carScore: MutableList<Int> = mutableListOf()
+    var carString: MutableList<String> = mutableListOf()
+
+    initListData(carNameList, carScore, carString)
+
+    val totalCount:Int = makeInteger(Console.readLine())
+
+
+}
+
+fun makeCarNames(input: String): List<String>{
+    val arr: MutableList<String> = mutableListOf()
+    val st = StringTokenizer(input, ",")
+
+    while(st.hasMoreTokens()){
+        val carName = st.nextToken()
+        if (validateName(carName)){
+            arr.add(carName)
+        }
+    }
+    return arr
+}
+
+fun validateName(carName: String): Boolean{
+    if (carName.length > 5){
+        throw IllegalArgumentException()
+    }
+    return true
+}
+
+fun initListData(carNameList: List<String>, carScore: MutableList<Int>, carString: MutableList<String>){
+    for(name in carNameList){
+        carScore.add(0)
+        carString.add("")
+    }
+}
+
+fun makeInteger(totalCount: String): Int{
+    try{
+        return totalCount.toInt()
+    }
+    catch (e: NumberFormatException){
+        throw IllegalArgumentException()
+    }
 }
