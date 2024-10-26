c55aa0daf2027931bb3bd9201b8683742c756d38
rkqls4764 <rkqls4764@naver.com>
2024-10-26 22:10:33 +0900

    feat: 자동차 이름 입력 함수 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -14,3 +14,18 @@ data class Car(
 fun makeCar(name: String): Car {
     return Car(name, "")
 }
+
+/* 자동차 이름을 입력 받는 함수 */
+fun inputName(carList: MutableList<Car>) {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val names = readLine().toString().split(",")
+
+    for (name in names) {
+        if (name.length <= 5) { //이름이 5자 이하면 자동차 객체 생성
+            carList.add(makeCar(name))
+        }
+        else { //5자 초과이면 IllegalArgumentException 발생, 애플리케이션 종료
+            throw IllegalArgumentException("이름은 5자 이하여야 합니다.")
+        }
+    }
+}
