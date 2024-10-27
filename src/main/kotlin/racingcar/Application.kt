2739b60f2645605228caf60065763323b048fa6d
yoon <cbj0010@naver.com>
2024-10-28 01:19:44 +0900

    feat : 시작 전
    시작 문구 출력 및 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
-    // TODO: 프로그램 구현
+//    randomValue(3)
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val player = Console.readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    val playNum = Console.readLine()
+
+
 }
+
