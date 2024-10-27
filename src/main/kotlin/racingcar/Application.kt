8c309217bab7d120abb9eed9281b5e05e1974036
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-10-27 20:42:27 +0900

    feat: 입력, 이름 임력 및 시도횟수 구현
    1. 입력 자동차 이름 기능 추가했습니다.
    - 2개 이상 16개 이하의 이름 입력 가능
    - 5글자 이하의 입력을 받음
    - 중복된 이름은 "이름_0", "이름_1"과 같이 이름 수정
    2. 시도할 횟수 기능 추가했습니다.
    - 최대 16이하의 자연수 입력 가능
    - 정수가 아닌 실수 오류처리
    - 빈값 오류처리
    - 문자 혹은 문자열이 포함되면 오류처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,47 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    // n대의 자동차 이름 입력
+    val input = Console.readLine().trim()
+    // 쉼표로 구분
+    val nameList = input.split(",")
+
+    // n은 2~16사이
+    if (nameList.size !in 2..16) throw IllegalArgumentException("최소 2개에서 최대 16개의 이름을 입력해주세요.")
+
+    // 5글자로 제한
+    for (name in nameList) {
+        if (name.length !in 1..5) throw IllegalArgumentException("이름은 최소 한글자에서 최대 5글자로 입력해주세요.")
+    }
+
+    // 중복 이름 구분
+    val names = nameList
+        .groupingBy { it }
+        .eachCount()
+        .flatMap { (key, count) ->
+            if (count > 1) {
+                List(count) { i -> "${key}_$i" }
+            } else {
+                listOf(key)
+            }
+        }
+        .joinToString(",")
+
+    println("시도할 횟수는 몇 회인가요?")
+    val stingTypeTryCountInput = Console.readLine().trim()
+
+    // 정수가 아닌 입력에 대한 오류처리
+    val test = stingTypeTryCountInput.toIntOrNull() == null
+    if (test) throw IllegalArgumentException("시도할 횟수는 정수를 입력해주세요.")
+
+    // 횟수 제한 범위가 넘어가는 오류
+    val tryCountInput = stingTypeTryCountInput.toInt()
+    val countRange = tryCountInput in 1..15
+    if (!countRange) throw IllegalArgumentException("시도할 횟수는 최소 1에서 16의 자연수로 입력해주세요.")
+
+    println(names)
+    println("$tryCountInput")
 }
