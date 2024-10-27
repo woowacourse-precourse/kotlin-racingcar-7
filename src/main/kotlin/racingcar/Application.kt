1db2e68578bb9f13cb50afa4c8810a8c8e53557e
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-10-27 21:49:35 +0900

    feat: 출력 기능 추가
    1. 기능 목록 출력 차수별 실행결과 기능 구현
    - 각 라운드별 결과 반환
    2. 우승자 안내 문구 구현
    - 최종 우승자는 자동차의 위치가 가장 먼 모든 자동차들

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,13 +1,14 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     // n대의 자동차 이름 입력
     val input = Console.readLine().trim()
     // 쉼표로 구분
-    val nameList = input.split(",")
+    var nameList = input.split(",")
 
     // n은 2~16사이
     if (nameList.size !in 2..16) throw IllegalArgumentException("최소 2개에서 최대 16개의 이름을 입력해주세요.")
@@ -18,7 +19,7 @@ fun main() {
     }
 
     // 중복 이름 구분
-    val names = nameList
+    nameList = nameList
         .groupingBy { it }
         .eachCount()
         .flatMap { (key, count) ->
@@ -28,8 +29,8 @@ fun main() {
                 listOf(key)
             }
         }
-        .joinToString(",")
 
+    // 시도 횟수 입력
     println("시도할 횟수는 몇 회인가요?")
     val stingTypeTryCountInput = Console.readLine().trim()
 
@@ -42,6 +43,35 @@ fun main() {
     val countRange = tryCountInput in 1..15
     if (!countRange) throw IllegalArgumentException("시도할 횟수는 최소 1에서 16의 자연수로 입력해주세요.")
 
-    println(names)
-    println("$tryCountInput")
+    // 실행 결과 출력
+    println("\n실행 결과")
+
+    // 이름과 현재 위치 매핑
+    var carWithCount = nameList
+        .groupBy { it }
+        .map { (carName, _) -> carName to 0 }
+
+    // 레이싱 시작
+    repeat(tryCountInput) {
+        // 난수조건에 따라 위치 변경
+        carWithCount = carWithCount.map { (carName, carLocation) ->
+            val isGoOneStep = Randoms.pickNumberInRange(0, 9) >= 4
+            if (isGoOneStep) carName to carLocation + 1 else carName to carLocation
+        }
+
+        // 각 라운드 결과 출력
+        for (car in carWithCount) {
+            val result = "-".repeat(car.second)
+            println("${car.first} : $result")
+        }
+
+        println()
+    }
+
+    // 최종 우승자 산출
+    val maxCount = carWithCount.maxOf { it.second }
+    val winners = carWithCount.filter { it.second == maxCount }.joinToString(", ") { it.first }
+
+    // 우승자 출력
+    println("최종 우승자 : $winners")
 }
