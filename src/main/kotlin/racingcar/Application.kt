a1e6f00309fd1568d2d820f13abe503b7ee3596f
eunyoung kim <kermit.eea@gmail.com>
2024-10-27 14:19:16 +0900

    feat: CarRacingGame 클래스 기본 구조 구현, Car 클래스 구현
    - 자동차 객체 리스트와 시도 횟수 변수 정의
    - CarRacingGame 클래스에 startRace 메서드 및 주요 메서드 스켈레톤 추가
    - Car 클래스 필드 구현, 전진 여부를 결정하는 shouldMove 메서드와 전진 로직을 담당하는 moveForwardIfPossible 메서드 구현
    - README 파일에 raceRound와 startRace 메서드 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,50 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    CarRacingGame().startRace()
+}
+
+class CarRacingGame {
+    private lateinit var cars: List<Car>
+    private var attemptCount: Int = 0
+
+    fun startRace() {
+        // cars에 getCarNames()으로 자동차 이름 입력 받기
+
+        // attemptCount에 getAttemptCount()으로 시도 횟수 입력 받기
+
+        println("실행 결과")
+        // attemptCount만큼 반복하면서
+        // 자동자 객체 별로 shouldMove와 moveForwardIfPossible 실행 -> printRaceStatus 출력
+        repeat(attemptCount) {
+            // raceRound()
+            // printRaceStatus()
+        }
+        // printWinners()
+    }
+
+    // getCarNames()
+
+    // getAttemptCount()
+
+    // raceRound()
+
+    // printRaceStatus()
+
+    // printWinners()
+}
+
+class Car(val name: String) {
+    var distance = 0
+        private set
+
+    fun moveForwardIfPossible() {
+        if (shouldMove()) {
+            distance++
+        }
+    }
+
+    private fun shouldMove(): Boolean = Randoms.pickNumberInRange(0, 9) >= 4
 }
