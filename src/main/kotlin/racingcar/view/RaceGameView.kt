6fa2fb0c97f49afb8dd3d180c68e272dccfa5348
noveril <pitterpatterpik@gmail.com>
2024-10-27 15:13:12 +0900

    feat: Implement printRaceGameResults method for displaying results
    Add the printRaceGameResults method in RaceGameView to output the results of the race.
    This method prints a header followed by each car's name and position, formatted for clarity.

--- src/main/kotlin/racingcar/view/RaceGameView.kt
+++ src/main/kotlin/racingcar/view/RaceGameView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.controller.dto.RaceGameResultDto
 
 const val CAR_NAMES_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val NOT_FOUND_INPUT_MESSAGE = "입력값이 비어있습니다. 다시 입력해주세요."
@@ -8,8 +9,12 @@ const val TRY_COUNT_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
 const val INVALID_TRY_COUNT_MESSAGE = "시도 횟수는 1 이상의 값이어야 합니다."
 const val CAR_NAMES_REGEX = "^(\\w+,)+\\w+$"
 const val INVALID_CAR_NAME_CHAR_MESSAGE = "자동차 이름에 쉼표를 제외한 다른 특수문자가 포함될 수 없습니다. 다시 입력해주세요."
+const val RESULT_MESSAGE = "실행 결과"
+const val TRACE = "-"
 
 class RaceGameView {
+    private var isFirstResultDisplayed: Boolean = true
+
     fun inputCarNames(): String {
         println(CAR_NAMES_PROMPT_MESSAGE)
         val carNames: String = readLine()
@@ -29,6 +34,12 @@ class RaceGameView {
         validateNumeric(parsedTryCount)
 
         return parsedTryCount
+    }
+
+    fun printRaceGameResults(raceResults: List<RaceGameResultDto>) {
+        printResultHeader()
+        raceResults.forEach { result -> println(display(result.carName, result.position)) }
+        println()
     }
 
     private fun readLine(): String {
@@ -51,5 +62,16 @@ class RaceGameView {
         if (input <= 0) {
             throw IllegalArgumentException(INVALID_TRY_COUNT_MESSAGE)
         }
+    }
+
+    private fun printResultHeader() {
+        if (isFirstResultDisplayed) {
+            println(RESULT_MESSAGE)
+            isFirstResultDisplayed = false
+        }
+    }
+
+    private fun display(name: String, position: Int): String {
+        return "$name : ${TRACE.repeat(position)}"
     }
 }
