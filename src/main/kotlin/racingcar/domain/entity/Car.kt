8623974a962faf106fc84d88cf12dfa3fc4324de
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-27 21:00:17 +0900

    refactor: 최소 최대 값이 포함된 상수 수정
    - 최소 최대 값이 대상 숫자의 앞에 오도록 수정
    
    상수명 형식을 공통되도록 변경해서 가독성 향상

--- src/main/kotlin/racingcar/domain/entity/Car.kt
+++ src/main/kotlin/racingcar/domain/entity/Car.kt
@@ -12,8 +12,8 @@ data class Car(
     }
 
     private fun validateTryNumberRange(tryNumber: Int) {
-        require(tryNumber >= MIN_TRY_NUMBER) { println(INVALID_TRY_NUMBER_MIN_ERROR) }
-        require(tryNumber <= MAX_TRY_NUMBER) { println(INVALID_TRY_NUMBER_MAX_ERROR) }
+        require(tryNumber >= MIN_TRY_NUMBER) { println(INVALID_MIN_TRY_NUMBER_ERROR) }
+        require(tryNumber <= MAX_TRY_NUMBER) { println(INVALID_MAX_TRY_NUMBER_ERROR) }
     }
 
     companion object {
@@ -21,7 +21,7 @@ data class Car(
         private const val MOVE_NUMBER_LIMIT = 4
         private const val MAX_TRY_NUMBER = 9
         private const val MIN_TRY_NUMBER = 0
-        private const val INVALID_TRY_NUMBER_MIN_ERROR = "이동에 사용하는 숫자는 0이상 숫자입니다."
-        private const val INVALID_TRY_NUMBER_MAX_ERROR = "이동에 사용하는 숫자는 9이하 숫자입니다 "
+        private const val INVALID_MIN_TRY_NUMBER_ERROR = "이동에 사용하는 숫자는 0이상 숫자입니다."
+        private const val INVALID_MAX_TRY_NUMBER_ERROR = "이동에 사용하는 숫자는 9이하 숫자입니다 "
     }
 }
--- src/main/kotlin/racingcar/domain/generator/RaceRandomGenerator.kt
+++ src/main/kotlin/racingcar/domain/generator/RaceRandomGenerator.kt
@@ -3,10 +3,10 @@ package racingcar.domain.generator
 import camp.nextstep.edu.missionutils.Randoms
 
 class RaceRandomGenerator : NumberGenerator {
-    override fun generate() = Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER)
+    override fun generate() = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
 
     companion object{
-        private const val RANDOM_MIN_NUMBER = 0
-        private const val RANDOM_MAX_NUMBER = 9
+        private const val MIN_RANDOM_NUMBER = 0
+        private const val MAX_RANDOM_NUMBER = 9
     }
 }
