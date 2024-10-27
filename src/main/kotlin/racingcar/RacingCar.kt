6fa83be26eef3beff35ce4599d0c33027fac589b
minseok0416 <day5274567@gmail.com>
2024-10-28 04:22:32 +0900

    feat(RacingCar): RacingCar 및 RacingCarTest 추가

---
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -0,0 +1,5 @@
+package racingcar
+
+class RacingCar(val name: String, val number: String) {
+
+}
---
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -0,0 +1,16 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class RacingCarTest {
+	@Test
+	fun `자동차 이름과 횟수를 입력받는다`() {
+		val inputName = "pobi,woni,jun"
+		val inputRound = "5"
+		val racingCar = RacingCar(inputName, inputRound)
+		assertThat(racingCar.name).isEqualTo("pobi,woni,jun")
+		assertThat(racingCar.number).isEqualTo("5")
+	}
+}
