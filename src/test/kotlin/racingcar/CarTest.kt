3522b861e912b45b30fd122c698ee07bccc62fd2
jklom0326 <jklom0326@gmail.com>
2024-10-28 00:39:35 +0900

    test: 자동차 전진 테스트 (이동 가능)

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,5 +1,18 @@
 package racingcar
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
 class CarTest {
     // 예외 처리 테스트
+    private val car = Car("car")
+
+    @Test
+    fun `자동차 전진 테스트`() {
+        car.moving(4)
+
+        assertEquals(1, car.position)
+    }
 }
