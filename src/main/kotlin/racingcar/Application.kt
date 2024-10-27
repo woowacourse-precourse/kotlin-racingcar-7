029b1ddc3d861709ddeef655d90f4ca4938adb4d
dioo1461 <dioo1461@gmail.com>
2024-10-27 22:13:25 +0900

    feat(Race.kt): 경주의 과정 및 결과를 출력하는 기능 구현
    기능 구현 사항 중 "4. 우승자 계산 및 출력"을 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,7 @@
 package racingcar
 
 fun main() {
-
+    val inputs = UserInput.request()
+    val race = Race(inputs.carNameList, inputs.rounds)
+    race.run()
 }
--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -8,11 +8,20 @@ const val RANDOM_TARGET_NUM = 4
 
 const val UNIT_DISTANCE: Long = 1
 
+const val RACE_RESULT_HEADER_MESSAGE = "실행 결과"
+const val RACE_RESULT_FINAL_WINNER_MESSAGE_PREFIX = "최종 우승자 : "
+
 class Race(private val carNameList: Iterable<String>, private val rounds: Long) {
     private val carList: List<Car> = carNameList.map { Car(it) }
 
     fun run() {
-
+        println(RACE_RESULT_HEADER_MESSAGE)
+        for (i in 0..rounds) {
+            executeRound()
+            printRoundResult()
+        }
+        print(RACE_RESULT_FINAL_WINNER_MESSAGE_PREFIX)
+        print(calculateFinalWinners().joinToString(", ") { it.name })
     }
 
     private fun executeRound() {
@@ -22,11 +31,29 @@ class Race(private val carNameList: Iterable<String>, private val rounds: Long) 
     }
 
     private fun tryToMoveCarForward(car: Car, distance: Long): Boolean {
-        if (pickNumberInRange(RANDOM_START_NUM, RANDOM_END_NUM) <= RANDOM_TARGET_NUM) {
+        if (pickNumberInRange(RANDOM_START_NUM, RANDOM_END_NUM) >= RANDOM_TARGET_NUM) {
             car.moveForward(distance)
             return true
         }
         return false
+    }
+
+    private fun printRoundResult() {
+        for (car in carList) {
+            print(car.name)
+            print(" : ")
+            print(convertDistanceToDash(car.distance))
+            print("\n")
+        }
+        print("\n")
+    }
+
+    private fun convertDistanceToDash(distance: Long): String {
+        val chars = mutableListOf<Char>()
+        for (i in 0..<distance) {
+            chars.add('-')
+        }
+        return chars.joinToString("")
     }
 
     private fun calculateFinalWinners(): Iterable<Car> {
