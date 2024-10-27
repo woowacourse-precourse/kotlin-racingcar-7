463577fec11277dd636465761dd850f4d9961d66
dioo1461 <dioo1461@gmail.com>
2024-10-27 20:53:04 +0900

    refactor(Car.kt): tryToMoveForward 메서드를 제거, name의 접근 제한을 public으로 확장
    - tryToMoveForward 메서드를 제거
    > 자동차를 확률적으로 전진시키는 것은 경주에 대한 규칙인데, 이를 Car 클래스에 구현하면 자동차-경주 사이에 redundant한 의존성이 발생한다고 판단하였음
    
    - name의 접근 제한을 public으로 확장
    > car의 name을 외부에서도 get할 수 있도록 수정

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,24 +1,10 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
-
-class Car(private val name: String){
+class Car(val name: String){
     var distance = 0L
         private set
 
-    /**
-     * 자동차를 확률적으로 distance만큼 전진시킵니다.
-     *
-     * @param distance 전진시킬 거리
-     * */
-    fun tryToMoveForward(distance: Long) {
-        if (pickNumberInRange(0,9) >= 4) {
-            moveForward(distance)
-        }
-    }
-
-    private fun moveForward(distance: Long) {
+    fun moveForward(distance: Long) {
         this.distance += distance
     }
 }
--- src/test/kotlin/racingcar/unitTest/CarTest.kt
+++ src/test/kotlin/racingcar/unitTest/CarTest.kt
@@ -1,37 +1,28 @@
-package racingcar.unitTest
+package racingcar
+
 import camp.nextstep.edu.missionutils.test.Assertions
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import racingcar.Car
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 class CarTest : NsTest() {
     @Test
     fun `전진 기능 테스트`() {
-        Assertions.assertRandomNumberInRangeTest(
-            {
-                run()
-                assertThat(output()).contains("1")
-            },
-            MOVING_FORWARD
-        )
-        Assertions.assertRandomNumberInRangeTest(
-            {
-                run()
-                assertThat(output()).contains("0")
-            },
-            STOP
-        )
+        Assertions.assertSimpleTest {
+            run("car", "1")
+            assertThat(output()).contains("car", "1")
+        }
+
     }
 
     override fun runMain() {
-        val car = Car("car")
-        car.tryToMoveForward(1)
+        val car = Car(readLine())
+        val distance = readLine().toLong()
+        car.moveForward(distance)
+        println(car.name)
         println(car.distance)
-    }
-
-    companion object {
-        private const val MOVING_FORWARD: Int = 4
-        private const val STOP: Int = 3
     }
 }
