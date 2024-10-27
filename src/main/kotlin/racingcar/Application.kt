00698c2af5241bc753e6f64a3859ade3b810f8c8
DoableTuple <doabletuple@gmail.com>
2024-10-28 02:14:37 +0900

    refactor: 입력 기능을 InputView class로 분리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,53 +1,9 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
+import racingcar.view.InputView
 
 fun main() {
-    fun splitNamesByDelimiter(input: String): List<String> {
-        return input.split(",")
-    }
-
-    fun validateNameNotNull(cars: List<String>) {
-        cars.forEach {
-            if (it.isEmpty()) throw IllegalArgumentException("자동차의 이름은 비어있을 수 없습니다.")
-        }
-    }
-
-    fun validateNameLength(cars: List<String>) {
-        cars.forEach {
-            if (it.length > 5) throw IllegalArgumentException("자동차의 이름은 최대 5자입니다.")
-        }
-    }
-
-    fun validateNameUniqueness(cars: List<String>) {
-        if (cars.toSet().size < cars.size) throw IllegalArgumentException("중복된 자동차 이름이 있습니다.")
-    }
-
-    fun readCarNames() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        val input = splitNamesByDelimiter(Console.readLine())
-        validateNameNotNull(input)
-        validateNameLength(input)
-        validateNameUniqueness(input)
-    }
-
-
-    fun validateCountIsNumeric(countInput: String) {
-        if (countInput.toIntOrNull() == null) throw IllegalArgumentException("올바른 숫자가 아닙니다.")
-    }
-
-    fun validateCountIsPositive(countInput: String) {
-        if (countInput.toInt() < 1) throw IllegalArgumentException("시행 횟수는 1 이상이어야 합니다.")
-    }
-
-    fun readTrialCount() {
-        println("시도할 횟수는 몇 회인가요?")
-        val input = Console.readLine()
-        validateCountIsNumeric(input)
-        validateCountIsPositive(input)
-    }
-
-
-    println(readCarNames())
-    println(readTrialCount())
+    val inputView = InputView()
+    inputView.readCarNames()
+    inputView.readTrialCount()
 }
