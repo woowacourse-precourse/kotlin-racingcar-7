48003af91abe533239be91f8e42c7c828d1dd186
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-27 18:55:21 +0900

    feat(Car): 이동을 시도하는 숫자 범위 예외 처리
    - 이동을 시도하는 숫자가 0~9가 아닐 경우에 예외가 발생하는 기능 추가

--- src/main/kotlin/racingcar/domain/entity/Car.kt
+++ src/main/kotlin/racingcar/domain/entity/Car.kt
@@ -7,11 +7,20 @@ data class Car(
     val position get() = _position
 
     fun tryMove(tryNumber: Int) {
+        validateTryNumber(tryNumber)
         if (tryNumber >= MOVE_NUMBER_LIMIT) _position++
+    }
+
+    private fun validateTryNumber(tryNumber: Int) {
+        val numberRange = MIN_TRY_NUMBER..MAX_TRY_NUMBER
+        require(numberRange.contains(tryNumber)) { println(INVALID_TRY_NUMBER_RANGE_ERROR) }
     }
 
     companion object {
         private const val INIT_POSITION = 0
         private const val MOVE_NUMBER_LIMIT = 4
+        private const val MAX_TRY_NUMBER = 9
+        private const val MIN_TRY_NUMBER = 0
+        private const val INVALID_TRY_NUMBER_RANGE_ERROR = "이동에 사용하는 숫자는 0~9사이의 정수입니다."
     }
 }
