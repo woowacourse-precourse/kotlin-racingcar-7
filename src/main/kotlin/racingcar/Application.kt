49945a971a365611cbe0dc5c7d9e487dc39774bd
minseok0416 <day5274567@gmail.com>
2024-10-28 04:34:27 +0900

    chore: use tab character 해제

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,10 +3,10 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-	println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-	val inputName = Console.readLine()
-	println("시도할 횟수는 몇 회인가요?")
-	val inputNumber = Console.readLine()
-	println("\n실행 결과")
-	//TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val inputName = Console.readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    val inputNumber = Console.readLine()
+    println("\n실행 결과")
+    //TODO: 프로그램 구현
 }
