fb551ab4ae28d2830c12cd90e8471f65b3b7fbcd
eden <devfeijoa@gmail.com>
2024-10-27 23:39:12 +0900

    feat: 입력받는 자동차 이름이 유효한지 확인하는 기능

--- src/main/kotlin/racingcar/Run.kt
+++ src/main/kotlin/racingcar/Run.kt
@@ -7,14 +7,21 @@ class Run {
 
     }
 
-    fun generateCars(): List<String> {
+    fun generateCars():List<String> {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val carNames = Console.readLine().split(",").map { it.trim() }
         return carNames
     }
 
-    fun getAttemptTimes(): Int {
+    fun getAttemptTimes():Int {
         val attemptTimes = Console.readLine().toInt()
         return attemptTimes
+    }
+
+    fun isValidCarNames(carNames:String ):Boolean {
+        if(carNames.isBlank()) throw IllegalArgumentException ("이름을 입력해 주세요.")
+        if(carNames.isEmpty()) throw IllegalArgumentException ("이름을 입력해 주세요.")
+        if( carNames.length < 1 && carNames.length > 5) throw IllegalArgumentException ("이름은 1자 이상 5자 이하로 입력해 주세요.")
+        return true
     }
 }
