d9416635ea2b115457d67575e9f02fcc55527ab9
eden <devfeijoa@gmail.com>
2024-10-27 22:53:01 +0900

    feat: 시도할 횟수를  입력받는 기능

--- src/main/kotlin/racingcar/Run.kt
+++ src/main/kotlin/racingcar/Run.kt
@@ -4,11 +4,17 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 class Run {
     fun inputData(){
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 
     }
+
     fun generateCars(): List<String> {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val carNames = Console.readLine().split(",").map { it.trim() }
         return carNames
+    }
+
+    fun getAttemptTimes(): Int {
+        val attemptTimes = Console.readLine().toInt()
+        return attemptTimes
     }
 }
