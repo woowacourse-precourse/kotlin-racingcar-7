56edee152b2421b3ba7c6bc753fcda3dba96e498
DoableTuple <doabletuple@gmail.com>
2024-10-28 01:16:34 +0900

    feat: 쉼표를 기준으로 자동차의 이름을 나누는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,10 +3,15 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    fun readCarNames(): String {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        return Console.readLine()
+    fun splitNamesByDelimiter(input: String): List<String> {
+        return input.split(",")
     }
+
+    fun readCarNames() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val input = splitNamesByDelimiter(Console.readLine())
+    }
+
 
     fun readTrialCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
