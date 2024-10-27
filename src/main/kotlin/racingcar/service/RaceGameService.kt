d71f95b456a4288fb06a49d1f12173b16c9e0551
noveril <pitterpatterpik@gmail.com>
2024-10-27 15:01:53 +0900

    feat: Implement race game service for car setup
    Create the RaceGameService to manage the setup of cars in the race.
    This includes parsing car names from a string and validating that each name does not exceed the maximum length.
    The service returns a list of Car objects, ready for use in the game.

---
+++ src/main/kotlin/racingcar/service/RaceGameService.kt
@@ -0,0 +1,20 @@
+package racingcar.service
+
+import racingcar.model.Car
+import racingcar.strategy.MoveStrategy
+
+const val CAR_NAMES_SEPARATOR = ","
+const val CAR_NAME_MAX_LENGTH = 5
+const val INVALID_CAR_NAME_LENGTH_MESSAGE = "자동차 이름은 5자 이하만 가능합니다."
+
+class RaceGameService(private val randomMoveStrategy: MoveStrategy) {
+    fun setupCars(carNames: String): List<Car> {
+        val cars: List<Car> = carNames.trim().split(CAR_NAMES_SEPARATOR).map { name -> Car(name) }
+
+        if (cars.any { it.name.length > CAR_NAME_MAX_LENGTH }) {
+            throw IllegalArgumentException(INVALID_CAR_NAME_LENGTH_MESSAGE)
+        }
+
+        return cars
+    }
+}
