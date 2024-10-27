f6a07d162ae4dbeca49dd19af062b6def37a219c
DDangDin <Kcorelec7123@gmail.com>
2024-10-27 20:25:47 +0900

    feat: 자동차 클래스에 행위 부여
    - 자동차 객체는 전진하거나 멈출 수 있지만 실질적으로 관여하는 메서드는 1개이다.
    (전진할 때 메서드: moveForward, 멈출 때: 아무 작업 X)
    - 자동차 클래스에서 moveForwardAndStop 메서드는 인자 값을 판단하여 전진하거나 멈추거나 결정한다.
    - 기타 사항1: 자동차 이름 5자 이상일 때 예외처리
    - 기타 사항2: README.md 자동차 행위 기능 업데이트

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -3,8 +3,21 @@ package racingcar
 class Car(
     val name: String,
 ) {
+    var forwardCount = 0
+        private set
+
     init {
         name.validateCarNaming()
+    }
+
+    fun moveForwardAndStop(randomNumber: Int) {
+        if (randomNumber.canMoveForward()) {
+            moveForward()
+        }
+    }
+
+    private fun moveForward() {
+        forwardCount++
     }
 
     private fun String.validateCarNaming() {
@@ -12,7 +25,10 @@ class Car(
             throw IllegalArgumentException(CAR_NAME_MAX_LENGTH_EXCEPTION)
         }
     }
+
+    private fun Int.canMoveForward(): Boolean = this <= MIN_VALUE_TO_MOVE_CAR
 }
 
 private const val CAR_NAME_MAX_LENGTH = 5
 private const val CAR_NAME_MAX_LENGTH_EXCEPTION = "자동차 이름은 5자 이하만 가능 합니다."
+private const val MIN_VALUE_TO_MOVE_CAR = 4
