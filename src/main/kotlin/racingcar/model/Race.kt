eec8b087b5abdce7e002cd1500e40d68e25f0e1a
jburnoo <6pizz@naver.com>
2024-10-27 17:47:41 +0900

    feat: 우승자 리스트 반환 구현

--- src/main/kotlin/racingcar/model/Race.kt
+++ src/main/kotlin/racingcar/model/Race.kt
@@ -3,9 +3,14 @@ package racingcar.model
 import camp.nextstep.edu.missionutils.Randoms
 
 class Race(private val carList: List<Car>) {
-    fun getResult(): List<Car> {
+    fun getRaceResult(): List<Car> {
         moveCar()
         return carList
+    }
+
+    fun getRaceWinner(): List<String> {
+        val maxMoves = carList.maxOf { it.moves }
+        return carList.filter { it.moves == maxMoves }.map { it.name }
     }
 
     private fun moveCar() {
