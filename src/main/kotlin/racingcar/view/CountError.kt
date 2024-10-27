08029806f673729c0d544a2eb4bdbf0f7e12d386
unknown <yangjk9203@naver.com>
2024-10-27 19:09:58 +0900

    style(CountError): 변수명 수정

--- src/main/kotlin/racingcar/view/CountError.kt
+++ src/main/kotlin/racingcar/view/CountError.kt
@@ -2,7 +2,8 @@ package racingcar.view
 
 class CountError {
 
-    val countBlank = "횟수를 입력하지 않았습니다."
-    val zeroInput = "0회는 할 수 없습니다."
-    val countIncludeString = "숫자만 입력 가능합니다."
+    val EMPTY_COUNT = "횟수를 입력하지 않았습니다."
+    val ZERO_INPUT = "0회는 할 수 없습니다."
+    val INCLUDED_STRING = "숫자만 입력 가능합니다."
+
 }
