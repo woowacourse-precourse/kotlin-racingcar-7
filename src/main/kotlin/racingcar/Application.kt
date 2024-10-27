e65c31ca5679efe83ef9e63cb6159363dfd1139f
DDangDin <Kcorelec7123@gmail.com>
2024-10-27 18:29:59 +0900

    feat: 시도할 횟수 입력 받는 기능 구현
    - 시도할 횟수를 입력 받고 정수형으로 변환

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,5 +6,8 @@ fun main() {
     println(RacingCarText.INPUT_CAR_NAME)
     val carsText = readLine()
     val cars = carsText.split(RacingCarDelimiters.NAME_DELIMITER)
-    println(cars)
+    println(RacingCarText.INPUT_TRY_COUNT)
+    val tryCountText = readLine().toInt()
+
+    println("$cars, $tryCountText")
 }
--- src/main/kotlin/racingcar/RacingCarText.kt
+++ src/main/kotlin/racingcar/RacingCarText.kt
@@ -4,4 +4,7 @@ package racingcar
 object RacingCarText {
     /** 자동차 이름 입력 텍스트 */
     const val INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+
+    /** 시도할 횟수 입력 텍스트 */
+    const val INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?"
 }
