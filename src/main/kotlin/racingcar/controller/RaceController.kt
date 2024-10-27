708d1e4995568867f82a7b19cea9d501c3fa3f5d
jburnoo <6pizz@naver.com>
2024-10-28 02:09:01 +0900

    ref(RaceController): 생성자 주입 변경 및 유효성 검증 로직 분리

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,15 +1,16 @@
 package racingcar.controller
 
-import racingcar.model.Car
 import racingcar.model.Race
+import racingcar.util.InputValidator
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
-class RaceController {
-    private val inputView = InputView()
-    private val outputView = OutputView()
+class RaceController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(),
+    private val validator: InputValidator = InputValidator(),
+) {
     private lateinit var race: Race
-    private var rounds = 0
 
     fun run() {
         setUp()
@@ -20,56 +21,23 @@ class RaceController {
     }
 
     private fun setUp() {
-        val carList = getValidCarList()
-        rounds = getValidRounds()
-        race = Race(carList)
+        val carNamesInput = inputView.getCarNames()
+        val roundsInput = inputView.getRounds()
+
+        val carList = validator.getValidCarList(carNamesInput)
+        val rounds = validator.getValidRounds(roundsInput)
+
+        race = Race(carList, rounds)
     }
 
     private fun showRoundResult() {
-        repeat(rounds) {
-            val roundResult = race.getRoundResult()
-            outputView.printRoundResult(roundResult)
+        race.play { carList ->
+            outputView.printRoundResult(carList)
         }
     }
 
     private fun showWinnerResult() {
         val raceWinner = race.getRaceWinner()
         outputView.printWinner(raceWinner)
-    }
-
-    private fun getValidCarList(): List<Car> {
-        val carNames: String = inputView.getCarNames()
-        val tmpCarList = carNames.split(",")
-
-        isAllValid(tmpCarList)
-
-        return tmpCarList.map { carName -> Car(name = carName) }
-    }
-
-    private fun isAllValid(tmpCarList: List<String>) {
-        tmpCarList.map { carName ->
-            isValidCarNameLength(carName)
-            hasBlankInCarName(carName)
-        }
-        hasDuplicatedCarName(tmpCarList)
-    }
-
-    private fun isValidCarNameLength(carName: String) {
-        val len = carName.length
-        if (1 > len || len > 5) throw IllegalArgumentException("차량 이름의 길이는 1-5자 입니다.")
-    }
-
-    private fun hasBlankInCarName(carName: String) {
-        if (carName.contains(" ")) throw IllegalArgumentException("차량 이름에 공백을 제거해 주세요.")
-    }
-
-    private fun hasDuplicatedCarName(carNameList: List<String>) {
-        if (carNameList.toSet().size != carNameList.size) throw IllegalArgumentException("차량 이름은 중복될 수 없습니다.")
-    }
-
-    private fun getValidRounds(): Int {
-        val rounds = inputView.getRounds()
-        if (rounds < 1) throw IllegalArgumentException("1 이상의 횟수를 입력해 주세요.")
-        return rounds
     }
 }
