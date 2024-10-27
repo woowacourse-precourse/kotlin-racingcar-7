bef44b08716ba34cde1dcb185e2d66db16141f5f
nonani <cjw1234577@gmail.com>
2024-10-27 23:17:10 +0900

    feat: MVC 패턴 적용하여 Domain과 View 구현

---
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -0,0 +1,14 @@
+package racingcar.domain
+
+class Race(val cars: List<Car>, val numberOfRounds: Int) {
+    fun raceRound(randomValueProvider: () -> Int) {
+        cars.forEach { car ->
+            if (randomValueProvider() >= 4) car.move()
+        }
+    }
+
+    fun findWinners(): List<String> {
+        val maxPosition = cars.maxOf { it.position }
+        return cars.filter { it.position == maxPosition }.map { it.name }
+    }
+}
---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,14 @@
+package racingcar.view
+
+import racingcar.domain.Car
+
+object OutputView {
+    fun printRoundResult(cars: List<Car>) {
+        cars.forEach { car -> println("${car.name} : ${"-".repeat(car.position)}") }
+        println()
+    }
+
+    fun printWinners(winners: List<String>) {
+        println("최종 우승자 : ${winners.joinToString(", ")}")
+    }
+}
