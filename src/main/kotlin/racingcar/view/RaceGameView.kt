c4c88f1692509b9cc0d724dd7ee442e734ca8194
noveril <pitterpatterpik@gmail.com>
2024-10-27 15:21:49 +0900

    feat: Implement printRaceGameWinners method for displaying winners
    Add the printRaceGameWinners method in RaceGameView to output the final winners of the race.

--- src/main/kotlin/racingcar/view/RaceGameView.kt
+++ src/main/kotlin/racingcar/view/RaceGameView.kt
@@ -2,6 +2,7 @@ package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
 import racingcar.controller.dto.RaceGameResultDto
+import racingcar.controller.dto.WinnerResultDto
 
 const val CAR_NAMES_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val NOT_FOUND_INPUT_MESSAGE = "입력값이 비어있습니다. 다시 입력해주세요."
@@ -11,6 +12,8 @@ const val CAR_NAMES_REGEX = "^(\\w+,)+\\w+$"
 const val INVALID_CAR_NAME_CHAR_MESSAGE = "자동차 이름에 쉼표를 제외한 다른 특수문자가 포함될 수 없습니다. 다시 입력해주세요."
 const val RESULT_MESSAGE = "실행 결과"
 const val TRACE = "-"
+const val WINNER_SEPARATOR = ", "
+const val WINNERS_MESSAGE = "최종 우승자 : "
 
 class RaceGameView {
     private var isFirstResultDisplayed: Boolean = true
@@ -40,6 +43,11 @@ class RaceGameView {
         printResultHeader()
         raceResults.forEach { result -> println(display(result.carName, result.position)) }
         println()
+    }
+
+    fun printRaceGameWinners(winnerResult: List<WinnerResultDto>) {
+        val winners = winnerResult.joinToString(separator = WINNER_SEPARATOR) { it.name }
+        println("$WINNERS_MESSAGE$winners")
     }
 
     private fun readLine(): String {
