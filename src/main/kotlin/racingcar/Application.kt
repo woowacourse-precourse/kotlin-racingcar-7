7a5007dff895d7ae34179d0901b4417f9178576f
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2024-10-28 00:11:44 +0900

    refactor: indent depth가 3을 넘지 않도록 race 함수를 분리
    - 무작위 숫자를 받아 4이상일 때 전진하는 randomMoveForward
    - 현재 위치를 출력하는 currentPosition
    - 입력 받은 횟수만큼 위의 두 함수를 실행하는 race

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -23,7 +23,7 @@ fun main() {
 
     println("실행 결과")
 
-    race(names, tryNumber.toInt())
+    winner(names, tryNumber.toInt())
 }
 
 fun validateName(name: String): String {
@@ -41,25 +41,42 @@ fun divideNames(names: String): List<String> {
     }
 }
 
-fun race(names: String, tryNumber: Int) {
+fun randomMoveForward(name: String, position: MutableMap<String, Int>) {
+    val randomValue = Randoms.pickNumberInRange(0, 9)
+    if (randomValue >= 4) {
+        position[name] = position[name]!! + 1
+    }
+}
+
+fun currentPosition(names: String, position: Map<String, Int>) {
+    val finalParticipants = divideNames(names)
+
+    finalParticipants.forEach { name ->
+        val currentPosition = "-".repeat(position[name]!!)
+        println("$name: $currentPosition")
+    }
+}
+
+fun race(names: String, tryNumber: Int): Map<String, Int> {
     val finalParticipants = divideNames(names)
     val position = finalParticipants.associateWith { 0 }.toMutableMap()
 
     for (trying in 1..tryNumber) {
         finalParticipants.forEach { name ->
-            val randomValue = Randoms.pickNumberInRange(0, 9)
-            if (randomValue >= 4) {
-                position[name] = position[name]!! + 1
-            }
-
-            val currentPosition = "-".repeat(position[name]!!)
-            println("$name: $currentPosition")
+            randomMoveForward(name, position)
         }
+        currentPosition(names, position)
         print("\n")
     }
+    return position
+}
 
+fun winner(names: String, tryNumber: Int) {
+    val position = race(names, tryNumber)
     val maxPosition = position.values.maxOrNull()
     val winners = position.filter {it.value == maxPosition}.keys
+
+    race(names, tryNumber)
 
     println("최종 우승자 : ${winners.joinToString(", ")}")
 }
