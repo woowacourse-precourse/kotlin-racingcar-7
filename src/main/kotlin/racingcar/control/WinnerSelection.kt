59695ab0ea27c5e28bad18a2d02688a7abb4a729
unknown <yangjk9203@naver.com>
2024-10-27 21:26:22 +0900

    feat(WinnerSelection): 우승자 선정 기능 추가

---
+++ src/main/kotlin/racingcar/control/WinnerSelection.kt
@@ -0,0 +1,9 @@
+package racingcar.control
+
+class WinnerSelection {
+
+    fun winnerSelection(raceOver:List<CarData>): String {
+        val winnerPoint = raceOver.maxByOrNull { it.point }?.point
+        return raceOver.filter { it.point == winnerPoint }.map { it.name }.joinToString()
+    }
+}
