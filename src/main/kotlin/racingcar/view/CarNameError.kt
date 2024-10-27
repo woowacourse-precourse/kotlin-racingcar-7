734ef3371e3b42cad68991aae4c03b5028c9e65b
unknown <yangjk9203@naver.com>
2024-10-27 19:11:30 +0900

    feat(CarNameError): 오류 메시지 추가 및 변수명 수정

--- src/main/kotlin/racingcar/view/CarNameError.kt
+++ src/main/kotlin/racingcar/view/CarNameError.kt
@@ -2,9 +2,13 @@ package racingcar.view
 
 class CarNameError {
 
-    val carNameBlank = "이름을 입력하지 않았습니다."
-    val onlyDelimiters = "구분자만 입력 되었습니다."
-    val carNameOverFive = "이름은 다섯글자를 초과할 수 없습니다."
-    val notCompetitor = "혼자 경주할 수 없습니다."
+    val EMPTY_NAME = "이름을 입력하지 않았습니다."
+    val ONLY_GAP = "공백만 입력할 수 없습니다."
+    val DELIMITER_INPUT = "구분자만 입력 되었습니다."
+    val CONTINUOUS_DELIMITER = "구분자는 연속 입력할 수 없습니다."
+    val START_AND_END_DELIMITER_INPUT = "시작과 끝 문자에 구분자를 입력할 수 없습니다."
+    val GAP_DELIMITER_INPUT = "공백만 있는 이름이 있습니다."
+    val EXCEEDED_CHARACTER_COUNT_ = "이름은 다섯글자를 초과할 수 없습니다."
+    val CANT_RACE_ALONE = "혼자 경주할 수 없습니다."
 
 }
