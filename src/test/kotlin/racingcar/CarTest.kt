1f7633c515b7808a16bbfbf2a90bec5fe86798ad
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-27 18:44:34 +0900

    test(Car): 이동에 사용하는 숫자가 0~9가 아닐 경우 예의 테스트 추가
    - 시도하는 숫자가 0~9가 아닐 경우 예의를 발생시키는 테스트 추가

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,26 +1,41 @@
 package racingcar
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import racingcar.domain.entity.Car
 
 class CarTest {
 
-    lateinit var car: Car
+    private lateinit var car: Car
 
-    @Test
-    fun `이동 성공 테스트`(){
+    @BeforeEach
+    fun setup() {
         car = Car("white")
-        car.tryMove(6)
-        val expect = 1
-        assertEquals(expect,car.position)
     }
 
     @Test
-    fun `이동 실패 테스트`(){
-        car = Car("white")
+    fun `이동 성공 테스트`() {
+        car.tryMove(6)
+        val expect = 1
+        assertEquals(expect, car.position)
+    }
+
+    @Test
+    fun `이동 실패 테스트`() {
         car.tryMove(3)
         val expect = 0
-        assertEquals(expect,car.position)
+        assertEquals(expect, car.position)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 10, 123442, -23231])
+    fun `숫자가 0~9가 아닐 경우 예외 발생 테스트`(tryNumber: Int) {
+        assertThrows<IllegalArgumentException> {
+            car.tryMove(tryNumber)
+        }
     }
 }
