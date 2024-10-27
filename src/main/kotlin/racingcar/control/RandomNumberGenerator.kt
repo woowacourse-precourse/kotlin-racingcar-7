b63ba72d804e0c2eb0a4a582d6dcf23a641a30cb
unknown <yangjk9203@naver.com>
2024-10-27 19:15:44 +0900

    style(RandomNumberGenerator): 변수명 수정

--- src/main/kotlin/racingcar/control/RandomNumberGenerator.kt
+++ src/main/kotlin/racingcar/control/RandomNumberGenerator.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 class RandomNumberGenerator {
     fun randomNumberGenerator(): Int {
         val arrange = SettingValue()
-        val randomNumberResult = Randoms.pickNumberInRange(arrange.startNumber,arrange.endNumber)
+        val randomNumberResult = Randoms.pickNumberInRange(arrange.STARTING_NUMBER,arrange.ENDING_NUMBER)
         return randomNumberResult
     }
 }
