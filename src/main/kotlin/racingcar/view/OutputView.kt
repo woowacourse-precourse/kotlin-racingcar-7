77831206f9b8ee12acb106bceb4139e698208e05
jburnoo <6pizz@naver.com>
2024-10-27 17:48:18 +0900

    feat: 출력 뷰 구현
    라운드 결과 출력
    우승자 리스트 출력

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,4 +1,24 @@
 package racingcar.view
 
+import racingcar.model.Car
+
 class OutputView {
+    fun printRoundResult(carResult: List<Car>) {
+        val sb = StringBuilder()
+        carResult.forEach {  car ->
+            sb.appendLine(car.formatRoundResult())
+        }
+        println(sb)
+    }
+
+    fun printWinner(carNames: List<String>) {
+        val winnerList = carNames.joinToString(", ")
+        print(WINNER_MSG_FORMAT.format(winnerList))
+    }
+
+    private fun Car.formatRoundResult() = "$name : ${"-".repeat(moves)}"
+
+    companion object {
+        private const val WINNER_MSG_FORMAT = "최종 우승자 : %s"
+    }
 }
