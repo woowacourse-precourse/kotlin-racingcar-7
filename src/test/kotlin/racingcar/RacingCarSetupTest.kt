34c1d8840cf0f5dc2747ff45a0d689c5bb163873
DDangDin <Kcorelec7123@gmail.com>
2024-10-27 19:48:23 +0900

    refactor: RacingCarSetupTest 테스트 코드 리팩토링
    - indent depth가 3이 넘지 않도록 수정

--- src/test/kotlin/racingcar/RacingCarSetupTest.kt
+++ src/test/kotlin/racingcar/RacingCarSetupTest.kt
@@ -13,9 +13,7 @@ class RacingCarSetupTest : NsTestCopy() {
     @Test
     fun `시도할 횟수에 숫자가 아닌 값 입력 시 예외 테스트`() {
         assertSimpleTest {
-            assertThrows<IllegalArgumentException> {
-                runException("pobi, ddangdin", "a")
-            }
+            assertThrows<IllegalArgumentException> { runException("pobi, ddangdin", "a") }
         }
     }
 }
