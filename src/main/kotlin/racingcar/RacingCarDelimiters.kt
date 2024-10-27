c64bbd4bee0597e031134bd20bdfdbde40f9064e
DDangDin <Kcorelec7123@gmail.com>
2024-10-28 02:34:49 +0900

    feat: 게임 진행 시 전진하는 자동차 출력 기능 구현
    - 게임 진행 시 자동차의 이름과 전진 표시를 출력하는 기능 구현

--- src/main/kotlin/racingcar/RacingCarDelimiters.kt
+++ src/main/kotlin/racingcar/RacingCarDelimiters.kt
@@ -3,4 +3,7 @@ package racingcar
 /** 자동차 경주 게임에서 사용되는 구분자 */
 object RacingCarDelimiters {
     const val NAME_DELIMITER = ","
+
+    /** 게임 진행 시 이름을 구분할 때 사용 */
+    const val CAR_NAME_DASH_DELIMITER = ":"
 }
--- src/main/kotlin/racingcar/RacingCarGame.kt
+++ src/main/kotlin/racingcar/RacingCarGame.kt
@@ -6,21 +6,33 @@ class RacingCarGame(
     private val cars: List<Car>,
     private val gameCount: Int,
 ) {
-    fun start() {
-        repeat(gameCount) {
-            processGame()
-        }
-    }
-
-    private fun processGame() {
+    private fun processGame(cars: List<Car>) {
         cars.forEach { car ->
             val randomNumber = pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX)
             car.moveForwardAndStop(randomNumber)
+        }
+        cars.forEach { car ->
+            printEachGame(car)
+        }
+        println()
+    }
+
+    private fun printEachGame(car: Car) {
+        val forwardCount = car.forwardCount
+        val forwardDashText = CAR_FORWARD_DASH.repeat(forwardCount)
+        val resultText = "${car.name} ${RacingCarDelimiters.CAR_NAME_DASH_DELIMITER} $forwardDashText"
+        println(resultText)
+    }
+
+    fun start() {
+        repeat(gameCount) {
+            processGame(cars)
         }
     }
 
     companion object {
         private const val RANDOM_NUMBER_MIN = 0
         private const val RANDOM_NUMBER_MAX = 9
+        private const val CAR_FORWARD_DASH = "-"
     }
 }
