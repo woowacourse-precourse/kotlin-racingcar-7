20ad3097146056acdfb5df2db88966c595a00cbb
unknown <yangjk9203@naver.com>
2024-10-27 22:25:27 +0900

    refactor(Race): 기능에 따라 함수 개별 분리

--- src/main/kotlin/racingcar/control/Race.kt
+++ src/main/kotlin/racingcar/control/Race.kt
@@ -8,17 +8,30 @@ import racingcar.view.Winner
 
 
 class Race {
-    fun race() {
+    fun start() {
+        winnerAnnouncement()
+    }
+
+    fun carList(): List<CarData> {
         println(Message().CAR_NAME)
         val carName = CarNameValidator().carNameValidator(CarNameInput().carNameInput())
         val carList = Cars().cars(CarNameSeperator().carNameSeperator(carName))
+        return carList
+    }
 
+    fun count(): Int {
         println(Message().COUNT)
         val count = CountValidator().countValidator(CountInput().countInput())
+        return count
+    }
 
-        println(Message().RESULT)
-        val raceOver = Racing(carList, count).repeatRacing()
-        val winner = WinnerSelection().winnerSelection(raceOver)
+    fun raceStart(): List<CarData> {
+        val raceOver = Racing(carList(), count()).repeatRacing()
+        return raceOver
+    }
+
+    fun winnerAnnouncement() {
+        val winner = WinnerSelection().winnerSelection(raceStart())
         Winner().winner(winner)
     }
 }
