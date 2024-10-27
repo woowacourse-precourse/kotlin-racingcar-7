4145a7af37d26427202f889eb10feda74de51dfa
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-27 21:03:24 +0900

    refactor: 유효성 객체 내 상수화

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -7,14 +7,21 @@ class Validator {
     }
 
     fun isValidCarName(name: String): Boolean {
-        return (name.isNotBlank() && name.length <= 5 && !name.contains(" "))
+        return (name.isNotBlank() && name.length <= MAXIMUM_CAR_NAME_LENGTH && !name.contains(BLANK))
     }
 
     fun isValidRaceCount(raceCount: String): Boolean {
-        return raceCount.toIntOrNull() != null && raceCount.toInt() > 0
+        return raceCount.toIntOrNull() != null && raceCount.toInt() > ZERO
     }
 
     fun isPossibleForward(randomValue: Int): Boolean {
-        return randomValue >= 4
+        return randomValue >= STANDARD_FORWARD
+    }
+
+    companion object {
+        private const val BLANK = " "
+        private const val MAXIMUM_CAR_NAME_LENGTH = 5
+        private const val ZERO = 0
+        private const val STANDARD_FORWARD = 4
     }
 }
