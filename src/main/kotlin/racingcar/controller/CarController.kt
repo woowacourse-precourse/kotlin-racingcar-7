c986ede94110b54a758282b59c9170d8eb6ead87
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-10-28 05:47:08 +0900

    feat(CarNameValidator): 자동차 이름 중복 예외 처리

--- src/main/kotlin/racingcar/controller/CarController.kt
+++ src/main/kotlin/racingcar/controller/CarController.kt
@@ -8,6 +8,8 @@ class CarController(private val carList: CarList) {
 
     fun addCarList(carNames: String) {
         val carNameList = carNames.split(",")
+        CarNameValidator.validateDuplicate(carNameList) // 자동차 이름 중복 유효성 검사
+
         for (carName in carNameList) {
             CarNameValidator.validateLength(carName.trim()) // 자동차 이름 길이 유효성 검사
             carList.addCar(Car(carName.trim()))
