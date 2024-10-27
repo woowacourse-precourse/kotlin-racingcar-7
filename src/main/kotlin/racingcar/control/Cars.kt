90e852d41a46582be39145b2b13a782d23ada3cd
unknown <yangjk9203@naver.com>
2024-10-27 20:16:45 +0900

    style(Cars): 변수명 수정

--- src/main/kotlin/racingcar/control/Cars.kt
+++ src/main/kotlin/racingcar/control/Cars.kt
@@ -2,9 +2,9 @@ import racingcar.control.CarData
 
 class Cars {
     fun cars(carNameSeperator: List<String>): List<CarData> {
-        val carPointGroup = carNameSeperator.map { CarData(it) }
+        val carList = carNameSeperator.map { CarData(it) }
         val carPointGroupType = mutableListOf<Pair<String, Int>>()
-        carPointGroup.forEach { car -> carPointGroupType.add(Pair(car.name, car.point)) }
-        return carPointGroup
+        carList.forEach { car -> carPointGroupType.add(Pair(car.name, car.point)) }
+        return carList
     }
 }
