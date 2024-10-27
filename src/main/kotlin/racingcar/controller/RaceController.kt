f7de70fb1b6267b1cd2a73ca2431d3ac8e440180
박지민 <pjm5489@naver.com>
2024-10-27 23:22:14 +0900

    feat: Error 클래스 객체 생성

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,9 +1,8 @@
 package racingcar.controller
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.model.Error
 import racingcar.model.Race
-import racingcar.model.carNameError
-import racingcar.model.tryCountError
 import racingcar.view.ErrorMsg
 import racingcar.view.Input
 import racingcar.view.Output
@@ -11,16 +10,18 @@ import racingcar.view.Output
 class RaceController {
     private val input = Input()
     private val output = Output()
-    private val errorMsg = ErrorMsg()
 
     fun run() {
+        val error = Error()
+        val errorMsg = ErrorMsg()
+
         input.carNameMsg()
         val carNames = Console.readLine()
-        if (carNameError(carNames)) errorMsg.errorMsg()
+        if (error.carNameError(carNames)) errorMsg.errorMsg()
 
         input.countMsg()
         val tryCount = Console.readLine()
-        if (tryCountError(tryCount)) errorMsg.errorMsg()
+        if (error.tryCountError(tryCount)) errorMsg.errorMsg()
 
         gameResult(carNames, tryCount)
     }
--- src/main/kotlin/racingcar/model/Error.kt
+++ src/main/kotlin/racingcar/model/Error.kt
@@ -2,49 +2,49 @@ package racingcar.model
 
 import racingcar.constants.Constants.COMMA
 
-
-private fun carNameNullError(carNames: String): Boolean {
-    return carNames.isEmpty()
-}
-
-private fun carNameLengthError(carNames: String): Boolean {
-    val carNameList = carNames.split(COMMA)
-
-    carNameList.forEach { carName ->
-        if (carName.length > 5) return true
-    }
-    return false
-}
-
-private fun sameCarNameError(carNames: String): Boolean {
-    val carNameList = carNames.split(COMMA)
-
-    carNameList.forEach { carName ->
-        if (carNameList.count { it == carName } > 1) return true
-    }
-    return false
-}
-
-fun carNameError(carNames: String): Boolean {
-
-    return carNameNullError(carNames) || carNameLengthError(carNames)
-            || sameCarNameError(carNames)
-}
-
-private fun notNumberError(tryCount: String): Boolean {
-    try {
-        tryCount.toInt()
-    } catch (e: Exception) {
-        return true
+class Error {
+    private fun carNameNullError(carNames: String): Boolean {
+        return carNames.isEmpty()
     }
 
-    return false
-}
+    private fun carNameLengthError(carNames: String): Boolean {
+        val carNameList = carNames.split(COMMA)
 
-private fun negativeNumberError(tryCount: String): Boolean {
-    return tryCount.toInt() <= 0
-}
+        carNameList.forEach { carName ->
+            if (carName.length > 5) return true
+        }
+        return false
+    }
 
-fun tryCountError(tryCount: String): Boolean {
-    return notNumberError(tryCount) || negativeNumberError(tryCount)
+    private fun sameCarNameError(carNames: String): Boolean {
+        val carNameList = carNames.split(COMMA)
+
+        carNameList.forEach { carName ->
+            if (carNameList.count { it == carName } > 1) return true
+        }
+        return false
+    }
+
+    fun carNameError(carNames: String): Boolean {
+
+        return carNameNullError(carNames) || carNameLengthError(carNames) || sameCarNameError(carNames)
+    }
+
+    private fun notNumberError(tryCount: String): Boolean {
+        try {
+            tryCount.toInt()
+        } catch (e: Exception) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun negativeNumberError(tryCount: String): Boolean {
+        return tryCount.toInt() <= 0
+    }
+
+    fun tryCountError(tryCount: String): Boolean {
+        return notNumberError(tryCount) || negativeNumberError(tryCount)
+    }
 }
