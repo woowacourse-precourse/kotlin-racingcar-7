f5b6a86683b770a4e34291be0cd51beeb4269190
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-27 22:42:40 +0900

    feat (OutputHandler.kt) : 경기 최종 우승자 출력 기능 구현

--- src/main/kotlin/racingcar/OutputHandler.kt
+++ src/main/kotlin/racingcar/OutputHandler.kt
@@ -7,6 +7,11 @@ class OutputHandler {
         println(PROMPT_OUTPUT_RACING_GAME)
     }
 
+    fun printRacingWinner(winner : List<String>) {
+        print(PROMPT_OUTPUT_RACING_WINNER)
+        println(winner.joinToString(","))
+    }
+
     companion object {
         const val PROMPT_OUTPUT_RACING_GAME = "실행 결과"
         const val PROMPT_OUTPUT_RACING_WINNER = "최종 우승자 : "
