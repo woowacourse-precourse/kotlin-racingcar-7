cb7ec56ff7c411e2334b6e0c3ac6d2670b44271d
yrsel <xxknu88@gmail.com>
2024-10-27 17:51:26 +0900

    fix(CarNameValidator): 상수화 및 중복된 차 이름 로직 수정
    자동차 이름 구분자와 탭 제거 정규표현식 상수화
    validateDuplicateCarName 내부 구현에서 set 자료구조 생성자에 바로 인자 주입해서 비교하는 방식으로 수정
    invalidCarNameSize 함수를 두어 validateCarNameSize 코드 읽기 수월하게 수정

--- src/main/kotlin/racingcar/validator/CarNameValidator.kt
+++ src/main/kotlin/racingcar/validator/CarNameValidator.kt
@@ -1,7 +1,12 @@
 package racingcar.validator
 
+import racingcar.controller.CAR_NAME_DELIMITER
 import racingcar.exception.ExceptionMessage
 import racingcar.exception.RacingGameException
+
+private const val CAR_NAME_MAX_SIZE = 5
+private val BLANK_AND_TAB_REMOVE_REGEX = """[ \t]""".toRegex()
+private const val BLANK_AND_TAB_REPLACEMENT = ""
 
 object CarNameValidator {
 
@@ -12,24 +17,25 @@ object CarNameValidator {
         if (!validateCarNameSize(candidateCarNames)) throw RacingGameException(ExceptionMessage.INVALID_CAR_NAME_SIZE)
         if (!validateDuplicateCarName(candidateCarNames)) throw RacingGameException(ExceptionMessage.DUPLICATED_CAR_NAME)
 
-        return candidateCarNames.joinToString(separator = ",") { it }
+        return candidateCarNames.joinToString(separator = CAR_NAME_DELIMITER) { it }
     }
 
     private fun replaceBlankByInput(input: String): String {
-        return input.replace("""[ \t]""".toRegex(), "")
+        return input.replace(BLANK_AND_TAB_REMOVE_REGEX, BLANK_AND_TAB_REPLACEMENT)
     }
 
     private fun convertValidateForm(input: String): List<String> {
-        return input.split(",").toList()
+        return input.split(CAR_NAME_DELIMITER).toList()
     }
 
     private fun validateCarNameSize(input: List<String>): Boolean {
-        return input.none { it.isEmpty() || it.length > 5 }
+        return input.none { invalidCarNameSize(it) }
     }
 
+    private fun invalidCarNameSize(carName: String) = carName.isEmpty() || carName.length > CAR_NAME_MAX_SIZE
+
     private fun validateDuplicateCarName(input: List<String>): Boolean {
-        val duplicateChecker = hashSetOf<String>()
-        duplicateChecker.addAll(input)
+        val duplicateChecker = hashSetOf(elements = input.toTypedArray())
         return duplicateChecker.size == input.size
     }
 
