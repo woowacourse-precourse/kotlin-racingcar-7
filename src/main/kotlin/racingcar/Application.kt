b045091c4e7f0ab6032a84757c95ac236c0ab1a8
DoableTuple <doabletuple@gmail.com>
2024-10-28 01:07:07 +0900

    feat: 경주에 참여하는 자동차의 목록을 입력받는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,10 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    fun readCarNames(): String {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        return Console.readLine()
+    }
 }
