3b4730a17bda21687714a44357fe37fbf49b75b9
wjdrjs00 <kimjk3409@gmail.com>
2024-10-27 23:51:51 +0900

    feat(domain): Lap class 추가
    - 생성자로 문자열 랩 카운드를 받는다.
    - 반복 횟수로 사용을 위한 숫자 변환 메소드를 추가한다.
    - 변환이 불가능한 문자인 경우 예외를 발생시킨다.
    - 반복 랩 횟수는 1이상이여야 하므로 검증로직을 추가한다.
    - 초기화시 변환과 검증을 수행한다.

---
+++ src/main/kotlin/racingcar/domain/Lap.kt
@@ -0,0 +1,23 @@
+package racingcar.domain
+
+class Lap(
+    private val stringLapCount: String
+) {
+
+    val count: Int
+
+    init {
+        count = parserLapCount().also { validateLapCountPositive(it) }
+    }
+
+    private fun parserLapCount() =
+        stringLapCount.toIntOrNull() ?: throw IllegalArgumentException("랩 카운트는 숫자를 입력해야 합니다.")
+
+    private fun validateLapCountPositive(lapCount: Int) =
+        require(lapCount >= MIN_LAP_COUNT) { "랩 카운트는 1회 이상 입력해야합니다." }
+
+    companion object {
+        private const val MIN_LAP_COUNT = 1
+    }
+
+}
