e7ebd74c97684df5bfdf3c08cb143704eafaaa27
DDangDin <Kcorelec7123@gmail.com>
2024-10-27 19:46:23 +0900

    test: RacingCarSetup 테스트 코드 작성
    - NsTest 코드를 복사해서 입맛에 맞게 수정
    - RacingCarSetupTest 에서 NsTestCopy 사용

---
+++ src/test/kotlin/racingcar/RacingCarSetupTest.kt
@@ -0,0 +1,21 @@
+package racingcar
+
+import NsTestCopy
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class RacingCarSetupTest : NsTestCopy() {
+    override fun runFunction() {
+        RacingCarSetup()
+    }
+
+    @Test
+    fun `시도할 횟수에 숫자가 아닌 값 입력 시 예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> {
+                runException("pobi, ddangdin", "a")
+            }
+        }
+    }
+}
