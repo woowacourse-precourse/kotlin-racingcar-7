7b825c01098316fcfd833c120bf365467ec5621d
wjdrjs00 <kimjk3409@gmail.com>
2024-10-27 23:43:22 +0900

    test(CarFactoryTest): CarFactory Class 테스트 추가
    - 차 이름이 한대인 경우 예외를 발생하는지
    - 중복된 차이름 있는 경우 예외를 발생하는지
    - 올바른 차 이름들이 주어지면 예외를 발생하지 않는지
    - 주어진 차 이름들과 같은 차리스트를 반환하는지

---
+++ src/test/kotlin/racingcar/domain/CarFactoryTest.kt
@@ -0,0 +1,61 @@
+package racingcar.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+class CarFactoryTest {
+
+    private val randomNumber = FakeRandomNumberImpl()
+
+    @Test
+    fun `주어진 차이름이 한대면 예외를 발생시킨다`() {
+        // Given
+        val carNames = listOf("소나타")
+
+        // When & Then
+        assertThrows<IllegalArgumentException> {
+            CarFactory.createCars(carNames, randomNumber)
+        }.apply {
+            assertThat(message).isEqualTo("차가 2대 이상있어야 레이싱을 시작할 수 있습니다.")
+        }
+    }
+
+    @Test
+    fun `중복된 차이름이 있으면 예외를 발생시킨다`() {
+        // Given
+        val carNames = listOf("소나타", "소나타", "아반떼")
+
+        // When & Then
+        assertThrows<IllegalArgumentException> {
+            CarFactory.createCars(carNames, randomNumber)
+        }.apply {
+            assertThat(message).isEqualTo("차 이름은 중복입력할 수 없습니다.")
+        }
+    }
+
+    @Test
+    fun `올바르게 차 이름들이 주어지면 예외를 발생시키지 않는다`() {
+        // Given
+        val carNames = listOf("소나타", "티코", "아반떼")
+
+        // When & Then
+        assertDoesNotThrow {
+            CarFactory.createCars(carNames, randomNumber)
+        }
+    }
+
+    @Test
+    fun `주어진 차 이름들을 가진 차리스트를 반환한다`() {
+        // Given
+        val carNames = listOf("소나타", "티코")
+
+        // When
+        val cars = CarFactory.createCars(carNames, randomNumber)
+
+        // Then
+        assertThat(cars.map { it.carName }).isEqualTo(carNames)
+    }
+
+}
