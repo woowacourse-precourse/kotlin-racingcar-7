5fc710b19c3aaee5cca2b9dfafb1194533b78493
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-27 22:31:15 +0900

    feat (InputValidator.kt) : 차 이동 횟수, 이름 입력값 검증 기능 구현

--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -2,6 +2,11 @@ package racingcar
 
 class InputValidator() {
 
+    fun validateInput(nameInput: String, countInput: String) {
+        nameInput.validateNameLength()
+        countInput.validateCountInput()
+    }
+
     fun String.validateNameLength() {
         this.split(",").forEach {
             if (it.length > 5)
@@ -9,6 +14,15 @@ class InputValidator() {
                     ErrorType.INVALID_NAME_OVER_5_LENGTH_INPUT.message
                 )
         }
+    }
+
+    fun String.validateCountInput() {
+        if (!(this.isNumeric()))
+            throw IllegalArgumentException(ErrorType.INVALID_COUNT_NOT_NUMBER_INPUT.message)
+        if (this.isNegativeNumber())
+            throw IllegalArgumentException(ErrorType.INVALID_COUNT_NEGATIVE_INPUT.message)
+        if (isDecimalNumber())
+            throw IllegalArgumentException(ErrorType.INVALID_COUNT_NOT_DIGIT_INPUT.message)
     }
 
     private fun String.isNumeric(): Boolean =
