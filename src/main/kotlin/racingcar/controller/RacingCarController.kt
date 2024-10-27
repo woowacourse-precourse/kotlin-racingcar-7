eb4f83861f3fe52b0032bf108c24ff657e961806
emjayMJkim <minjeong030420@gmail.com>
2024-10-28 02:05:45 +0900

    feat(winner): 최종 우승자 출력
    - 누가 우승했는지 출력합니다.
    - 우승자가 여러명일 경우 (,)를 이용해 구분합니다.

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -4,10 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms
 import racingcar.model.RacingCar
 import racingcar.util.Constant.MOVE_VALID_CONDITION
 import racingcar.util.Constant.RANDOM_NUMBER_MAX_RANGE
+import racingcar.util.Constant.WINNER_JOIN_LETTER
 import racingcar.util.Constant.ZERO
 import racingcar.util.PrintMessage.RESULT_TITLE
 import racingcar.util.PrintMessage.START_OUTPUT_MESSAGE
 import racingcar.util.PrintMessage.TRY_NUMBER_OUTPUT_MESSAGE
+import racingcar.util.PrintMessage.WINNER_RESULT_CONTENT
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
@@ -37,11 +39,14 @@ class RacingCarController {
     private fun doRacing(carList: List<RacingCar>, tryNumber: Int) {
         outputView.printMessage(RESULT_TITLE)
 
+        var updatedCarList: List<RacingCar> = emptyList()
         repeat(tryNumber) {
-            val updatedCarList: List<RacingCar> = updateRacingCarList(carList)
+            updatedCarList = updateRacingCarList(carList)
 
             outputView.printRacingResult(updatedCarList)
         }
+
+        checkFinalWinner(updatedCarList)
     }
 
     private fun updateRacingCarList(carList: List<RacingCar>): List<RacingCar> {
@@ -57,5 +62,26 @@ class RacingCarController {
 
         if (randomMoveNumber >= MOVE_VALID_CONDITION)
             carItem.onMoveCar()
+    }
+
+    private fun checkFinalWinner(carList: List<RacingCar>) {
+        val maxLocationCarNameList = checkMaxLocation(carList)
+        val winner: String = maxLocationCarNameList.joinToString(WINNER_JOIN_LETTER)
+
+        outputView.printMessage("$WINNER_RESULT_CONTENT : $winner")
+    }
+
+    private fun checkMaxLocation(carList: List<RacingCar>): List<String> {
+        val maxLocation = carList.maxOf { carItem ->
+            carItem.location
+        }
+
+        val maxLocationCarNameList: List<String> = carList.filter { carItem ->
+            carItem.location == maxLocation
+        }.map { maxCarItem ->
+            maxCarItem.carName
+        }
+
+        return maxLocationCarNameList
     }
 }
--- src/main/kotlin/racingcar/util/Constant.kt
+++ src/main/kotlin/racingcar/util/Constant.kt
@@ -2,6 +2,7 @@ package racingcar.util
 
 object Constant {
     const val CAR_NAME_SPLITTER = ','
+    const val WINNER_JOIN_LETTER = ", "
     const val BLANK = ' '
     const val ZERO = 0
     const val MAX_LENGTH = 5
