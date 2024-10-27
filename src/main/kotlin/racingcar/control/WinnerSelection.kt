9e4d274c1159d2ada6d9b380423b26fc57bdb5b7
unknown <yangjk9203@naver.com>
2024-10-27 22:13:36 +0900

    chore(WinnerSelection): 코드 정렬

--- src/main/kotlin/racingcar/control/WinnerSelection.kt
+++ src/main/kotlin/racingcar/control/WinnerSelection.kt
@@ -2,7 +2,7 @@ package racingcar.control
 
 class WinnerSelection {
 
-    fun winnerSelection(raceOver:List<CarData>): String {
+    fun winnerSelection(raceOver: List<CarData>): String {
         val winnerPoint = raceOver.maxByOrNull { it.point }?.point
         return raceOver.filter { it.point == winnerPoint }.map { it.name }.joinToString()
     }
