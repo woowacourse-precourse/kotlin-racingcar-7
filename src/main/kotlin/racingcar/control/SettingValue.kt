b7abd0cc5c4eca84df1bf797780d1a4ce48bf458
unknown <yangjk9203@naver.com>
2024-10-27 15:37:38 +0900

    docs(SettingValue): 변수명 수정

--- src/main/kotlin/racingcar/control/SettingValue.kt
+++ src/main/kotlin/racingcar/control/SettingValue.kt
@@ -3,7 +3,7 @@ package racingcar.control
 class SettingValue {
     val startNumber = 0
     val endNumber = 9
-    val minimumNumberToMoveForward = 4
+    val minimumNumberToOneStepForward = 4
     val delimiters = ","
     val maximumNumberOfCharacters = 5
 }
