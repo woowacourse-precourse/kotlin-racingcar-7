3cb3983f4ec167d5b429f7643811c54ee5c9aabb
wjdrjs00 <kimjk3409@gmail.com>
2024-10-27 23:37:13 +0900

    feat(carFactory): 차이름 리스트 검증로직 구현
    - 게임에 참여하는 차가 2대 이상인지에 대한 검증 구현
    - 중복된 차가 있는지에 대한 검증 구현

--- src/main/kotlin/racingcar/domain/CarFactory.kt
+++ src/main/kotlin/racingcar/domain/CarFactory.kt
@@ -7,6 +7,12 @@ class CarFactory {
         private fun carGenerator(carNames: List<String>, randomNumber: RandomNumber): List<Car> =
             carNames.map { carName -> Car(carName, randomNumber) }
 
+        private fun validateCarNamesCount(carNames: List<String>) =
+            require(carNames.size > 1) { "차가 2대 이상있어야 레이싱을 시작할 수 있습니다." }
+
+        private fun validateUniqueCarNames(carNames: List<String>) =
+            require(carNames.size == carNames.toSet().size) { "차 이름은 중복입력할 수 없습니다." }
+
     }
 
 }
