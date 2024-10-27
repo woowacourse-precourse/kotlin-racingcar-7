9f66a29357aedf92973d4d2263c0b6115ec7f50b
jhseo12 <jhseo1778@gmail.com>
2024-10-27 10:02:32 +0900

    refactor: 무작위 값 생성 함수 합병

--- src/main/kotlin/racingcar/controller/Race.kt
+++ src/main/kotlin/racingcar/controller/Race.kt
@@ -1,5 +1,6 @@
 package racingcar.controller
 
+import camp.nextstep.edu.missionutils.Randoms
 import racingcar.model.Move
 import racingcar.view.InputView
 import racingcar.view.OutputView
@@ -14,6 +15,7 @@ class Race {
         outputView.printInputCountMessage()
         val count = inputView.inputCount()
         play(infoOfCar, count)
+        winner(infoOfCar)
     }
 
     private fun play(infoOfCar: MutableMap<String, Int>, count: Int) {
@@ -21,7 +23,6 @@ class Race {
         for (i in 1..count) {
             status(infoOfCar)
         }
-        winner(infoOfCar)
     }
 
     private fun status(infoOfCar: MutableMap<String, Int>) {
@@ -32,14 +33,15 @@ class Race {
     private fun step(infoOfCar: MutableMap<String, Int>): MutableMap<String, Int> {
         val move = Move()
         for ((name, nowStep) in infoOfCar) {
-            move.moveCar(infoOfCar, name, nowStep, move.getRandomNumber())
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            move.moveCar(infoOfCar, name, nowStep, randomNumber)
         }
         return infoOfCar
     }
 
     fun winner(infoOfCar: MutableMap<String, Int>) {
         val maxMove = infoOfCar.maxOf { it.value }
-        val winner = infoOfCar.filter{ it.value == maxMove }.keys
-        outputView.printWinnerMessage(winner)
+        val winnerName = infoOfCar.filter{ it.value == maxMove }.keys
+        outputView.printWinnerMessage(winnerName)
     }
 }
--- src/main/kotlin/racingcar/model/Move.kt
+++ src/main/kotlin/racingcar/model/Move.kt
@@ -7,16 +7,11 @@ class Move {
         infoOfCar: MutableMap<String, Int>,
         name: String,
         nowStep: Int,
-        number: Int,
+        number: Int
     ): MutableMap<String, Int> {
         if (number >= 4) {
             infoOfCar[name] = nowStep + 1
         }
         return infoOfCar
-    }
-
-    fun getRandomNumber(): Int {
-        val randomNumber = Randoms.pickNumberInRange(0, 9)
-        return randomNumber
     }
 }
