3d1af4c345b11c44c9a78e79ead15db1962da6c4
rungeun <wjdrmsdl5@gmail.com>
2024-10-27 19:10:16 +0900

    feat(InputView): 자동차 이름과 시도 횟수를 입력받는 기능 추가
    - 가이드 라인 텍스트 출력 및 입력 기능 구현
    - 문자열 상수를 따로 분리하여 유지 보수를 용이하게 하기 위함
    - 입력 시 예외 상황을 즉시 처리하여 불필요한 연산을 줄이기 위함

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,19 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputView {
+    fun carName(): String{
+        println(InputViewString.CAR_NAME_GUIDE)
+        val input = Console.readLine()
+        require(input.isNotBlank()){ExceptionString.BLANK}
+        return input
+    }
+
+    fun tryCount(): UInt{
+        println(InputViewString.RACING_TRY_GUIDE)
+        val input = Console.readLine()
+        require(input.toUIntOrNull() != null){ExceptionInt.NOT_UINT}
+        return input.toUInt()
+    }
+}
