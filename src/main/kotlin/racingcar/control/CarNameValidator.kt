6d82126a95bf013ac19a0008ab7aa69275dfd48e
unknown <yangjk9203@naver.com>
2024-10-27 19:12:42 +0900

    feat(CarNameValidator): 오류 처리 추가

--- src/main/kotlin/racingcar/control/CarNameValidator.kt
+++ src/main/kotlin/racingcar/control/CarNameValidator.kt
@@ -2,27 +2,38 @@ package racingcar.control
 
 import racingcar.view.CarNameError
 
+
 class CarNameValidator {
 
-    fun carNameValidator(carNameInput: String): List<String> {
+    fun carNameValidator(carNameInput: String): String {
 
         if (carNameInput.isBlank()) {
-            throw IllegalArgumentException(CarNameError().carNameBlank)
+            throw IllegalArgumentException(CarNameError().EMPTY_NAME)
         }
-        if (carNameInput.all { it.toString() == SettingValue().delimiters }) {
-            throw IllegalArgumentException(CarNameError().onlyDelimiters)
+        if (carNameInput.all { it.toString() == " " }) {
+            throw IllegalArgumentException(CarNameError().ONLY_GAP)
         }
-
-        val carName = CarNameSeparator().carNameSeparator(carNameInput)
-
-        for (i in carName) {
-            if (i.length > SettingValue().maximumNumberOfCharacters) {
-                throw IllegalArgumentException(CarNameError().carNameOverFive)
+        if (carNameInput.all { it.toString() == SettingValue().DELIMITER }) {
+            throw IllegalArgumentException(CarNameError().DELIMITER_INPUT)
+        }
+        if (carNameInput.contains(",,")) {
+            throw IllegalArgumentException(CarNameError().CONTINUOUS_DELIMITER)
+        }
+        if (carNameInput.startsWith(",") || carNameInput.endsWith(",")) {
+            throw IllegalArgumentException(CarNameError().START_AND_END_DELIMITER_INPUT)
+        }
+        for (i in carNameInput.split(SettingValue().DELIMITER)) {
+            if (i.all { it.toString() == " " }) {
+                throw IllegalArgumentException(CarNameError().GAP_DELIMITER_INPUT)
+            }
+            if (i.length > SettingValue().MAXIMUM_NUMBER_OF_CHARACTERS) {
+                throw IllegalArgumentException(CarNameError().EXCEEDED_CHARACTER_COUNT_)
             }
         }
-        if (!carNameInput.contains(SettingValue().delimiters) || carName.count() == 1) {
-            throw IllegalArgumentException(CarNameError().notCompetitor)
+        if (carNameInput.contains(SettingValue().DELIMITER) && carNameInput.split(SettingValue().DELIMITER).size == 1) {
+            throw IllegalArgumentException(CarNameError().CANT_RACE_ALONE)
         }
-        return carName
+        println(carNameInput)
+        return carNameInput
     }
 }
