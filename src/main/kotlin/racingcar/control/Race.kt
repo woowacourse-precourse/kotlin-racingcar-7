49f774d735c8805527b2ace6aaecd6876b6c2aaf
unknown <yangjk9203@naver.com>
2024-10-27 21:30:00 +0900

    feat(Race): 경주 기능 추가

--- src/main/kotlin/racingcar/control/Race.kt
+++ src/main/kotlin/racingcar/control/Race.kt
@@ -1,21 +1,24 @@
 package racingcar.control
 
+import Cars
 import racingcar.userInput.CarNameInput
 import racingcar.userInput.CountInput
 import racingcar.view.Message
+import racingcar.view.Winner
+
 
 class Race {
     fun race() {
-        println(Message().carName)
-        val carNameInput = CarNameInput().carNameInput()
-        val carName = CarNameValidator().carNameValidator(carNameInput)
+        println(Message().CAR_NAME)
+        val carName = CarNameValidator().carNameValidator(CarNameInput().carNameInput())
+        val carList = Cars().cars(CarNameSeperator().carNameSeperator(carName))
 
-        println(Message().count)
-        val countInput = CountInput().countInput()
-        val count = CountValidator().countValidator(countInput)
+        println(Message().COUNT)
+        val count = CountValidator().countValidator(CountInput().countInput())
 
-
-
-
+        println(Message().RESULT)
+        val raceOver = Racing(carList, count).repeatRacing()
+        val winner = WinnerSelection().winnerSelection(raceOver)
+        Winner().winner(winner)
     }
 }
