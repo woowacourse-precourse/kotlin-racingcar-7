e024e8be1dc9d5e66f1fca478958444f81a2ef0b
unknown <yangjk9203@naver.com>
2024-10-27 20:43:57 +0900

    feat(Racing): 매 회차마다 경주상황 계산하고 출력

---
+++ src/main/kotlin/racingcar/control/Racing.kt
@@ -0,0 +1,28 @@
+package racingcar.control
+
+import racingcar.view.Message
+
+class Racing(private val cars: List<CarData>, private val count: Int) {
+    fun repeatRacing(): List<CarData> {
+        repeat(count) {
+            racing()
+        }
+        return cars
+    }
+
+    private fun racing() {
+        cars.forEach { c ->
+            if (OneStepForward().oneStepForward()) {
+                ++c.point
+            }
+        }
+        cars.forEach { c ->
+            println(c.name + Message().NAME_FORWARD_STRING_SEPARATOR + hyphensCount(c.point))
+        }
+        println()
+    }
+
+    private fun hyphensCount(int: Int): String {
+        return Message().FORWARD_STRING.repeat(int)
+    }
+}
