45659d4fd8a061c2541f81c1e905d9c6301e5703
unknown <yangjk9203@naver.com>
2024-10-27 19:16:33 +0900

    style(Message): 변수명 수정

--- src/main/kotlin/racingcar/view/Message.kt
+++ src/main/kotlin/racingcar/view/Message.kt
@@ -2,10 +2,10 @@ package racingcar.view
 
 class Message {
 
-        val carName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-        val count = "시도할 횟수는 몇 회인가요?"
-        val result = "실행 결과"
-        val winner = "최종 우승자 : "
-        val forwardString = "-"
-        val nameForwardStringSeparator = " : "
+    val CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    val COUNT = "시도할 횟수는 몇 회인가요?"
+    val RESULT = "실행 결과"
+    val FORWARD_STRING = "-"
+    val NAME_FORWARD_STRING_SEPARATOR = " : "
+    val WINNER = "최종 우승자 : "
 }
