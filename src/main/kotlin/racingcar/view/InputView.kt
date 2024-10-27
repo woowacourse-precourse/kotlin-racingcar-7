cbe93591b40c0aeb6785a19cb6c7a6db85523bd5
jburnoo <6pizz@naver.com>
2024-10-27 17:49:23 +0900

    ref: 질문 메시지 상수 분리

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,17 +4,23 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun getCarNames(): String {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
+        println(NAME_QUEST_MSG)
         return read()
     }
 
     fun getRounds(): Int {
-        println("시도할 횟수는 몇 회인가요?\n")
+        println(ROUND_QUEST_MSG)
         val rounds = read().toIntOrNull() ?: throw IllegalArgumentException()
+        println()
         return rounds
     }
 
     private fun read(): String = Console.readLine() ?: throw IllegalArgumentException()
 
     fun close() = Console.close()
+
+    companion object {
+        private const val NAME_QUEST_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val ROUND_QUEST_MSG = "시도할 횟수는 몇 회인가요?"
+    }
 }
