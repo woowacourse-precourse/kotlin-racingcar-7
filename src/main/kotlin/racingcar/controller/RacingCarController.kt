75a1d23626b0898a89f564ea37e08a09c2af74d3
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-10-27 09:34:12 +0900

    feat: 경기 진행 및 결과 출력 기능 구현

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -3,6 +3,7 @@ package racingcar.controller
 import racingcar.model.Model
 import racingcar.utils.Validator
 import racingcar.view.InputView
+import racingcar.view.OutputView
 
 object RacingCarController {
     fun run() {
@@ -11,7 +12,25 @@ object RacingCarController {
         val round = InputView.inputRound().toInt()
         Validator.validateInputRound(round.toString())
 
-        inputNames.split(",").map { it.trim() }.map { Model(it) }
+        val cars = inputNames.split(",").map { it.trim() }.map { Model(it) }
+
+        raceStrat(cars, round)
+    }
+
+    fun raceStrat(cars: List<Model>, round: Int) {
+        println("실행결과")
+        repeat(round) {
+            RandomCarMove(cars)
+            OutputView.outputRaceStatus(cars)
+            println()
+        }
+
+    }
+
+    fun RandomCarMove(cars: List<Model>) {
+        for (car in cars) {
+            if (car.canMoveForward()) car.moveCount++
+        }
     }
 
 }
--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,8 +3,7 @@ package racingcar.view
 import racingcar.model.Model
 
 object OutputView {
-    fun raceStart(cars: List<Model>) {
-        println("실행결과")
+    fun outputRaceStatus(cars: List<Model>) {
         for (car in cars) {
             val carName = car.name
             val carMoveCount = "-".repeat(car.moveCount)
