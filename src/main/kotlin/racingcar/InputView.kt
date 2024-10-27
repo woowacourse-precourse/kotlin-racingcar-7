25b1f61fe160ba5a474ddf7e1b7b8887beb26660
yeonjeen <jeendev2019@gmail.com>
2024-10-28 03:02:48 +0900

    chore(StringMessages): 메시지 상수 분리 및 StringMessages 객체 생성 - 자동차 이름 입력 및 이동 횟수 관련 메시지를 상수로 정의하여 코드에서 재사용 가능하도록 했습니다. - 기존에 InputView.kt에 있던 스트링 관련 상수는 제거했습니다.

--- src/main/kotlin/racingcar/InputView.kt
+++ src/main/kotlin/racingcar/InputView.kt
@@ -1,6 +1,11 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.Messages.ERROR_DUPLICATE_NAME
+import racingcar.Messages.ERROR_INVALID_TRY_COUNT
+import racingcar.Messages.ERROR_NAME_LENGTH
+import racingcar.Messages.INPUT_CAR_NAMES
+import racingcar.Messages.INPUT_TRY_COUNT
 
 class InputView {
     fun getCarNames(): List<String> {
@@ -39,13 +44,5 @@ class InputView {
         if (tryCount <= 0) {
             throw IllegalArgumentException(ERROR_INVALID_TRY_COUNT)
         }
-    }
-
-    companion object{
-        const val INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-        const val ERROR_NAME_LENGTH = "자동차 이름은 1자 이상 5자 이하의 이름이어야 합니다."
-        const val ERROR_DUPLICATE_NAME = "자동차 이름은 중복될 수 없습니다."
-        const val INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?"
-        const val ERROR_INVALID_TRY_COUNT = "이동 횟수는 양의 정수로 입력해야 합니다."
     }
 }
