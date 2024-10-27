e7c56d4e397ec113d9d9df55312baf6d62b9f294
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-10-27 14:43:55 +0900

    feat(RacingCar): 레이싱카 전진기능 추가

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import racingcar.model.RacingCar
 
 class ApplicationTest : NsTest() {
     @Test
@@ -15,7 +16,8 @@ class ApplicationTest : NsTest() {
                 run("pobi,woni", "1")
                 assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
             },
-            MOVING_FORWARD, STOP
+            MOVING_FORWARD,
+            STOP,
         )
     }
 
@@ -24,6 +26,13 @@ class ApplicationTest : NsTest() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
         }
+    }
+
+    @Test
+    fun `레이싱카 전진 테스트`() {
+        val racingCar = RacingCar("apple")
+        repeat(5) { racingCar.moveForward() }
+        println(racingCar)
     }
 
     override fun runMain() {
