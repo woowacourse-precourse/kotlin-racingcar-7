95a6a9cec91519e9f5b7a279f89a86c262f9726d
gixxla <gigixquisite@gmail.com>
2024-10-28 07:45:10 +0900

    feat: 안내 문구 출력 및 문자열 입력 기능 추가
    - 안내 문구 출력
    - 순서대로 자동차 이름 및 시도할 횟수 입력 후 저장

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val cars = Console.readLine().split(",")
+    println("시도할 횟수는 몇 회인가요?")
+    val count: Int = Console.readLine().toInt()
 }
