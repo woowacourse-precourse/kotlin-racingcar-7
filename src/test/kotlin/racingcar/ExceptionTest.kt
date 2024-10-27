6e7a9b665275d2f0c3ec16677fcdabdd0b080b33
baekcii <bsy8470@gmail.com>
2024-10-27 20:41:40 +0900

    Feat(Test): 기능별 테스트 클래스 분리 및 테스트 케이스 수정

---
+++ src/test/kotlin/racingcar/ExceptionTest.kt
@@ -0,0 +1,57 @@
+package racingcar
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ExceptionTest {
+    val check = CheckException()
+
+    @Test
+    fun `firstInput_빈문자열 또는 공백을 입력할 경우`() {
+        assertThrows<IllegalArgumentException> { check.firstInput(" ") }
+        assertThrows<IllegalArgumentException> { check.firstInput("") }
+    }
+
+    @Test
+    @DisplayName("")
+    fun `firstInput_자동차 이름이 공백일 경우`() {
+        assertThrows<IllegalArgumentException> { check.firstInput("a,") }
+        assertThrows<IllegalArgumentException> { check.firstInput(",a,b") }
+    }
+
+    @Test
+    @DisplayName("")
+    fun `firstInput_자동차 이름이 하나만 입력된 경우`() {
+        assertThrows<IllegalArgumentException> { check.firstInput("a") }
+    }
+
+
+    @Test
+    @DisplayName("쉼표와 자동차 이름 사이의 공백은 무시, 문자 사이의 공백은 1자로 취급")
+    fun `firstInput_자동차 이름이 5자가 넘는 경우`() {
+        assertThrows<IllegalArgumentException> { check.firstInput("abcd, abcde, abcdef") }
+        assertThrows<IllegalArgumentException> { check.firstInput("a, b, c, d, e, f, g hijk") }
+    }
+
+    @Test
+    fun `secondInput_빈문자열 또는 공백을 입력할 경우`() {
+        assertThrows<IllegalArgumentException> { check.secondInput("") }
+        assertThrows<IllegalArgumentException> { check.secondInput(" ") }
+
+    }
+
+    @Test
+    @DisplayName("양의 정수가 아닌 경우: 0, 음수, 실수, 문자")
+    fun `secondInput_입력 형태`() {
+        assertThrows<IllegalArgumentException> { check.secondInput("0") }
+        assertThrows<IllegalArgumentException> { check.secondInput("3.3") }
+        assertThrows<IllegalArgumentException> { check.secondInput("-3") }
+        assertThrows<IllegalArgumentException> { check.secondInput("a") }
+    }
+
+    @Test
+    fun resultException() {
+        assertThrows<IllegalArgumentException> { check.result(mutableMapOf("a" to 0, "b" to 0, "c" to 0)) }
+    }
+}
--- src/test/kotlin/racingcar/RaceTest.kt
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -1,64 +1,43 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
-import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
-import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class RaceTest {
     val race = RaceGame()
-    val check = CheckException()
 
     @Test
+    @DisplayName("각 자동차의 전진값을 0으로 초기화")
     fun splitCarsTest() {
-        val cars = "  abc, def,  g hi "
+        val cars = "a,b,c,d,e"
         val carMap = race.splitCars(cars)
 
         assertThat(carMap).containsExactlyEntriesOf(
-            mapOf("abc" to 0, "def" to 0, "g hi" to 0)
+            mapOf("a" to 0, "b" to 0, "c" to 0, "d" to 0, "e" to 0)
         )
     }
 
     @Test
     fun goForwardTest() {
-        val carMap = mutableMapOf<String, Int>("a" to 0, "b" to 0)
-        var a = 10
+        val carMap = mutableMapOf<String, Int>("a" to 0, "b" to 0, "c" to 0, "d" to 0, "e" to 0)
+
         assertRandomNumberInRangeTest(
             {
                 val increaseScore = race.goForward(carMap)
                 assertThat(increaseScore["a"]).isEqualTo(1)
                 assertThat(increaseScore["b"]).isEqualTo(0)
-            }, 4, 3
+                assertThat(increaseScore["a"]).isEqualTo(1)
+                assertThat(increaseScore["b"]).isEqualTo(0)
+                assertThat(increaseScore["a"]).isEqualTo(1)
+            }, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD
         )
     }
 
-    @Test
-    fun firstInputExceptionTest() {
-        assertThrows<IllegalArgumentException> { check.firstInput("abcd, abcde, abcdef") }
-        assertThrows<IllegalArgumentException> { check.firstInput("") }
-        assertThrows<IllegalArgumentException> { check.firstInput(" ") }
-        assertThrows<IllegalArgumentException> { check.firstInput("a,") }
-        assertThrows<IllegalArgumentException> { check.firstInput(",") }
-        assertThrows<IllegalArgumentException> { check.firstInput("a") }
-        assertThrows<IllegalArgumentException> { check.firstInput(",a,b") }
-    }
-
-    @Test
-    fun secondInputExceptionTest() {
-        assertThrows<IllegalArgumentException> { check.secondInput("") }
-        assertThrows<IllegalArgumentException> { check.secondInput(" ") }
-        assertThrows<IllegalArgumentException> { check.secondInput("0") }
-        assertThrows<IllegalArgumentException> { check.secondInput("3.3") }
-        assertThrows<IllegalArgumentException> { check.secondInput("-3") }
-        assertThrows<IllegalArgumentException> { check.secondInput("a") }
-    }
-
-    @Test
-    fun resultException() {
-        assertThrows<IllegalArgumentException> { check.result(mutableMapOf("a" to 0, "b" to 0, "c" to 0)) }
+    companion object {
+        private const val MOVING_FORWARD: Int = 4
+        private const val STOP: Int = 3
     }
 
 }
