1c27987e0c4cdd29481cb0db000bc2e1c002d92c
Labomem-No-004 <kurikame73@gmail.com>
2024-10-28 16:44:48 +0900

    feat(Input): 인풋 기능
    자동차 이름 입력 (5자)
    시도 횟수 입력 (음수 제외)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,25 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+}
+
+class InputProcessor {
+    fun inputCarNames(): List<String> {
+        println("자동차 이름을 쉼표(,)로 구분하여 입력하시오.")
+        val input = Console.readLine()
+        val carNames = input.split(",").map { it.trim() }
+        require(carNames.all { it.length <= 5 }) { "자동차 이름은 5자 이하가 가능함." }
+        return carNames
+    }
+
+    fun inputAttemptCount(): Int {
+        println("시도할 횟수를 입력하시오.")
+        val input = Console.readLine()
+        val count = input.toIntOrNull() ?: throw IllegalArgumentException("시도 횟수는 양의 정수여야 함.")
+        require(count > 0) { "시도 횟수는 양의 정수여야 함." }
+        return count
+    }
 }
