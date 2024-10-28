54b81094a4364b45b8094481c61de70a5dbcbab6
SANDY <nnal0256@naver.com>
2024-10-28 14:32:01 +0900

    feat: 참가할 자동차 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+    val carInput = getInputCarList()
+
+}
+
+private const val INPUT_PLAY_CAR_STR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+fun getInputCarList(): String {
+    println(INPUT_PLAY_CAR_STR)
+    val input = Console.readLine()
+    Console.close()
+    return input
 }
