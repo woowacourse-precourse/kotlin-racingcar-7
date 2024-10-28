6dc66d03eb6bfec4b73cfb9fe8336b19bdf41672
minseok0416 <day5274567@gmail.com>
2024-10-28 21:01:04 +0900

    feat(RacingGame): 전진 조건 만족 여부에 따라 차수별 실행 결과를 출력하는 기능 추가

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -20,4 +20,28 @@ class RacingGame(val inputNumber: String) {
         NumericCheck()
         MinCheck()
     }
+
+    fun gameStart(racingCars: MutableMap<String, Int>, number: Int) {
+        var finalLog = ""
+        // depth 가 3이므로 분리 필요
+        for (i in 1..number) {//1. number 만큼 반복
+            // 아래 for 문을 number 만큼 반복한다
+            for (racingCar in racingCars.keys) {//2. racingCars 의 키 갯수 만큼 반복
+                // 아래 코드를 자동차 수만큼 반복한다
+                val condition = Randoms.pickNumberInRange(0, 9)//3-1. 전진 조건에 따라 racingCar 의 value +1
+                if (condition >= 4) {
+                    racingCars[racingCar] = racingCars.getOrDefault(racingCar, 0) + 1
+                }
+
+                var racingLog = "$racingCar : "//3-2. 전진 횟수 만큼 "-" 추가
+                val movements = racingCars[racingCar] ?: 0
+                for (j in 1..movements) {
+                    racingLog += "-"
+                }
+                finalLog = finalLog + racingLog + "\n"
+            }
+            finalLog += "\n"
+        }
+        println(finalLog)
+    }
 }
