1680a772bad516a7c43b4570c4668563f9fc25f6
jusungwon <tjddnjspower@naver.com>
2024-10-28 19:29:37 +0900

    feat(initialize): 맵 컬렉션에 Key은 자동차 이름, Value값은 전진한 횟수로 사용할 수 있도록 초기화

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,7 +6,9 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNames = inputCarNames()
     println("시도할 횟수는 몇 회인가요?")
-    val moveCount = inputMoveCount()
+    val tryCount = inputMoveCount()
+
+    val carPositions = initializeCarPositions(carNames)
 }
 
 
@@ -18,11 +20,21 @@ private fun userInput(): String {
 private fun inputCarNames(): List<String> {
     val userInput = userInput()
     val carNames = userInput.split(",")
-    return carNames
+    val trimmedCarNames = carNames.map { it.trim() }
+    return trimmedCarNames
 }
 
 private fun inputMoveCount(): Int {
     val userInput = userInput()
     val moveCount = userInput.toInt()
     return moveCount
+}
+
+
+private fun initializeCarPositions(carNames: List<String>): MutableMap<String, Int> {
+    val carPositions = mutableMapOf<String, Int>()
+    for (carName in carNames) {
+        carPositions[carName] = 0
+    }
+    return carPositions
 }
