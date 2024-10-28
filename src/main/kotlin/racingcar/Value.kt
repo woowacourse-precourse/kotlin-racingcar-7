74528e611d0cd33b8762eff5bb90395947de5ce0
jklom0326 <jklom0326@gmail.com>
2024-10-28 16:05:25 +0900

    refactor: 문자열 상수 파일 생성

---
+++ src/main/kotlin/racingcar/Value.kt
@@ -0,0 +1,21 @@
+package racingcar
+
+// String 값
+const val DASH = "-"
+const val COMMA = ','
+const val EXECUTION_RESULT = "\n실행 결과"
+const val INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+const val INPUT_ROUND_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+const val WINNER_FRONT_MESSAGE = "최종 우승자 : "
+
+// Validation Error Message
+const val CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다."
+const val CAR_NAME_BLANK_ERROR_MESSAGE = "자동차 이름이 공백일 수 없습니다."
+const val CAR_NAME_ONLY_ONE_ERROR_MESSAGE = "자동차 이름은 2대 이상 입력해 주세요."
+const val CAR_NAME_DUPLICATED_ERROR_MESSAGE = "중복된 이름은 작성할 수 없습니다."
+const val ROUND_COUNT_NOT_NUMBER_ERROR_MESSAGE = "숫자가 아닙니다."
+const val ROUND_COUNT_NEGATIVE_INTEGER_ERROR_MESSAGE = "1보다 작은 수는 입력할 수 없습니다."
+
+// Number 값
+
+const val POSSIBLE_NUMBER = 4
