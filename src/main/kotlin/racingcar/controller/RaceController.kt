480a280f4e732bd4bf8e943878f80bb8ea4d6460
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 20:40:37 +0900

    feat(RaceController): Create moveCar() function to move car
    - Called Car.move() with random value (1~9)

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,5 +1,6 @@
 package racingcar.controller
 
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 import racingcar.model.Car
 import racingcar.model.Race
 
@@ -9,4 +10,5 @@ class RaceController {
     private val cars = race.cars
 
     private fun makeCar() = inputController.getCarNames().forEach { cars.add(Car(it)) }
+    private fun moveCar() = cars.forEach { it.move(pickNumberInRange(1, 9)) }
 }
