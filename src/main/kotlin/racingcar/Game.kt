cf366242f3c4683440eae18861a64cc456e3c25b
eden <devfeijoa@gmail.com>
2024-10-28 19:39:55 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -3,12 +3,26 @@ package racingcar
 import camp.nextstep.edu.missionutils.Console
 
 class Game {
-    fun inputData(){
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        GameManager.generateCars(Console.readLine())
-        println("시도할 횟수는 몇 회인가요?")
-        GameManager.setAttemptTimes(Console.readLine())
+    companion object {
+        const val INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val INPUT_ATTEMPT_TIMES_MESSAGE = "시도할 횟수는 몇 회인가요?"
+        const val WINNERS_ANNOUNCEMENT_MESSAGE = "최종 우승자 : "
+    }
+
+    fun inputData() {
+        inputCarNames()
+        inputAttemptTimes()
         GameManager.getRound()
+    }
+
+    private fun inputCarNames() {
+        println(INPUT_CAR_NAMES_MESSAGE)
+        GameManager.generateCars(Console.readLine())
+    }
+
+    private fun inputAttemptTimes() {
+        println(INPUT_ATTEMPT_TIMES_MESSAGE)
+        GameManager.setAttemptTimes(Console.readLine())
     }
 
     fun play() {
@@ -16,7 +30,7 @@ class Game {
     }
 
     fun printWinners() {
-        val winners = GameManager.getWinners().joinToString(separator = ",")
-        println("최종 우승자 : $winners")
+        val winners = GameManager.getWinners().joinToString(separator = ", ")
+        println("$WINNERS_ANNOUNCEMENT_MESSAGE$winners")
     }
 }
