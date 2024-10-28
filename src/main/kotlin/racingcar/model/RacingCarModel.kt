1ead4e690987de98d2d3b4e31683563a75c121cb
Jordan <leesoyoung0112@gmail.com>
2024-10-28 14:09:19 +0900

    feat, refactor(RacingCarModel.kt) : add runRace, findWinners & change name

--- src/main/kotlin/racingcar/model/RacingCarModel.kt
+++ src/main/kotlin/racingcar/model/RacingCarModel.kt
@@ -2,8 +2,7 @@ package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class RacingCarModel {
-
+class RacingCarModel{
     private lateinit var carList : List<String>
     private lateinit var carFowards : MutableList<String>
 
@@ -12,14 +11,34 @@ class RacingCarModel {
         carFowards = MutableList(carList.size) {""}
     }
 
-    fun getRaceResult() : Boolean {
+    fun getRaceResult(): Boolean {
         return Randoms.pickNumberInRange(0, 9) >= 4
     }
 
-    fun repeatRace() {
+    fun runRaceAttempt() : String {
+        var eachAttemptResult = ""
         carList.indices.forEach{ index ->
             if (getRaceResult())
                 carFowards[index] += "-"
         }
+        for (i in carList.indices) {
+            eachAttemptResult += "${carList[i]} : ${carFowards[i]}\n"
+        }
+        return eachAttemptResult+"\n"
     }
+    fun findWinners(): String {
+        val max = carFowards.max().length
+        return carList.filterIndexed{index, _ -> carFowards[index].length == max}.joinToString(",")
+    }
+
+    fun runRace(inputCarList: String, attempts: Int): Pair<String, String> {
+        var attemptResult = ""
+        seperateCarNames(inputCarList)
+        repeat(attempts) {
+            attemptResult += runRaceAttempt()
+        }
+        val winners = findWinners()
+        return Pair(attemptResult, winners)
+    }
+
 }
