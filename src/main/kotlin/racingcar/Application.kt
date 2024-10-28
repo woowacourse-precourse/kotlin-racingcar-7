57f61f8bb7a9b3ed1f5317406fca89480ee8930c
chaemina <chaemina82@naver.com>
2024-10-28 16:59:13 +0900

    feat(Application): 경주 자동차 이름 입력 기능 추가
    사용자가 경주할 자동차의 이름을 쉼표로 구분해 입력하도록 구현함
    - `readCarNames` 함수를 통해 입력된 문자열을 처리하고 이름을 배열로 반환
    - `split`과 `map`을 사용해 문자열을 분리하고, `trim`으로 공백 제거
    - 모든 이름의 길이를 `all` 메소드로 검증하며, `require`로 유효하지 않은 입력에 예외 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    try {
+        val carNames = readCarNames()
+
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException("입력이 잘못되었습니다. 프로그램을 종료합니다.")
+    }
+}
+
+private fun readCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = Console.readLine()
+    val names = input.split(",").map { it.trim() }
+
+    require(names.all { it.length <= 5 }) {
+        "자동차 이름은 5자 이하만 가능합니다."
+    }
+
+    return names
 }
