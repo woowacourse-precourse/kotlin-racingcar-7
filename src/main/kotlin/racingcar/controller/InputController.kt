90f01d335a8d04bbf07b211d1945d487ceec600d
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 18:58:30 +0900

    feat(InputController): Create InputController to validate user input
    - Implemented getCarNames() to check the length of car name (1 to 5 characters)
    - Implemented getRaceCount() to validate if the input is a number

---
+++ src/main/kotlin/racingcar/controller/InputController.kt
@@ -0,0 +1,20 @@
+package racingcar.controller
+
+import racingcar.view.Input
+
+class InputController {
+    private val input = Input()
+
+    fun getCarNames(): List<String> {
+        return input.carNames().split(",").map {
+            require(it.length in 1..5) { "자동차 이름은 1 ~ 5자로 입력해주세요. ${it}의 이름은 ${it.length}자 입니다." }
+            it.trim()
+        }
+    }
+
+    fun getRaceCount(): Int {
+        return input.raceCount().let {
+            it.toIntOrNull() ?: throw IllegalArgumentException("경주 횟수는 숫자만 입력 가능합니다. ${it}은 숫자가 아닙니다.")
+        }
+    }
+}
