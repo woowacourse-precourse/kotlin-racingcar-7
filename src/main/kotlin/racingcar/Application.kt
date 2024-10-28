9a0a486a608ba347f91944eb8a38db956c816e5f
jusungwon <tjddnjspower@naver.com>
2024-10-28 17:10:57 +0900

    feat(input): 사용자에게 문자열을 입력받는 기능

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = inputCarNames()
+    println("시도할 횟수는 몇 회인가요?")
+    val moveCount = inputMoveCount()
+}
+
+
+private fun userInput(): String {
+    val userInput = Console.readLine()
+    return userInput
+}
+
+private fun inputCarNames(): List<String> {
+    val userInput = userInput()
+    val carNames = userInput.split(",")
+    return carNames
+}
+
+private fun inputMoveCount(): Int {
+    val userInput = userInput()
+    val moveCount = userInput.toInt()
+    return moveCount
 }
