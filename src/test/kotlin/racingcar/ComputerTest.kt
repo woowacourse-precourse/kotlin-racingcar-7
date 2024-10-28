d77da086ffea8a1795ea349e8a511f83f58c31a1
jklom0326 <jklom0326@gmail.com>
2024-10-28 16:44:04 +0900

    test: 우승자 출력 메시지 테스트(우승자 여러명)

--- src/test/kotlin/racingcar/ComputerTest.kt
+++ src/test/kotlin/racingcar/ComputerTest.kt
@@ -30,4 +30,34 @@ class ComputerTest {
         val winnerResult = game.determinedWinners(cars)
         assertEquals("최종 우승자 : welch", computer.makeWinnerMessage(winnerResult))
     }
+
+    @Test
+    fun `경주 테스트(우승자 2명)`() {
+        val cars = listOf(
+            Car("coca"),
+            Car("cider"),
+            Car("welch"),
+            Car("fanta"),
+            Car("pepsi"),
+        )
+        val roundCounts = 5
+
+        val randomNumbers1 = listOf(1, 2, 3, 4, 5)
+        val randomNumbers2 = listOf(2, 3, 4, 5, 6)
+        val randomNumbers3 = listOf(3, 4, 5, 6, 7)
+        val randomNumbers4 = listOf(3, 4, 5, 6, 7)
+        val randomNumbers5 = listOf(3, 4, 5, 6, 7)
+
+        repeat(roundCounts) {
+            cars[0].moving(randomNumbers1[it])
+            cars[1].moving(randomNumbers2[it])
+            cars[2].moving(randomNumbers3[it])
+            cars[3].moving(randomNumbers4[it])
+            cars[4].moving(randomNumbers5[it])
+        }
+
+        val winnerResult = game.determinedWinners(cars)
+
+        assertEquals("최종 우승자 : welch, fanta, pepsi", computer.makeWinnerMessage(winnerResult))
+    }
 }
