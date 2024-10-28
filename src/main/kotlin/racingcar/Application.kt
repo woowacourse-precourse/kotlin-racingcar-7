296006f54f585d7c243dd1216f466dadc748aeac
Seongdeok Hong <sdhong0609@gmail.com>
2024-10-28 18:26:48 +0900

    refactor: 시도 횟수 입력받기 기능 분리
    자동차 경주 시도 횟수 입력받기 InputCount 클래스로 분리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,8 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
+import racingcar.feature.InputCount
 import racingcar.feature.InputNames
 import racingcar.feature.RacingCarGame
-import racingcar.utils.isCountInvalid
 
 fun main() {
     val inputNames = InputNames()
@@ -13,16 +12,18 @@ fun main() {
         validateNames()
     }
 
-    println("시도할 횟수는 몇 회인가요?")
-    val countInput = Console.readLine()
-    if (countInput.isCountInvalid()) throw IllegalArgumentException()
+    val inputCount = InputCount()
+    inputCount.run {
+        printInputCountMessage()
+        readCount()
+        validateCount()
+    }
 
     println()
 
-    val racingCarGame = RacingCarGame(inputNames.names)
-
     println("실행 결과")
-    repeat(countInput.toInt()) {
+    val racingCarGame = RacingCarGame(inputNames.names)
+    repeat(inputCount.count.toInt()) {
         racingCarGame.moveCars()
         racingCarGame.printResults()
         println()
