c1d025402fa3559bbd06599ad6c3158ec63abae1
ErroredPasta <ForeignServices3132@pm.me>
2024-10-28 18:38:09 +0900

    test: 자동차 이름 생성에 대한 테스트 추가
    원래는 CarTest에 자동차 이름에 관한 테스트를 같이 작성할 계획이였지만
    따로 테스트를 하는 것이 더 옳다 판단하여 테스트를 분리

---
+++ src/test/kotlin/racingcar/domain/CarNameTest.kt
@@ -0,0 +1,34 @@
+package racingcar.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import racingcar.domain.CarName.Companion.asCarName
+
+class CarNameTest {
+    @Test
+    fun `5글자 이하시 정상적으로 CarName 생성`() {
+        // act, assert
+        assertDoesNotThrow { "pobi".asCarName() }
+    }
+
+    @Test
+    fun `5글자 초과시 예외 발생`() {
+        // act, assert
+        assertThrows<IllegalArgumentException> { "long car name".asCarName() }
+    }
+
+    @Test
+    fun `toString시 정상적으로 지정한 이름의 문자열이 반환`() {
+        // arrange
+        val name = "pobi"
+        val carName = name.asCarName()
+
+        // act
+        val result = carName.toString()
+
+        // assert
+        assertThat(result).isEqualTo(name)
+    }
+}
