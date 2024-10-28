238d8b7dcb204a649f2e655cd82639ab3cca0a1f
chaemina <chaemina82@naver.com>
2024-10-28 17:55:08 +0900

    feat(RacingGame): 레이싱 게임 로직 구현
    RacingGame 클래스를 생성하여 경주에 관한 기능 관리하도록 구현함
    - 생성자에서 자동차 목록과 시도 횟수를 받아 초기화
    - `race` 함수에서 각 자동차에 대해 랜덤 값을 생성하고, `move`함수로 전달

---
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -0,0 +1,13 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class RacingGame(private val cars: List<Car>, private val attempts: Int) {
+
+    private fun race() {
+        cars.forEach { car ->
+            val randomValue = Randoms.pickNumberInRange(0, 9)
+            car.move(randomValue)
+        }
+    }
+}
