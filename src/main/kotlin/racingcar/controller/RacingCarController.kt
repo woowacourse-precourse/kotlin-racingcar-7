1010801daa0a1cad1cf7e6177476dd935ccd855b
이지윤 <02leejiyun@naver.com>
2024-10-28 16:55:42 +0900

    refactor: InputView와 OutputView를 object로 변경

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -4,11 +4,9 @@ import racingcar.domain.RacingGame
 import racingcar.view.InputView
 
 class RacingCarController {
-    private val inputView: InputView = InputView()
-
     fun start() {
-        val carNames: List<String> = inputView.inputCarNames()
-        val gameRound: Int = inputView.inputGameRound()
+        val carNames: List<String> = InputView.inputCarNames()
+        val gameRound: Int = InputView.inputGameRound()
         val racingGame = RacingGame(carNames, gameRound)
         racingGame.play()
     }
---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,9 @@
+package racingcar.view
+
+object OutputView {
+    fun printExecutionResult() {
+        println(OUTPUT_EXECUTION_RESULT_MESSAGE)
+    }
+
+    private const val OUTPUT_EXECUTION_RESULT_MESSAGE = "실행 결과"
+}
