a6b35b42d526524162d01ed6c66da908e3f6d354
souk0712 <souk0712@gmail.com>
2024-10-28 23:05:25 +0900

    feat: Race 클래스에 경주 실행 로직 추가
    - 경주를 실행하고 모든 경기 현황을 반환하는 함수
    - 시도 횟수 만큼 자동차를 움직이는 함수

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -9,6 +9,17 @@ class Race(private val tryCount: Int, private val cars: List<Car>) {
         require(tryCount >= 0) { WRONG_TRY_COUNT_MESSAGE }
     }
 
+    fun run() = (0 until tryCount).map {
+        moveCar()
+        cars
+    }
+
+    private fun moveCar() {
+        cars.forEach { car ->
+            car.move()
+        }
+    }
+
     companion object {
         private const val WRONG_TRY_COUNT_MESSAGE = "시도 횟수는 0이상이어야 합니다."
     }
