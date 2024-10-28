f1f00a767e5d258a356ac7be43fc9196ff01b56a
Jieun <lje2000lje@naver.com>
2024-10-28 20:23:31 +0900

    feat(ResultView.kt): 라운드 별 진행상황 출력 함수 구현
    라운드 별 '자동차 이름 : -- (이동 거리)' 출력

--- src/main/kotlin/racingcar/view/ResultView.kt
+++ src/main/kotlin/racingcar/view/ResultView.kt
@@ -1,5 +1,13 @@
 package racingcar.view
 
+import racingcar.model.Car
+
 class ResultView {
 
+    fun printRoundResult(cars: List<Car>) {
+        cars.forEach { car ->
+            println("${car.name} : ${"-".repeat(car.distance)}")
+        }
+        println()
+    }
 }
