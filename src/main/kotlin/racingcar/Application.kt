eb13fc3bce7164bb3cb32ef073afee75213f4f12
DaeYoung Jeong <eodud1025@naver.com>
2024-10-28 13:19:40 +0900

    feat: 사용자 입력 공백 예외 처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+
+    try {
+        val carNames = readLine()
+        checkIsEmpty(carNames)
+    } catch (e: Exception) {
+        throw IllegalArgumentException(e.message)
+    }
+
+}
+fun checkIsEmpty(s: String): Boolean {
+    if (s.isBlank()) {
+        throw IllegalArgumentException("자동차 이름을 입력해주세요.")
+    }
+    return true
 }
