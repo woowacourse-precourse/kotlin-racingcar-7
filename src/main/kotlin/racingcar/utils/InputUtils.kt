e7debffcd1b78472bbd4305b69dee504dd795693
SANDY <nnal0256@naver.com>
2024-10-28 18:50:49 +0900

    add: 자동차 이름, 실시 횟수 입력 예외처리

--- src/main/kotlin/racingcar/utils/InputUtils.kt
+++ src/main/kotlin/racingcar/utils/InputUtils.kt
@@ -21,7 +21,24 @@ object InputUtils {
      * @return 분리한 이름 리스트(List)
      */
     private fun String.toCarList(): List<String> {
-        return split(INPUT_CARS_SEPARATOR).map { it.trim() }
+        return split(INPUT_CARS_SEPARATOR).map {
+            val name = it.trim()
+            checkValidCarName(name)
+            name
+        }.filter {
+            it.isBlank()
+        }
+    }
+
+    private const val VALID_CAR_NAME_MAX_LENGTH = 5
+    /**
+     * 자동차 이름이 유효한 양식인지 확인한다. 이름은 반드시 [VALID_CAR_NAME_MAX_LENGTH] 이하만 가능하다.
+     * @throws IllegalArgumentException
+     */
+    private fun checkValidCarName(name: String) {
+        if(name.length > VALID_CAR_NAME_MAX_LENGTH) {
+            throw IllegalArgumentException("자동차 이름은 ${VALID_CAR_NAME_MAX_LENGTH}자를 넘길 수 없습니다.")
+        }
     }
 
     private const val INPUT_GAME_ROUND_STR = "시도할 횟수는 몇 회인가요?"
@@ -37,7 +54,11 @@ object InputUtils {
     }
 
     private fun String.toGameCount(): Int {
-        return trim().toInt()
+        return try {
+            trim().toInt()
+        } catch (e: Exception) {
+            throw IllegalArgumentException("숫자만 입력 가능합니다.")
+        }
     }
 
 }
