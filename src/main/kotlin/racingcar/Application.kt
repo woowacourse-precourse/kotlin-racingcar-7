06b42f6cc084de554618bcf182f4be6780f8fe85
joo <yjy010222gmail.com>
2024-10-28 20:46:22 +0900

    feat: 자동차 경주 점수 배열 생성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,4 +6,7 @@ import racingcar.view.racenuminputview
 fun main() {
     val carname = carnameinputview()
     val racenum = racenuminputview()
+
+    val carscore = Array<Int>(carname.size,{0})
+
 }
