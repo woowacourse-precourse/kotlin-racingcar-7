98a7ae4fd5c776c27d5eaad899032b9faed5afd4
minseok0416 <day5274567@gmail.com>
2024-10-28 16:30:13 +0900

    feat(RacingCar): 잘못된 자동차 이름을 입력한 경우 에러를 발생시키는 기능 추가

--- src/main/kotlin/racingcar/RacingCar.kt
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -1,5 +1,23 @@
 package racingcar
 
 class RacingCar(val name: String, val number: String) {
+    val racingCars = name.split(",")
+    fun lengthCheck() {
+        for (racingCar in racingCars) {
+            if (racingCar.length > 5) {
+                throw IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.")
+            }
+        }
+    }
 
+    fun numberCheck() {
+        if (racingCars.size < 2) {
+            throw IllegalArgumentException("[ERROR] 경기 진행을 위해서는 최소 2대의 자동차를 입력해야 합니다.")
+        }
+    }
+
+    fun validateName() {
+        lengthCheck()
+        numberCheck()
+    }
 }
--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -3,6 +3,7 @@ package racingcar
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class RacingCarTest {
     @Test
@@ -12,5 +13,21 @@ class RacingCarTest {
         val racingCar = RacingCar(inputName, inputRound)
         assertThat(racingCar.name).isEqualTo("pobi,woni,jun")
         assertThat(racingCar.number).isEqualTo("5")
+    }
+
+    @Test
+    fun `이름이 6자 이상인 경우 에러가 발생한다`() {
+        val lengthOver = RacingCar("abcdef,abc", "5")
+        assertThrows<IllegalArgumentException> {
+            lengthOver.lengthCheck()
+        }
+    }
+
+    @Test
+    fun `2대 미만의 자동차를 입력한 경우 에러가 발생한다`() {
+        val lessThanTwo = RacingCar("abc", "5")
+        assertThrows<IllegalArgumentException> {
+            lessThanTwo.numberCheck()
+        }
     }
 }
