c69a2ba898de0cd45145a488d521e0283ce0a9d9
ErroredPasta <ForeignServices3132@pm.me>
2024-10-28 22:53:04 +0900

    test: ApplicationTest에 테스트 케이스 추가

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
-    fun `기능 테스트`() {
+    fun `단독 우승자 기능 테스트`() {
         assertRandomNumberInRangeTest(
             {
                 run("pobi,woni", "1")
@@ -20,9 +20,55 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `예외 테스트`() {
+    fun `복수 우승자 기능 테스트`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni")
+            },
+            MOVING_FORWARD, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    fun `이름이 5글자를 초과할 경우 예외 발생`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
+    @Test
+    fun `이름을 지정하지 않은 경우 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,,woni", "1") }
+        }
+    }
+
+    @Test
+    fun `시도 횟수를 0으로 지정시 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "0") }
+        }
+    }
+
+    @Test
+    fun `시도 횟수를 음수로 지정시 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "-1") }
+        }
+    }
+
+    @Test
+    fun `시도 횟수를 실수로 지정시 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "4.3") }
+        }
+    }
+
+    @Test
+    fun `시도 횟수 입력으로 문자열 입력시 예외 발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "asdf") }
         }
     }
 
