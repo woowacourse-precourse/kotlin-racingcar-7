e578cd0f105c23bd23fa32633803c459d3c79bf4
gixxla <gigixquisite@gmail.com>
2024-10-28 12:31:06 +0900

    feat: 예외 처리 기능 추가
    아래 상황이 발생했을 시 IllegalArgumentException 예외 발생
    - 사용자가 입력한 자동차의 이름이 5글자 초과
    - 시도할 횟수를 입력할 때 정수 외의 다른 문자가 입력됨

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,8 +6,20 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val cars = Console.readLine().split(",")
+    for (i in cars.indices) {
+        if (cars[i].count() > 5) {
+            throw IllegalArgumentException("잘못된 입력입니다. (자동차의 이름은 5자 이하여야 합니다.)")
+        }
+    }
+
     println("시도할 횟수는 몇 회인가요?")
-    val count: Int = Console.readLine().toInt()
+    val count: Int
+    try {
+        count = Console.readLine().toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("잘못된 입력입니다. (횟수는 정수로만 입력되어야 합니다.)")
+    }
+
     println()
 
     var race = ArrayList<String>()
