1e03046601e1b3ac497b2a36d2275c0db5d4e096
DoableTuple <doabletuple@gmail.com>
2024-10-28 11:27:16 +0900

    feat: 난수에 따라 자동차를 움직이는 기능 추가

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,11 +1,14 @@
 package racingcar.controller
 
 import racingcar.view.InputView
+import racingcar.model.Cars
 
 class RaceController {
     fun run() {
         val inputView = InputView()
         val carNames = inputView.readCarNames()
         val trialCount = inputView.readTrialCount()
+
+        val cars = Cars(carNames)
     }
 }
--- src/main/kotlin/racingcar/model/Cars.kt
+++ src/main/kotlin/racingcar/model/Cars.kt
@@ -1,5 +1,14 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 data class Cars(val carNames: List<String>) {
     val cars: List<Car> = carNames.map { Car(it) }
+
+    fun move() {
+        for (car in cars) {
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            if (randomNumber >= 4) car.moveForWard()
+        }
+    }
 }
