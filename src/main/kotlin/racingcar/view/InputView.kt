b63d33ea75b7ced23db9dde2070f73e2e633d072
Jieun <lje2000lje@naver.com>
2024-10-28 18:57:39 +0900

    feat(InputView.kt): 자동차 이름 입력 함수 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,5 +1,15 @@
 package racingcar.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
 
+    fun inputCarNames() {
+        println(MESSAGE_ENTER_CAR_NAMES)
+        val input = Console.readLine().orEmpty()
+    }
+
+    companion object {
+        const val MESSAGE_ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    }
 }
