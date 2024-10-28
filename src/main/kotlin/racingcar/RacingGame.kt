caa054bfaefcebb323d86af3d5fa0c5a468d9ac4
PIYUJIN <yjyj020201@gmail.com>
2024-10-28 10:05:41 +0900

    feat($CheckInput): 사용자가 잘못된 값을 입력할 경우 예외처리 기능 구현
    - 경주할 자동차 이름을 입력하지 않은 경우 (공백 입력)
    - 자동차 이름이 5자 초과인 경우
    - 시도할 횟수 입력시 정수가 아닌 다른 값이 입력된 경우
    - 시도할 횟수 입력시 음수가 입력된 경우

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -5,12 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class RacingGame {
 
+    private val checkInput = CheckInput()
 
     fun initGame() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val cars = Console.readLine()
 
+        val finalCarList = checkInput.checkParticipant(cars)
+
         println("시도할 횟수는 몇 회인가요?")
         val numOfGame = Console.readLine()
+
+        val trial = checkInput.checkTrial(numOfGame)
     }
 }
