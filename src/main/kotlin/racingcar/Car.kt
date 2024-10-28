59cb0eadbc10fec4c47fba9f0208d6c758954f2d
souk0712 <souk0712@gmail.com>
2024-10-28 22:35:21 +0900

    feat: Car 클래스의 유효성 검사 로직 추가
    - 이름의 제약사항 추가

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,4 +4,17 @@ class Car(val name: String) {
 
     private var _goState: String = ""
     val goState: String get() = _goState
+
+    init {
+        checkValidate()
+    }
+
+    private fun checkValidate() {
+        require(name.length <= NAME_LIMIT) { WRONG_NAME_MESSAGE }
+    }
+
+    companion object {
+        private const val NAME_LIMIT = 5
+        private const val WRONG_NAME_MESSAGE = "자동차 이름은 5자 이하만 가능합니다."
+    }
 }
