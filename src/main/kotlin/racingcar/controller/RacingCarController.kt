456d864ba0442b2b2f5809fd86503cc16f2cfffb
Jordan <leesoyoung0112@gmail.com>
2024-10-28 14:11:12 +0900

    feat(RacingCarController.kt) : add race logic to controller

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -10,5 +10,8 @@ class RacingCarController (
     fun run() {
         val inputCarList = view.getCarName()
         val inputAttempts = view.getAttempts()
+        val result = model.runRace(inputCarList, inputAttempts)
+        view.displayAttemptResult(result.first)
+        view.displayResult(result.second)
     }
 }
