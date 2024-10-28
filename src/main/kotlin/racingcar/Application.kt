f8e5637b51705cacdae6c9c5cf2a0403ccd89b6e
Gumraze-git <galaxydh4110@gmail.com>
2024-10-28 17:35:08 +0900

    feat(racing): update racing feature
    Generate a random list with "generateRandomList" based on the racer's count.
    Determine racer movement with "processList" based on random list.
    Start and display current race with "startRace" for each racer.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun checkRacerName(name: String): Pair<Boolean, List<String>> {
     val racerNames = name.split(",")
@@ -23,10 +24,28 @@ fun checkRaceAttempts(attempts: String): Int {
     return attemptCount
 }
 
+fun generateRandomList(racers: List<String>): List<Int> {
+    return racers.map { Randoms.pickNumberInRange(0, 9) }
+}
+
+fun processList(randomList: List<Int>, moving: List<Int>): List<Int> {
+    return randomList.mapIndexed { index, value ->
+        if (value >= 4) moving[index] + 1 else moving[index]
+    }
+}
+
+fun startRace(racerName: List<String>, racerMove: List<Int>) {
+    for (racer in 0 until racerName.size) {
+        println("${racerName[racer]} : ${"-".repeat(racerMove[racer])}")
+    }
+}
+
+
 fun main() {
     // TODO: 프로그램 구현
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val name = Console.readLine()
+
     println("시도할 횟수는 몇 회인가요?")
     val attempts = Console.readLine()
 
@@ -35,5 +54,15 @@ fun main() {
 
     if (isRacerNameValid) {
         println("자동차 경주를 시작합니다.")
+        var racerMove = racerName.map { 0 }
+        var count = 0
+
+        while (count < maxAttemptCount) {
+            val randomList = generateRandomList(racerName)
+            racerMove = processList(randomList, racerMove)
+            startRace(racerName, racerMove)
+            count ++
+        }
     }
 }
+
