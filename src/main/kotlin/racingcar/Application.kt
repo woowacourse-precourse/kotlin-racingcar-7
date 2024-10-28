94a2f66783eeb066ad2aa9491054310556c5c71b
thsamajiki <ant1102@naver.com>
2024-10-29 00:43:55 +0900

    feat: 이동한 횟수 입력받기 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,4 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
   println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
   val input = Console.readLine()
+  
+  println("시도할 횟수는 몇 회인가요?")
+  val count = Console.readLine()
 }
