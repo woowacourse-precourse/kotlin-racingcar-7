bc1d2afad4ec2d4b281d11950251a8e4859f9589
jklom0326 <jklom0326@gmail.com>
2024-10-28 16:46:12 +0900

    refactor: 문자열 상수값 추가

--- src/main/kotlin/racingcar/Value.kt
+++ src/main/kotlin/racingcar/Value.kt
@@ -7,6 +7,7 @@ const val EXECUTION_RESULT = "\n실행 결과"
 const val INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 const val INPUT_ROUND_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
 const val WINNER_FRONT_MESSAGE = "최종 우승자 : "
+const val COMMA_AND_BLANK = ", "
 
 // Validation Error Message
 const val CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다."
