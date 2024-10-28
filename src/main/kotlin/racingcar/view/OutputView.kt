99ec1d2347b1b3365ca6b5af37b5e58e9d5782b2
nuyhhyun <sarahs19991115@gmail.com>
2024-10-28 23:11:38 +0900

    feat(Output): 우승자 출력 기능 구현
    최종 우승자 출력 기능 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -21,9 +21,16 @@ class OutputView {
         }
     }
 
+    fun printWinner(winners: List<Car>) {
+        val nameOfWinners = winners.joinToString(CAR_NAME_SPLIT_POINT) { it.name }
+        println(FINAL_WINNERS + nameOfWinners)
+    }
+
     companion object {
-        const val ENTER_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-        const val ENTER_TRY_COUNTS = "시도할 횟수는 몇 회인가요?"
-        const val ONE_STEP = "-"
+        private const val ENTER_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        private const val ENTER_TRY_COUNTS = "시도할 횟수는 몇 회인가요?"
+        private const val ONE_STEP = "-"
+        private const val FINAL_WINNERS =  "최종 우승자: "
+        private const val CAR_NAME_SPLIT_POINT = ","
     }
 }
