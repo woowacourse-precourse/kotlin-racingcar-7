23b9ac78c17c8bc279e42ffef70870f38561f0f4
jusungwon <tjddnjspower@naver.com>
2024-10-28 20:15:57 +0900

    feat(move): 입력받은 시도횟수 만큼 자동차 전진 출력

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,14 +1,18 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNames = inputCarNames()
+
     println("시도할 횟수는 몇 회인가요?")
     val tryCount = inputMoveCount()
 
     val carPositions = initializeCarPositions(carNames)
+    println("실행 결과")
+    startRacing(carPositions, tryCount)
 }
 
 
@@ -37,4 +41,36 @@ private fun initializeCarPositions(carNames: List<String>): MutableMap<String, I
         carPositions[carName] = 0
     }
     return carPositions
+}
+
+
+private fun startRacing(carPositions: MutableMap<String, Int>, tryCount: Int) {
+    repeat(tryCount) {
+        moveCars(carPositions)
+        printCurrentPositions(carPositions)
+        println()
+    }
+}
+
+private fun moveCars(carPositions: MutableMap<String, Int>) {
+    carPositions.keys.forEach { carName ->
+        moveCar(carName, carPositions)
+    }
+}
+
+private fun moveCar(carName: String, carPositions: MutableMap<String, Int>) {
+    if (randomMove()) {
+        carPositions[carName] = carPositions.getValue(carName) + 1
+    }
+}
+
+private fun printCurrentPositions(carPositions: Map<String, Int>) {
+    carPositions.forEach { (carName, position) ->
+        println("$carName : ${"-".repeat(position)}")
+    }
+}
+
+private fun randomMove(): Boolean {
+    val randomNumber = Randoms.pickNumberInRange(0, 9)
+    return randomNumber >= 4
 }
