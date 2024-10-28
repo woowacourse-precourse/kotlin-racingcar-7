e1a88bf6d37559c5983040872c795af989d137bf
SANDY <nnal0256@naver.com>
2024-10-28 23:23:29 +0900

    add: 정상임에도 발생한 예외 수정
    input 값이 "xxx,"형태일 경우 정상임에도 발생한 IllegalArgumentException 수정

--- src/main/kotlin/racingcar/utils/ValidCheckUtils.kt
+++ src/main/kotlin/racingcar/utils/ValidCheckUtils.kt
@@ -12,7 +12,7 @@ object ValidCheckUtils {
             throw IllegalArgumentException("자동차 이름은 ${VALID_CAR_NAME_MAX_LENGTH}자를 넘길 수 없습니다.")
         }
         // 주의: input 값이 "xxx,"형태일 경우 index가 1일 때 name은 Empty형태지만 이 경우 예외가 발생해서는 안된다.
-        if(name.isEmpty() && index > 1) {
+        if(name.isEmpty() && index != 1) {
             throw IllegalArgumentException("유효한 자동차 이름이 아닙니다.")
         }
     }
