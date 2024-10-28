d6befab17078a7935f51215ccb18ae9da8a4b679
joo <yjy010222gmail.com>
2024-10-28 20:56:36 +0900

    feat: 경기 1회마다 경기 현황 출력

--- src/main/kotlin/racingcar/view/outputview.kt
+++ src/main/kotlin/racingcar/view/outputview.kt
@@ -6,5 +6,15 @@ fun raceoutput(carname: List<String>, racenum: Int, carscore: Array<Int>) {
     println("\n실행 결과")
     for(race: Int in 0..<racenum){
         carrandomgo(carname,carscore)
+        raceprint(carname,carscore)
     }
+}
+
+fun raceprint(carname: List<String>, carscore: Array<Int>) {
+    for(scoreindex: Int in carname.indices) {
+        val car = carname[scoreindex]
+        print("$car : ")
+        println("-".repeat(carscore[scoreindex]))
+    }
+    println()
 }
