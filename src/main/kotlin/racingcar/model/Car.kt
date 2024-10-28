65ec0ce169ba734c25ec24d8ee0377fdaa30d155
Jinwoo Choi <cjw1234577@gmail.com>
2024-10-28 15:23:41 +0900

    feat(Car): 빈 문자열을 이름으로 가지는 경우 예외 처리 - throw에서 require로 리팩토링 - 에러 메세지에 빈 문자열인 경우 에러 메세지 상수로 추가

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -4,9 +4,8 @@ import racingcar.utils.ErrorMessages
 
 data class Car(val name: String, var position: Int = 0) {
     init {
-        if (name.length > 5 || name.isEmpty()) {
-            throw IllegalArgumentException(ErrorMessages.NAME_LENGTH_ERROR)
-        }
+        require(name.length <= 5) { ErrorMessages.NAME_LENGTH_ERROR }
+        require(name.isNotEmpty()) { ErrorMessages.NAME_EMPTY_ERROR }
     }
 
     fun move() {
--- src/main/kotlin/racingcar/utils/ErrorMessages.kt
+++ src/main/kotlin/racingcar/utils/ErrorMessages.kt
@@ -2,5 +2,6 @@ package racingcar.utils
 
 object ErrorMessages {
     const val NAME_LENGTH_ERROR = "자동차 이름은 5자 이하여야 합니다."
+    const val NAME_EMPTY_ERROR = "자동차 이름은 빈 문자열이 될 수 없습니다."
 
 }
