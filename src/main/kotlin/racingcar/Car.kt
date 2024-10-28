080fbed3d736224a744098785eb6e6b8f2ee9b1f
eden <devfeijoa@gmail.com>
2024-10-28 19:45:27 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,13 +2,19 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 class Car(private val name: String) {
+    companion object {
+        const val MOVE_THRESHOLD = 4
+        const val MAX_RANDOM_NUMBER = 9
+        const val MIN_RANDOM_NUMBER = 0
+    }
+
     private var position = 0
 
     fun getName() = name
     fun getPosition() = position
 
     fun moveForward() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) >= MOVE_THRESHOLD) {
             position++
         }
     }
