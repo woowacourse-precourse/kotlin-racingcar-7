70a4fe35c2a2a359f2f7208f1cd74ca00351f9fe
minseok0416 <day5274567@gmail.com>
2024-10-28 21:46:44 +0900

    refactor(RacingGame): 전진 횟수 만큼 "-" 추가하는 기능 분리

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -24,14 +24,11 @@ class RacingGame(val inputNumber: String) {
     fun gameStart(racingCars: MutableMap<String, Int>, number: Int) {
         var finalLog = ""
         // depth 가 3이므로 분리 필요
-        for (i in 1..number) {//1. number 만큼 반복
-            for (racingCar in racingCars.keys) {//2. racingCars 의 키 갯수 만큼 반복
-                var racingLog = "$racingCar : "//3-2. 전진 횟수 만큼 "-" 추가
+        for (i in 1..number) {
+            for (racingCar in racingCars.keys) {
                 moveCar(racingCars, racingCar)
                 val movements = racingCars[racingCar] ?: 0
-                for (j in 1..movements) {
-                    racingLog += "-"
-                }
+                val racingLog = getRacingLog(racingCar, movements)
                 finalLog = finalLog + racingLog + "\n"
             }
             finalLog += "\n"
@@ -45,6 +42,14 @@ class RacingGame(val inputNumber: String) {
         if (condition >= 4) {
             racingCars[racingCar] = racingCars.getOrDefault(racingCar, 0) + 1
         }
+    }
+
+    fun getRacingLog(racingCar: String, movements: Int): String {
+        var racingLog = "$racingCar : "
+        for (i in 1..movements) {
+            racingLog += "-"
+        }
+        return racingLog
     }
 
     fun printWinner(racingCars: MutableMap<String, Int>) {
