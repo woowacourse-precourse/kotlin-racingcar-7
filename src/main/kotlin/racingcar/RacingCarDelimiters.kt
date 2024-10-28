e51b78b44ae620769d5aa8a0e2ff52468d49ba28
DDangDin <Kcorelec7123@gmail.com>
2024-10-28 17:29:53 +0900

    docs: 주석 이해하기 쉽게 수정

--- src/main/kotlin/racingcar/RacingCarDelimiters.kt
+++ src/main/kotlin/racingcar/RacingCarDelimiters.kt
@@ -2,8 +2,9 @@ package racingcar
 
 /** 자동차 경주 게임에서 사용되는 구분자 */
 object RacingCarDelimiters {
+    /** 이름이 여러 개 일 때 구분하기 위해 사용  */
     const val NAME_DELIMITER = ","
 
-    /** 게임 진행 시 이름을 구분할 때 사용 */
+    /** 게임 진행 시 이름과 '-'(진행표시기호)를 구분하기 위해 사용 */
     const val CAR_NAME_DASH_DELIMITER = ":"
 }
