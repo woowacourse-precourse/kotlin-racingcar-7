5dd14031bf40d7e62bc0866e8010699359ccda9b
eden <devfeijoa@gmail.com>
2024-10-28 19:57:38 +0900

    feat: 쉼표가 없는 입력이 들어왔을때의 테스트 코드

--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -1,8 +1,25 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 class RacingCarTest {
 
+    private lateinit var game: Game
 
+    @BeforeEach
+    fun setup() {
+        game = Game()
+    }
+
+    @Test
+    fun `쉼표가 없는 입력이 들어오면 예외가 발생한다`() {
+        val validation = Validation()
+        assertThatIllegalArgumentException().isThrownBy {
+            validation.validateInputHasComma("pobi jason")
+        }.withMessageContaining("입력에는 쉼표(,)가 포함되어야 합니다.")
+    }
+    
 }
