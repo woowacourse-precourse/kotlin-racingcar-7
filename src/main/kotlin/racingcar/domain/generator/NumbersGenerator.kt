ac4edc333fb464c5cf000190cb3155f34a336598
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-28 23:58:50 +0900

    refactor: 수정하면서 변경된 코드들 포맷팅

--- src/main/kotlin/racingcar/domain/generator/NumbersGenerator.kt
+++ src/main/kotlin/racingcar/domain/generator/NumbersGenerator.kt
@@ -10,6 +10,6 @@ class NumbersGenerator(numbers: List<Int>) : NumberGenerator {
     }
 
     companion object {
-        private const val  EXCEEDED_NUMBER_CALL_EXCEPTION_MESSAGE = "숫자들의 개수보다 많이 호출되었습니다."
+        private const val EXCEEDED_NUMBER_CALL_EXCEPTION_MESSAGE = "숫자들의 개수보다 많이 호출되었습니다."
     }
 }
--- src/main/kotlin/racingcar/domain/generator/RaceRandomGenerator.kt
+++ src/main/kotlin/racingcar/domain/generator/RaceRandomGenerator.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 class RaceRandomGenerator : NumberGenerator {
     override fun generate() = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
 
-    companion object{
+    companion object {
         private const val MIN_RANDOM_NUMBER = 0
         private const val MAX_RANDOM_NUMBER = 9
     }
