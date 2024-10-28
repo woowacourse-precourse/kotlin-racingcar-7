0c4cc46ee2df0de494b32fc659c830229a4f0422
이지윤 <02leejiyun@naver.com>
2024-10-28 16:35:33 +0900

    feat: 랜덤 값이 4 이상인 경우 자동차 전진

--- src/main/kotlin/racingcar/domain/RacingCar.kt
+++ src/main/kotlin/racingcar/domain/RacingCar.kt
@@ -4,13 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class RacingCar(
     private val name: String,
-    private val position: Int = 0
+    private var position: Int = 0
 ) {
     fun play() {
         val randomNumber = getRandomNumber()
+        moveForward(randomNumber)
     }
 
     private fun getRandomNumber(): Int {
         return Randoms.pickNumberInRange(0, 9)
+    }
+
+    private fun moveForward(number: Int) {
+        if (number >= 4) {
+            position++
+        }
     }
 }
