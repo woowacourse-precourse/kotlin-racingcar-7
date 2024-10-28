0050b93d2a0f2546e04654359251850ea65f2b3f
LeeHaiLim <hailim1997@gmail.com>
2024-10-28 17:33:49 +0900

    feat: 출력 형식 변경

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 private const val INPUT_NAMES_PREFIX = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
 private const val INPUT_TURNS_PREFIX = "시도할 횟수는 몇 회인가요?"
-private const val WINNER_FORMAT = "최종 우승자 : %s"
+private const val WINNER_FORMAT = "최종 우승자 : %s\n"
 
 fun main() {
     val reader = MyReader()
