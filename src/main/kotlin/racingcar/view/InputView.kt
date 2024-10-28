7f193416de2a5ef32de0ea9dfd351af089312a13
이지윤 <02leejiyun@naver.com>
2024-10-28 21:32:31 +0900

    feat: 시도할 횟수 입력에 대한 예외 처리

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -9,16 +9,20 @@ object InputView {
     }
 
     fun inputGameRound(): Int {
-        println(INPUT_GAME_ROUND)
-        return Console.readLine().toIntOrNull() ?: throw IllegalArgumentException(
-            ERROR_INVALID_GAME_ROUND
-        )
+        println(INPUT_GAME_ROUND_MESSAGE)
+        val gameRound = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException(ERROR_INVALID_GAME_ROUND)
+        validateGameRound(gameRound)
+        return gameRound
+    }
+
+    private fun validateGameRound(gameRound: Int) {
+        if (gameRound <= 0) {
+            throw IllegalArgumentException(ERROR_INVALID_GAME_ROUND)
+        }
     }
 
     private const val INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    private const val INPUT_GAME_ROUND = "시도할 횟수는 몇 회인가요?"
-
-    private const val CAR_NAME_DELIMITER = ','
-
+    private const val INPUT_GAME_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?"
     private const val ERROR_INVALID_GAME_ROUND = "[ERROR] 0 이상의 정수값을 입력해야 합니다."
+    private const val CAR_NAME_DELIMITER = ','
 }
