b98dced3b602f5ed1f1be824d14c6d3120f906b0
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-10-28 15:33:01 +0900

    refactor(util): 에러 메시지 파일 분리

--- src/main/kotlin/racingcar/util/CarNameValidator.kt
+++ src/main/kotlin/racingcar/util/CarNameValidator.kt
@@ -2,19 +2,16 @@ package racingcar.util
 
 object CarNameValidator {
 
-    private const val INVALID_LENGTH_ERROR = "자동차 이름은 1자 이상, 5자 이하만 가능합니다: "
-    private const val INVALID_DUPLICATE_ERROR = "자동차 이름은 중복될 수 없습니다."
-
     fun validateLength(carName: String) {
         // 이모티콘인 경우에도 5글자 이하만 가능하도록 설정
         if (carName.codePointCount(0, carName.length) > 5 || carName.isBlank()) {
-            throw IllegalArgumentException("$INVALID_LENGTH_ERROR$carName")
+            throw IllegalArgumentException("${ErrorMessages.INVALID_CAR_NAME_LENGTH_ERROR}$carName")
         }
     }
 
     fun validateDuplicate(carNameList: List<String>) {
         if (carNameList.size != carNameList.distinct().size) {
-            throw IllegalArgumentException(INVALID_DUPLICATE_ERROR)
+            throw IllegalArgumentException(ErrorMessages.INVALID_CAR_NAME_DUPLICATE_ERROR)
         }
     }
 
--- src/main/kotlin/racingcar/util/RaceCountValidator.kt
+++ src/main/kotlin/racingcar/util/RaceCountValidator.kt
@@ -2,34 +2,29 @@ package racingcar.util
 
 object RaceCountValidator {
 
-    private const val INVALID_DECIMAL_ERROR = "소수 입력은 허용되지 않습니다: "
-    private const val INVALID_POSITIVE_INTEGER_ERROR = "경주 횟수는 1 이상의 정수여야 합니다: "
-    private const val INVALID_RACE_COUNT_FORMAT_ERROR = "유효한 경주 횟수 형식이 아닙니다."
-    private const val INPUT_EXCEEDS_LIMIT_ERROR = "100,000 이하의 숫자를 입력해 주세요."
-
     fun validatePositiveInteger(input: String): Int {
         return try {
             validateInteger(input)
             val raceCount = input.toInt()
             if (raceCount <= 0) {
-                throw IllegalArgumentException("$INVALID_POSITIVE_INTEGER_ERROR$raceCount")
+                throw IllegalArgumentException("${ErrorMessages.INVALID_RACE_COUNT_POSITIVE_INTEGER_ERROR}$raceCount")
             }
             raceCount
         } catch (e: NumberFormatException) {
-            throw IllegalArgumentException(INVALID_RACE_COUNT_FORMAT_ERROR)
+            throw IllegalArgumentException(ErrorMessages.INVALID_RACE_COUNT_FORMAT_ERROR)
         }
     }
 
     fun validateNumberSize(raceCount: Int) {
         if (raceCount > 100000) {
-            throw IllegalArgumentException(INPUT_EXCEEDS_LIMIT_ERROR)
+            throw IllegalArgumentException(ErrorMessages.INPUT_EXCEEDS_RACE_COUNT_LIMIT_ERROR)
         }
     }
 
     private fun validateInteger(input: String) {
         val doubleValue = input.toDouble()
         if (doubleValue % 1 != 0.0) {
-            throw IllegalArgumentException("$INVALID_DECIMAL_ERROR$input")
+            throw IllegalArgumentException("${ErrorMessages.INVALID_RACE_COUNT_DECIMAL_ERROR}$input")
         }
     }
 }
