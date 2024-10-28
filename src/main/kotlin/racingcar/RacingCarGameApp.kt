25b7522b4587eba236cf517122dbf6d40b427806
Tnalxmsk <alstnwkd990@naver.com>
2024-10-28 23:26:35 +0900

    refactor: RacingCarGameApp 실행 로직 변경
    RacingCarPresenter, RacingGame 개선에 따른 실행 로직 변경

--- src/main/kotlin/racingcar/RacingCarGameApp.kt
+++ src/main/kotlin/racingcar/RacingCarGameApp.kt
@@ -2,22 +2,15 @@ package racingcar
 
 import racingcar.presenter.RacingCarPresenter
 import racingcar.view.InputView
-import racingcar.view.OutputView
 
 class RacingCarGameApp(
     private val inputView: InputView,
-    private val outputView: OutputView,
     private val racingCarPresenter: RacingCarPresenter
 ) {
 
     fun run() {
         val carNames = inputView.readNames()
-        val cars = racingCarPresenter.createRacingCars(carNames)
-
-        val number = inputView.readAttemptCount()
-        val attemptCount = racingCarPresenter.createAttemptCount(number)
-        outputView.printNewLine()
-
-        racingCarPresenter.startRacing(cars, attemptCount, outputView)
+        val attemptCount = inputView.readAttemptCount()
+        racingCarPresenter.startRacing(carNames, attemptCount)
     }
 }
