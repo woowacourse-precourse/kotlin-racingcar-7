04aeb7d0ae9b06927bca7dbbfb93e73c1179b781
nuyhhyun <sarahs19991115@gmail.com>
2024-10-28 22:51:20 +0900

    feat(Output): 현재 경주 상황 출력 기능 구현
    각 시도마다 경주 상황 출력하는 기능 구현

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,7 +1,6 @@
 package racingcar.view
 
-const val ENTER_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-const val ENTER_TRY_COUNTS = "시도할 횟수는 몇 회인가요?"
+import racingcar.Model.Car
 
 class OutputView {
     fun enterNameOfCars() {
@@ -10,5 +9,21 @@ class OutputView {
 
     fun enterTryCounts() {
         println(ENTER_TRY_COUNTS)
+    }
+
+    fun printCurrentRound(cars: List<Car>) {
+        for (car in cars) {
+            print("${car.name} : ")
+            repeat(car.position) {
+                print(ONE_STEP)
+            }
+            println()
+        }
+    }
+
+    companion object {
+        const val ENTER_NAME_OF_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val ENTER_TRY_COUNTS = "시도할 횟수는 몇 회인가요?"
+        const val ONE_STEP = "-"
     }
 }
