0909df1292cc14690ca6f0f9d1c4d3b7daf8a4f6
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 23:49:37 +0900

    test(ApplicationTest): Create test

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -4,11 +4,13 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
+    @DisplayName("우승자가 한 명일 경우")
     fun `기능 테스트`() {
         assertRandomNumberInRangeTest(
             {
@@ -20,9 +22,38 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
+    @DisplayName("우승자가 여러 명일 경우")
+    fun returnMultipleWinnersWhenSameScore() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni")
+            },
+            MOVING_FORWARD, MOVING_FORWARD
+        )
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 입력되어야 한다.")
+    fun throwExceptionWhenCarNameIsEmpty() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("", "1") }
+        }
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 6자 이상이면 예외가 발생해야 한다.")
     fun `예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
+    @Test
+    @DisplayName("레이싱 횟수는 숫자만 가능하다.")
+    fun throwExceptionWhenRoundIsNotNumber() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "a") }
         }
     }
 
