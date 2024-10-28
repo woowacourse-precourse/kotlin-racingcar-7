3c1c610b4d55e389591a84b1d18fd43531826802
jooiss <jusong513@pusan.ac.kr>
2024-10-28 22:08:18 +0900

    feat: 자동차 전진 조건 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -33,7 +33,17 @@ fun getTrialNumber(): Int {
 class Car(val name: String) {
     var position = 0
 
+    fun move() {
+        if (checkForwardCondition(getRandomNumber())) {
+            position++
+        }
+    }
+
     private fun getRandomNumber(): Int {
         return Random.nextInt(0, 10)
+    }
+
+    private fun checkForwardCondition(randomNumber: Int): Boolean {
+        return randomNumber >= 4
     }
 }
