3d45adf5baa3a2ddfa19a8050c1fe69ef0627dbd
Heejun-0219 <heejun0219@gmail.com>
2024-10-28 11:15:22 +0900

    chore: 추가 라이브러리 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,6 @@ repositories {
 
 dependencies {
     implementation("com.github.woowacourse-projects:mission-utils:1.2.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
-    testImplementation("org.mockito:mockito-core:5.12.0")
-    testImplementation("org.mockito:mockito-all:1.10.19")
 }
 
 tasks {
