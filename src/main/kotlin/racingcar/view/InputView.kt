9d419aaf6a597a5be3ce5941253b4e6e46d7d5ad
Jieun <lje2000lje@naver.com>
2024-10-28 19:08:49 +0900

    feat(InputView.kt): 각 자동차 이름 길이 검사 함수 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
+import java.util.prefs.Preferences.MAX_NAME_LENGTH
 
 class InputView {
 
@@ -28,10 +29,17 @@ class InputView {
         return input.split(COMMA).map { it.trim() }
     }
 
+    private fun validateNameLength(names: List<String>) {
+        require(names.all { it.isNotBlank() && it.length <= MAX_NAME_LENGTH }) {
+            ERROR_NAME_LENGTH
+        }
+    }
+
     companion object {
         const val MESSAGE_ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         const val ERROR_EMPTY_INPUT = "자동차 이름을 입력해야 합니다."
         const val ERROR_INVALID_SEPARATOR = "자동차 이름은 쉼표(,)로 구분해야 합니다."
+        const val ERROR_NAME_LENGTH = "자동차 이름은 1자 이상 5자 이하여야 합니다."
         const val COMMA = ","
     }
 }
