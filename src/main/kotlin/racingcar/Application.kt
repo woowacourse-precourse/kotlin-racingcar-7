2ff21778fab97ae28fcf4ff6f77f33c181bd135d
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-10-28 15:50:58 +0900

    feat: 잘못된 입력값에 대해 IllegalArgumentException 발생 기능
    - 자동차 각각의 이름은 5자 이하

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -9,6 +9,7 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     // TODO : trim() ? 으로 공백 제거? // 커밋 body에 올렸다 PR 설명에 적기
     for (carName in readLine().split(",")) {
+        require(carName.length <= 5) { "자동차 이름은 5자 이하만 가능하다." }  // TODO: 조건식 변수화
         carMap.put(carName, 0)
     }
 
