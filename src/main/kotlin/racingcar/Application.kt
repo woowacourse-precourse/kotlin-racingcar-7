1ac07d2d1a2927b2e0da9bdab9ae3cf5f4be4634
eeazzy <khs2498@gmail.com>
2024-10-28 11:58:42 +0900

    feat: 이동 횟수 입력 기능 추가
    사용자가 시도할 이동 횟수를 입력받아 int로 변환하여 반환

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,11 +2,19 @@ package racingcar
 
 fun main() {
     val carNames = getCarNames()
+    val moveCount = getMoveCount()
     println("입력된 자동차 이름: $carNames")
+    println("입력된 이동 횟수: $moveCount")
 }
 fun getCarNames(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val input = readLine() ?: ""
     val names = input.split(",").map { it.trim() } // 이름을 쉼표로 구분하고, 양쪽 공백 제거
     return names
+}
+
+fun getMoveCount(): Int {
+    println("시도할 횟수를 입력하세요.")
+    val input = readLine() ?: "0"
+    return input.toInt()
 }
