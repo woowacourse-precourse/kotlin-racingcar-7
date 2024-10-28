2745c1ae0e59902bf8dc965297bf83d09bd3101c
이지윤 <02leejiyun@naver.com>
2024-10-28 21:46:45 +0900

    feat: 자동차 이름이 중복된 경우 예외 처리

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -17,15 +17,19 @@ object InputView {
         return gameRound
     }
 
-    private fun validateCarNames(carNames: List<String>) {
+    fun validateCarNames(carNames: List<String>) {
+        val carNamesSet = mutableSetOf<String>()
         for (name in carNames) {
             if (name.isBlank() || name.length > 5) {
                 throw IllegalArgumentException(ERROR_INVALID_CAR_NAME)
             }
+            if (!carNamesSet.add(name)) {
+                throw IllegalArgumentException(ERROR_DUPLICATE_CAR_NAME)
+            }
         }
     }
 
-    private fun validateGameRound(gameRound: Int) {
+    fun validateGameRound(gameRound: Int) {
         if (gameRound <= 0) {
             throw IllegalArgumentException(ERROR_INVALID_GAME_ROUND)
         }
@@ -34,6 +38,8 @@ object InputView {
     private const val INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private const val INPUT_GAME_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?"
     private const val ERROR_INVALID_CAR_NAME = "[ERROR] 자동차 이름은 5자 이하여야 합니다."
+    private const val ERROR_DUPLICATE_CAR_NAME = "[ERROR] 자동차 이름은 중복될 수 없습니다."
     private const val ERROR_INVALID_GAME_ROUND = "[ERROR] 0 이상의 정수값을 입력해야 합니다."
+
     private const val CAR_NAME_DELIMITER = ','
 }
