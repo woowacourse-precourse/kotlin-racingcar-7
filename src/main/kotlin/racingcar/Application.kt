efa2cb0abc414ac52b0adcce406988e066535174
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2024-10-28 20:58:23 +0900

    refactor(processCarNames()): 가독성을 위해 return type 명시

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -25,7 +25,7 @@ fun main() {
 fun promptForCarNames() = println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 fun promptForAttemptNumber() = println("시도할 횟수는 몇 회인가요?")
 
-fun processCarNames(input: String) = input.split(",")
+fun processCarNames(input: String): List<String> = input.split(",")
     .map { it.trim() }
 
 fun processAttemptNumber(input: String) = input.toInt()
