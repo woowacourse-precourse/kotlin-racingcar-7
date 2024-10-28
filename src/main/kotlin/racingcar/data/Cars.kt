f2537260540c933d15ff363d6487333c961dee96
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-28 14:49:40 +0900

    feat(Cars.kt): 잘못된 자동차들의 정보 입력시 예외 발생 기능 구현
    예외 발생 상황
    - 중복되는 자동차 이름이 있을 경우
    - 참가 자동차의 수가 2대 보다 적을 경우
    - 참가 자동차의 정보를 입력하지 않았을 경우

---
+++ src/main/kotlin/racingcar/data/Cars.kt
@@ -0,0 +1,19 @@
+package racingcar.data
+
+class Cars(
+    private val input: String
+) {
+    private val cars: List<Car> = input.toCars()
+
+    init {
+        require(input.isNotEmpty())
+        require(cars.map { it.name }.distinct().size == cars.size)
+        require(cars.size >= MINIMUM_CAR_COUNT)
+    }
+
+    companion object {
+        private const val MINIMUM_CAR_COUNT = 2
+        private const val DELIMITER = ","
+        private fun String.toCars() = this.split(DELIMITER).map { Car(it) }
+    }
+}
