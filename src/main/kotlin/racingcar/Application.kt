f4f5fa7983a71e31e2f297f52539159b96e8122c
hi2242 <doodukly99@gmail.com>
2024-10-28 18:15:04 +0900

    Style: 주석 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,8 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console.readLine
 
 fun main() {
+    // 운전자 이름 리스트와 시도 횟수를 입력받을 변수 선언
     val inputName: List<String>
     val movingNumber: Int
+    // 운전자 이름 리스트 입력받기
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     try {
         inputName = readLine().split(",").map { it.trim() }
@@ -14,8 +16,8 @@ fun main() {
         throw IllegalArgumentException("잘못된 값을 입력했습니다.")
     }
 
+    // 시도 횟수 입력받기
     println("시도할 횟수는 몇 회인가요?")
-
     try {
         movingNumber = readLine().toInt()
         require(movingNumber > 0) { "시도 횟수는 1 이상이어야 합니다." }
@@ -23,12 +25,14 @@ fun main() {
         throw IllegalArgumentException("잘못된 값을 입력했습니다.")
     }
 
-    // 주어진 횟수 n대의 자동차는 전진 또는 멈춤
+    // 자동차 대수 세기
     val carCount = inputName.count()
-
+    // 현재 자동차의 위치를 저장
     val location: MutableList<String?> = MutableList(carCount) {""}
+
+    // 실행 부분
     println("실행 결과")
-    // 조건문으로 0 ~ 9 사이의 무작위 값을 구해서 4이상인 경우 전진 나머진 멈춤
+    // 각 자동차의 위치를 move를 통해 저장하고 출력
     for (i in 0 until movingNumber) {
         move(inputName, location)
         for (j in 0 until carCount) {
@@ -36,10 +40,12 @@ fun main() {
         }
         println()
     }
+    // 최종 우승자를 출력
     val winners = win(inputName, location)
     println("최종 우승자 : ${winners.joinToString(", ")}")
 }
 
+// 랜덤 뽑기로 자동차의 전진과 멈춤을 저장하고 반환
 fun move(inputName: List<String>, location: MutableList<String?>): MutableList<String?> {
     val carCount = inputName.count()
     for (i in 0 until carCount) {
@@ -52,6 +58,7 @@ fun move(inputName: List<String>, location: MutableList<String?>): MutableList<S
     return location
 }
 
+// 최종 우승자를 리스트의 max 값으로 비교하여 반환
 fun win(inputName: List<String>, location: MutableList<String?>): List<String> {
     val carCount = inputName.count()
     val winners = mutableListOf<String>()
