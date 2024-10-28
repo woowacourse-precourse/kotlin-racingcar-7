86914503feb57edd9874e05f3d41ed2c55af4aae
조형석 <chs9902@naver.com>
2024-10-28 21:42:26 +0900

    :white_check_mark: test: Input 클래스 테스트 추가

--- src/test/kotlin/racingcar/view/InputTest.kt
+++ src/test/kotlin/racingcar/view/InputTest.kt
@@ -1,16 +1,27 @@
 package racingcar.view
 
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.properties.Delegates
 
-import org.junit.jupiter.api.Assertions.*
-
-class InputTest {
+class InputTest :NsTest(){
+    private lateinit var result: List<String>
+    private var result2 by Delegates.notNull<Int>()
 
     @Test
-    fun getCars() {
+    fun `자동차 이름을 입력 받아 리스트로 반환한다`() {
+        run("Car1,Car2,Car3","3")
+        assertEquals(listOf("Car1", "Car2", "Car3"), result)
     }
-
     @Test
     fun getCount() {
+        run("Car1,Car2,Car3","3")
+        assertEquals(3, result2)
+    }
+
+    override fun runMain() {
+        result = Input().getCars()
+        result2 = Input().getCount()
     }
 }
