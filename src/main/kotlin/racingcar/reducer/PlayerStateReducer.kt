f609305d7d9a070870701f077b06caf425d5e3d5
chanho0908 <uni070@naver.com>
2024-10-28 20:38:01 +0900

    refactor(PlayerStateReducer.kt) : 이동 상태 반환 함수 추가

--- src/main/kotlin/racingcar/reducer/PlayerStateReducer.kt
+++ src/main/kotlin/racingcar/reducer/PlayerStateReducer.kt
@@ -1,13 +1,22 @@
 package racingcar.reducer
 
+import racingcar.constans.Constants.MIN_MOVE_COUNT
 import racingcar.model.MoveState
 import racingcar.model.CarRacingState.PlayerState
 
 class PlayerStateReducer : Reducer<Int, PlayerState> {
     override fun invoke(mutation: Int, state: PlayerState): PlayerState {
-        return when (MoveState.fromMoveCount(mutation)) {
+        return when (getMoveState(mutation)) {
             is MoveState.Movable -> state.copy(position = state.position + mutation)
             is MoveState.Immovable -> state
+        }
+    }
+
+    private fun getMoveState(moveCount: Int): MoveState {
+        return if (moveCount >= MIN_MOVE_COUNT) {
+            MoveState.Movable(moveCount)
+        } else {
+            MoveState.Immovable
         }
     }
 }
