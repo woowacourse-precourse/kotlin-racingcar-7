5bb74eee734e3e8b289ba1f47f7fdaa59d81fbb9
eeazzy <khs2498@gmail.com>
2024-10-28 11:53:03 +0900

    feat: 자동차 이름 입력 함수 추가
    쉼표로 구분된 자동차 이름을 입력받고 리스트로 반환하는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,12 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+    val carNames = getCarNames()
+    println("입력된 자동차 이름: $carNames")
+}
+fun getCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = readLine() ?: ""
+    val names = input.split(",").map { it.trim() } // 이름을 쉼표로 구분하고, 양쪽 공백 제거
+    return names
 }
