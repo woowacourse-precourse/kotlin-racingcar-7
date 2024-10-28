177652d68e6efa3c7ad739cdacc572a1b49c593b
SANDY <nnal0256@naver.com>
2024-10-28 17:14:09 +0900

    feat: 실행할 게임 횟수 입력 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,8 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // TODO: 프로그램 구현
     val carInput = getInputCarList()
-    val racingCar = RacingCar(carInput)
-
+    val gameCount = getInputGameCount()
 }
 
 private const val INPUT_PLAY_CAR_STR = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
@@ -15,4 +14,16 @@ fun getInputCarList(): String {
     val input = Console.readLine()
     Console.close()
     return input
+}
+
+private const val INPUT_GAME_COUNT_STR = "시도할 횟수는 몇 회인가요?"
+fun getInputGameCount(): Int {
+    println(INPUT_PLAY_CAR_STR)
+    val input = Console.readLine()
+    Console.close()
+    return input.toGameCount()
+}
+
+private fun String.toGameCount(): Int {
+    return trim().toInt()
 }
