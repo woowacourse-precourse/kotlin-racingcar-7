97a99e9869e2d7a8d375db8904044980ac6648f5
junwoo <skwon_5810@naver.com>
2024-10-28 15:54:50 +0900

    refactor(car): 클래스에서 순위 속성 제거

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,93 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+// 자동차를 나타내는 클래스, 이름과 이동 거리, 순위를 속성으로 가짐
+data class Car(
+    var name: String,
+    var move: Int = 0,
+)
+
 fun main() {
-    val names = readCarNames()
-    val tryNumber = readTryNumber()
+    val carNames = getCarNames()
+    val moveCount = getMoveCount()
+    val cars = createCars(carNames)
 
-    println("실행 결과")
+    repeat(moveCount) {
+        moveCars(cars)
+        printCarsStatus(cars)
+    }
 
-    winner(names, tryNumber)
+    val winners = winners(cars)
+    println("최종 우승자 : ${winners.joinToString(", ")}")
+}
+
+// 사용자가 입력한 자동차 이름을 ','로 구분하여 리스트로 반환
+fun getCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+    val carNameInput = Console.readLine() ?: ""
+
+    return carNameInput.split(",").map { it.trim() }
+}
+
+// 자동차 이름 리스트로 Car 객체 리스트를 생성
+fun createCars(carNames: List<String>): List<Car> {
+    return carNames.map { name ->
+        validateCarName(name)
+        Car(name = name)
+    }
+}
+
+// 자동차 이름 길이 검사
+fun validateCarName(name: String) {
+    if (name.length > 5) {
+        throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: $name")
+    }
+}
+
+// 이동 횟수를 입력받아 반환
+fun getMoveCount(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    val input = Console.readLine()
+    return validateMoveCount(input)
+}
+
+// 이동횟수 검증하고 숫자로 반환
+fun validateMoveCount(input: String?): Int {
+    // 입력값이 숫자인지 확인
+    if (input.isNullOrBlank() || !input.all { it.isDigit() }) {
+        throw IllegalArgumentException("숫자만 입력가능 합니다.")
+    }
+    return input.toInt()
+}
+
+// 랜덤값에 따라 자동차 전진여부 결정
+fun randomMove(): Boolean {
+    return Randoms.pickNumberInRange(0, 9) >= 4
+}
+
+// 자동차를 전진 시키는 로직
+fun moveCars(cars: List<Car>) {
+    cars.forEach { car ->
+        moveCarIfNeeded(car)
+    }
+}
+
+fun moveCarIfNeeded(car: Car) {
+    if (randomMove()) {
+        car.move++
+    }
+}
+
+fun printCarsStatus(cars: List<Car>) {
+    println()
+    cars.forEach { car ->
+        println("${car.name} : ${"-".repeat(car.move)}")
+    }
+}
+
+fun winners(cars: List<Car>): List<String> {
+    val maxDistance = cars.maxOf { it.move }
+    return cars.filter { it.move == maxDistance }.map { it.name }
 }
