4a8badba97806c14abbd3fbb9ca44b77a6cde771
souk0712 <souk0712@gmail.com>
2024-10-28 22:37:58 +0900

    feat: Car 클래스에 이동 로직 추가
    - 무작위 숫자를 반환하는 함수
    - 이동시, 진행상태를 업데이트하는 함수
    - 조건에 맞춰 이동하는 함수

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(val name: String) {
 
     private var _goState: String = ""
@@ -13,8 +15,23 @@ class Car(val name: String) {
         require(name.length <= NAME_LIMIT) { WRONG_NAME_MESSAGE }
     }
 
+    fun move() {
+        val randomNumber = getRandomNumber()
+        if (randomNumber >= RANDOM_STANDARD) go()
+    }
+
+    private fun go() {
+        _goState += GO_STATE_SYMBOL
+    }
+
+    private fun getRandomNumber(): Int = Randoms.pickNumberInRange(RANDOMS_MIN_RANGE, RANDOMS_MAX_RANGE)
+
     companion object {
+        private const val RANDOM_STANDARD = 4
         private const val NAME_LIMIT = 5
+        private const val RANDOMS_MIN_RANGE = 0
+        private const val RANDOMS_MAX_RANGE = 9
+        private const val GO_STATE_SYMBOL = '-'
         private const val WRONG_NAME_MESSAGE = "자동차 이름은 5자 이하만 가능합니다."
     }
 }
