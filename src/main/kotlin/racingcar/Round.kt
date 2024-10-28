7ded8129d78ef07466245fd8197ad88ab975dfdb
eden <devfeijoa@gmail.com>
2024-10-28 17:18:47 +0900

    feat: 게임시작시에 먼저 실행되는 기능

--- src/main/kotlin/racingcar/Round.kt
+++ src/main/kotlin/racingcar/Round.kt
@@ -3,4 +3,11 @@ package racingcar
 class Round(carNames: List<String>) {
     private var cars: MutableList<Car> = carNames.map { Car(it) }.toMutableList()
 
+    fun start() {
+        cars.forEach {
+            it.moveForward()
+        }
+        println()
+    }
+    
 }
