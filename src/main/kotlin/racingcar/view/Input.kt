c020e319bb095bfae58ee9bdbe075b6d882f4d9f
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 18:55:28 +0900

    feat(Input): Create raceCount() function for user input
    - Implemented raceCount() to receive race count from the user

--- src/main/kotlin/racingcar/view/Input.kt
+++ src/main/kotlin/racingcar/view/Input.kt
@@ -3,6 +3,7 @@ package racingcar.view
 import camp.nextstep.edu.missionutils.Console
 
 class Input {
+    fun raceCount(): String = readLine()
     fun carNames(): String = readLine()
     private fun readLine(): String = Console.readLine()
 }
