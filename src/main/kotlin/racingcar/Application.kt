9d703e73e35a47c7e0fd09d4140cfffceb6671a1
yoon <cbj0010@naver.com>
2024-10-28 09:55:38 +0900

    refact : 함수 분리
    main에 들어가는 안쓰는 함수 정리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -10,42 +10,13 @@ fun main() {
     val player = Console.readLine()
     println("시도할 횟수는 몇 회인가요?")
     val playNum = Console.readLine()
+    val carList = Car(player)
+//    validateData(carList.carRun())
+
 }
-
-
-
-private fun validateData() {
-    //검증 완료된 이름 값
-    val name = ""
-    showGameProgress(name, randomValue())
-}
-
 
 private fun showGameProgress(playerName: String, randomNum: Int) {
     //todo : 랜덤 수 만큼 전진하여야 한다.
-    display(playerName, randomNum)
+    println("showGameProgress : ${playerName} ${randomNum}")
 
 }
-
-private fun display(name: String, distance: Int): String {
-    val GO = "_"
-    return "$name : ${GO.repeat(distance)}"
-}
-
-
-private fun randomValue(): Int {
-    var validRandomNum = 0
-
-    //만약 true 이면 나온 횟수만큼 전진, 아니면 전진하지 않는다.
-    val randomNum = Randoms.pickNumberInRange(0, 9)
-    if (isRandomNumberValid(randomNum)) {
-        validRandomNum = randomNum
-    }
-    return validRandomNum
-}
-
-private fun isRandomNumberValid(randomNum: Int): Boolean {
-// 뽑는 숫자가 4 이상인 경우에만 전진을 할 수 있다.
-    return randomNum in 4..9
-}
-
