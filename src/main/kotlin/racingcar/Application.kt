eca83fb3051df937f0bc45930bb02a82320228ba
eeazzy <khs2498@gmail.com>
2024-10-28 12:23:13 +0900

    feat: 예외처리 기능
    자동차 이름에 공백, 5자 초과시 예외처리 발생
    이동 횟수 0이하 일 때 예외처리 발생

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -12,13 +12,26 @@ fun getCarNames(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val input = readLine() ?: ""
     val names = input.split(",").map { it.trim() } // 이름을 쉼표로 구분하고, 양쪽 공백 제거
+
+    // 자동차 이름 유효성 검사
+    if (names.any { it.isEmpty() || it.length > 5 }) {
+        throw IllegalArgumentException() // 빈 이름이나 5자 초과인 경우 예외 발생
+    }
+
     return names
 }
 
 fun getMoveCount(): Int {
     println("시도할 횟수를 입력하세요.")
     val input = readLine() ?: "0"
-    return input.toInt()
+    val moveCount = input.toInt()
+
+    // 이동 횟수 유효성 검사
+    if (moveCount <= 0) {
+        throw IllegalArgumentException() // 0 이하의 값인 경우 예외 발생
+    }
+
+    return moveCount
 }
 
 fun raceCars(carNames: List<String>, moveCount: Int) {
