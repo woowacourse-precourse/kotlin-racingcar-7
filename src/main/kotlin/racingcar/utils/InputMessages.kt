4db628b8197039f771eeb9421c4f02ee36189202
Jinwoo Choi <cjw1234577@gmail.com>
2024-10-28 18:07:02 +0900

    refactor(View): InputMessages 상수로 정의

---
+++ src/main/kotlin/racingcar/utils/InputMessages.kt
@@ -0,0 +1,6 @@
+package racingcar.utils
+
+object InputMessages {
+    const val CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val NUMBER_OF_ROUNDS_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+}
