40caa0138b5f418d49e8ee4415b3077be6c5704b
eden <devfeijoa@gmail.com>
2024-10-28 20:28:48 +0900

    style: 코드 포매팅

--- src/main/kotlin/racingcar/Validation.kt
+++ src/main/kotlin/racingcar/Validation.kt
@@ -1,7 +1,5 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Console
-
 class Validation {
 
     fun validateCarNames(carNames: List<String>): List<String> {
@@ -9,6 +7,7 @@ class Validation {
             forEach { validateCarName(it) }
         }
     }
+
     private fun validateCarName(name: String) {
         require(name.isNotBlank()) { "자동차 이름을 입력해주세요." }
         require(name.length <= 5) { "이름은 5자 이하로 입력해주세요." }
--- src/test/kotlin/racingcar/RacingCarTest.kt
+++ src/test/kotlin/racingcar/RacingCarTest.kt
@@ -1,9 +1,7 @@
 package racingcar
 
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class RacingCarTest {
     @Test
