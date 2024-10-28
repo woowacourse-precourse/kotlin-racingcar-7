d1de4e24276527a12053322b53d293d2d84c06d1
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 20:21:23 +0900

    refactor(Input): Rename function raceCount() to raceRound()
    - Changed function name to better reflect the purpose of counting rounds in each race
    - Aligned with InputController's getRaceRound() method for consistency

--- src/main/kotlin/racingcar/view/Input.kt
+++ src/main/kotlin/racingcar/view/Input.kt
@@ -3,7 +3,8 @@ package racingcar.view
 import camp.nextstep.edu.missionutils.Console
 
 class Input {
-    fun raceCount(): String = readLine()
+    fun raceRound(): String = readLine()
     fun carNames(): String = readLine()
+
     private fun readLine(): String = Console.readLine()
 }
