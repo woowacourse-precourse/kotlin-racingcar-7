56a552f13fee93b0775b18a8b500bee5ade0e090
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-10-28 21:47:32 +0900

    refactor(Car): 이동에 필요한 최소값 상수 이름 수정
    - MOVE_NUMBER_LIMIT -> MIN_MOVE_NUMBER로 변경
    
    이동하는 최소 숫자가 상수 이름에 더 적합하다고 생각해서 수정

--- src/main/kotlin/racingcar/domain/entity/Car.kt
+++ src/main/kotlin/racingcar/domain/entity/Car.kt
@@ -12,7 +12,7 @@ data class Car(
 
     fun tryMove(tryNumber: Int) {
         validateTryNumberRange(tryNumber)
-        if (tryNumber >= MOVE_NUMBER_LIMIT) _position++
+        if (tryNumber >= MIN_MOVE_NUMBER) _position++
     }
 
     private fun validateTryNumberRange(tryNumber: Int) {
@@ -25,7 +25,7 @@ data class Car(
 
     companion object {
         private const val INIT_POSITION = 0
-        private const val MOVE_NUMBER_LIMIT = 4
+        private const val MIN_MOVE_NUMBER = 4
         private const val MAX_TRY_NUMBER = 9
         private const val MIN_TRY_NUMBER = 0
         private const val INVALID_MIN_TRY_NUMBER_ERROR = "이동에 사용하는 숫자는 0이상 숫자입니다."
