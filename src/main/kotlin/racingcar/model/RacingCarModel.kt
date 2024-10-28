256e27d5d439b6aade739cc01e9ca1f28813070f
Jordan <leesoyoung0112@gmail.com>
2024-10-28 12:53:29 +0900

    feat(RacingCarModel.kt) : add getRaceResult, repeatRace

--- src/main/kotlin/racingcar/model/RacingCarModel.kt
+++ src/main/kotlin/racingcar/model/RacingCarModel.kt
@@ -1,7 +1,25 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class RacingCarModel {
-    fun seperateCarNames(input: String) : List<String> {
-        return input.split(",").map { it }
+
+    private lateinit var carList : List<String>
+    private lateinit var carFowards : MutableList<String>
+
+    fun seperateCarNames(input: String) {
+        carList = input.split(",").map { it }
+        carFowards = MutableList(carList.size) {""}
+    }
+
+    fun getRaceResult() : Boolean {
+        return Randoms.pickNumberInRange(0, 9) >= 4
+    }
+
+    fun repeatRace() {
+        carList.indices.forEach{ index ->
+            if (getRaceResult())
+                carFowards[index] += "-"
+        }
     }
 }
