0af52063e6e89798accd5875d07e64f63a8c6a7c
eeazzy <khs2498@gmail.com>
2024-10-28 12:08:07 +0900

    feat: 자동차 전진 조건 설정
    0에서 9 사이의 무작위 값을 생성하고, 4 이상일 경우 자동차를 전진시킴
    전진 결과를 출력하는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,12 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     val carNames = getCarNames()
     val moveCount = getMoveCount()
-    println("입력된 자동차 이름: $carNames")
-    println("입력된 이동 횟수: $moveCount")
+    println("\n실행결과")
+    raceCars(carNames, moveCount) // 전진 작동
 }
 fun getCarNames(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
@@ -17,4 +19,31 @@ fun getMoveCount(): Int {
     println("시도할 횟수를 입력하세요.")
     val input = readLine() ?: "0"
     return input.toInt()
+}
+
+fun raceCars(carNames: List<String>, moveCount: Int) {
+    val results = mutableMapOf<String, Int>()
+
+    // 초깃값
+    carNames.forEach { name ->
+        results[name] = 0
+    }
+
+    // 각 이동 횟수마다 결과를 출력
+    repeat(moveCount) { moveIndex ->
+        for (name in carNames) {
+            val randomValue = Randoms.pickNumberInRange(0, 9)
+            if (randomValue >= 4) {
+                results[name] = results[name]!! + 1 // 전진
+            }
+        }
+        displayResults(results, moveIndex + 1) // 이동 결과 출력
+    }
+}
+
+fun displayResults(results: Map<String, Int>, moveIndex: Int) {
+    results.forEach { (name, distance) ->
+        println("$name : ${"-".repeat(distance)}")
+    }
+    println() // 결과 간격 추가
 }
