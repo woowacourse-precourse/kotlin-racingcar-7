a9860205680365108b0dc152ae4092dca53bf1ef
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-10-28 15:51:02 +0900

    feat(Race.kt): 라운드 별 자동차 이름과 거리 출력 기능 구현

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -5,6 +5,7 @@ import racingcar.data.Cars.Companion.toList
 import racingcar.data.Round.Companion.toInt
 import racingcar.ui.InputView
 import racingcar.ui.OutputView
+import racingcar.utils.Random
 
 class Race(
     private val inputView: InputView,
@@ -13,6 +14,7 @@ class Race(
     fun start() {
         val cars = buildCars()
         val round = buildRound()
+        repeatRounds(cars, round)
     }
 
     private fun buildCars(): List<Car> {
@@ -23,5 +25,20 @@ class Race(
     private fun buildRound(): Int {
         outputView.printRoundCountInput()
         return inputView.parseRound().toInt()
+    }
+
+    private fun repeatRounds(cars: List<Car>, round: Int) {
+        outputView.printNewLine()
+        outputView.printResultTitle()
+        repeat(round) { startRound(cars) }
+    }
+
+    private fun startRound(cars: List<Car>) = showRoundResult(cars)
+
+    private fun showRoundResult(cars: List<Car>) {
+        for (car in cars) {
+            outputView.printRoundResult(car.name, car.move(Random.pick()))
+        }
+        outputView.printNewLine()
     }
 }
--- src/main/kotlin/racingcar/ui/OutputView.kt
+++ src/main/kotlin/racingcar/ui/OutputView.kt
@@ -3,8 +3,18 @@ package racingcar.ui
 object OutputView {
     private const val CARS_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     private const val ROUND_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+    private const val RESULT_TITLE_MESSAGE = "실행 결과"
+    private const val RESULT_DELIMITER = " : "
+    private const val DISTANCE_SYMBOL = "-"
 
     fun printCarsNameInput() = println(CARS_NAME_INPUT_MESSAGE)
 
     fun printRoundCountInput() = println(ROUND_COUNT_INPUT_MESSAGE)
+
+    fun printNewLine() = println()
+
+    fun printResultTitle() = println(RESULT_TITLE_MESSAGE)
+
+    fun printRoundResult(name: String, distance: Int) =
+        println("$name$RESULT_DELIMITER${DISTANCE_SYMBOL.repeat(distance)}")
 }
