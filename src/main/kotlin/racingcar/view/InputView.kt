ba421793d7a718c6967fd26b3680916c11d9d62d
Jieun <lje2000lje@naver.com>
2024-10-28 19:02:35 +0900

    feat(InputView.kt): 자동차 이름 입력 시 구분자 검사 함수 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -8,14 +8,24 @@ class InputView {
         println(MESSAGE_ENTER_CAR_NAMES)
         val input = Console.readLine().orEmpty()
         validateEmptyInput(input)
+        validateSeparator(input)
     }
 
     private fun validateEmptyInput(input: String) {
         if (input.isBlank()) throw IllegalArgumentException(ERROR_EMPTY_INPUT)
     }
 
+    private fun validateSeparator(input: String) {
+        val names = input.split(COMMA).map { it.trim() }
+        require(names.isNotEmpty() && names.all { it.isNotBlank() }) {
+            ERROR_INVALID_SEPARATOR
+        }
+    }
+
     companion object {
         const val MESSAGE_ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         const val ERROR_EMPTY_INPUT = "자동차 이름을 입력해야 합니다."
+        const val ERROR_INVALID_SEPARATOR = "자동차 이름은 쉼표(,)로 구분해야 합니다."
+        const val COMMA = ","
     }
 }
