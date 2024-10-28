57f0f305df69c9381bc41cf8d9c104fa8e1d4145
Jordan <leesoyoung0112@gmail.com>
2024-10-28 15:35:03 +0900

    fix(RacingCarView.kt) : fix wrong attempt input exception

--- src/main/kotlin/racingcar/view/RacingCarView.kt
+++ src/main/kotlin/racingcar/view/RacingCarView.kt
@@ -1,26 +1,34 @@
 package racingcar.view
+
 import camp.nextstep.edu.missionutils.Console
+
 class RacingCarView {
 
-    val getCarNameStr = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    val getAttemptsStr = "시도할 횟수는 몇 회인가요?"
-    val showResults = "최종 우승자 :"
-    fun getCarName() : String {
+    private val getCarNameStr = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    private val getAttemptsStr = "시도할 횟수는 몇 회인가요?"
+    private val showResults = "최종 우승자 :"
+    private val naturalNumException = "횟수는 자연수 숫자를 입력해야 합니다"
+    fun getCarName(): String {
         println(getCarNameStr)
         return Console.readLine()
     }
 
-    fun getAttempts() : Int {
+    fun getAttempts(): Int {
         println(getAttemptsStr)
-        return Console.readLine().toInt()
+        return isNaturalNumber(Console.readLine())
     }
 
-    fun displayAttemptResult(resultAttempts : String) {
+    fun displayAttemptResult(resultAttempts: String) {
         print(resultAttempts)
     }
 
-    fun displayResult(result : String) {
+    fun displayResult(result: String) {
         print("$showResults $result")
+    }
+
+    //Exception
+    private fun isNaturalNumber(it: String): Int{
+        return it.toIntOrNull() ?: throw IllegalArgumentException("$naturalNumException: $it")
     }
 
 }
