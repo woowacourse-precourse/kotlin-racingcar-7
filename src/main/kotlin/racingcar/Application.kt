bd0fd74fd2bec6c97a152bb9c8bb8d39a5ddb3af
Soobin-Park <jrkd2165@gmail.com>
2024-10-28 15:41:12 +0900

    feat: 자동차 이름 입력받는 기능 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.*;
+
 fun main() {
-    // TODO: 프로그램 구현
+    inputCars()
+
+}
+
+fun inputCars(){
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val cars = Console.readLine().split(",").map{it.trim()}
+    if (cars.any { it.isBlank() || it.length > 5 }) {
+        throw IllegalArgumentException("자동차 이름은 1자 이상 5자 이하로 입력해주세요.")
+    }
+
 }
