800367e24231882ae9a87e1f939aad42f939ad45
mcdmaisd <tkim0156@gmail.com>
2024-10-28 23:34:25 +0900

    feat: 플레이어 관련 기능 추가
    - 플레이어 생성 함수
    - 플레이어 이름 검증 함수

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,20 @@
 package racingcar
 
-fun main() {
-    // TODO: 프로그램 구현
+import camp.nextstep.edu.missionutils.Console.readLine
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
+data class Player(val name: String, var progress: Int = 0)
+
+fun createPlayers(input: String): List<Player> {
+    val names = input.split(",").map { it.trim() }
+    validateCarNames(names)
+    return names.map { name -> Player(name) }
+}
+
+fun validateCarNames(names: List<String>) {
+    for (name in names) {
+        if (name.isEmpty() || name.length > 5) {
+            throw IllegalArgumentException("1자 이상 5자 이하 이름만 가능합니다.")
+        }
+    }
 }
