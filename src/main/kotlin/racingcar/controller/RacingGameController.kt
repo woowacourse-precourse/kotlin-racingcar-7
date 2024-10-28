d68dd49ef4c71d73f355897dfa1991accdcd7a0d
Jieun <lje2000lje@naver.com>
2024-10-28 20:29:20 +0900

    feat(RacingGameController.kt): 입력받은 round 횟수만큼 게임 진행

--- src/main/kotlin/racingcar/controller/RacingGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingGameController.kt
@@ -1,11 +1,20 @@
 package racingcar.controller
 
+import racingcar.model.Game
 import racingcar.view.InputView
+import racingcar.view.ResultView
 
 class RacingGameController(
-    private val inputView: InputView = InputView()
+    private val inputView: InputView = InputView(),
+    private val resultView: ResultView = ResultView(),
+    private val racingGame: Game = Game(inputView.inputCarNames())
 ) {
     fun startGame() {
         val rounds = inputView.inputRounds()
+
+        repeat(rounds) {
+            racingGame.playRound()
+            resultView.printRoundResult(racingGame.getCars())
+        }
     }
 }
