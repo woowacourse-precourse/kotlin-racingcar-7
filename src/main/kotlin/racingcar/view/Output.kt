cdea6cfb52a43bf2240cceb5ba538ef1a4ec3b8c
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 20:46:00 +0900

    feat(Output): Add showRoundResult() function to show each round result

--- src/main/kotlin/racingcar/view/Output.kt
+++ src/main/kotlin/racingcar/view/Output.kt
@@ -1,6 +1,20 @@
 package racingcar.view
 
+import racingcar.model.Car
+
 class Output {
     fun infoGetCarNames() = println("경주할 자동차 이름을 입력하세요.")
     fun infoGetRound() = println("시도할 횟수는 몇 회인가요?")
+
+    fun showRoundResult(cars: List<Car>) {
+        println("실행 결과")
+        cars.forEach {
+            print("${it.name}: ")
+            repeat(it.moving) {
+                print("-")
+            }
+            println()
+        }
+        println()
+    }
 }
