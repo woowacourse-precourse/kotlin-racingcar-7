bbc81ac0e9d4660e1434a0f414e17acbf115e5e9
parkjiwon <ji11won@naver.com>
2024-10-28 17:45:06 +0900

    refactor : 출력값 상수화
    SENTENCE_INPUT_NAME, SENTENCE_INPUT_TIMES, SENTENCE_OUTPUT_RESULT 상수화

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -9,15 +9,15 @@ class Race(
     private var times: Int = 0
 
     fun ready() {
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        println(SENTENCE_INPUT_NAME)
         cars = examineName(Console.readLine())
-        println("시도할 횟수는 몇 회인가요?")
+        println(SENTENCE_INPUT_TIMES)
         times = examineTimes(Console.readLine())
         println()
     }
 
     fun start() {
-        println("실행 결과")
+        println(SENTENCE_OUTPUT_RESULT)
         repeat(times) {
             for (car in cars) {
                 car.move()
@@ -50,9 +50,14 @@ class Race(
     }
 
     private fun getWinner(): List<String> {
-        var max = cars.maxOf { it.getPosition() }
+        val max = cars.maxOf { it.getPosition() }
         return cars.filter { it.getPosition() == max }.map { it.getName() }
     }
 
-    companion object {}
+    companion object {
+        const val SENTENCE_INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val SENTENCE_INPUT_TIMES = "시도할 횟수는 몇 회인가요?"
+        const val SENTENCE_OUTPUT_RESULT = "실행 결과"
+
+    }
 }
