41bbde5c5038f5bac9dc9f3fa1775ec77531d357
yoon <cbj0010@naver.com>
2024-10-28 09:51:16 +0900

    feat : 차 공장 생성
    유효성 검증하고 그에 맞는 만큼 차를 전진시키기

---
+++ src/main/kotlin/racingcar/CarFactory.kt
@@ -0,0 +1,18 @@
+package racingcar
+
+class CarFactory() {
+    var maxDistance = 0
+
+    fun displayCarRace(car: List<Car>, tryGame: Int): Int {
+        repeat(tryGame) {
+            car.forEach { it.checkMove() }
+            val max = car.maxOfOrNull { it.distance }
+            if (max != null && max > maxDistance) {
+                maxDistance = max
+            }
+            car.forEach { println(it.display()) }
+            println()
+        }
+        return maxDistance
+    }
+}
