e90a6a69a05821b2df9d12a3c10b4e66be30dcb8
Jordan <leesoyoung0112@gmail.com>
2024-10-28 15:32:16 +0900

    refactor, fix(RacingCarModel.kt) : refactor code to align with programming requirements 2, fix length exception

--- src/main/kotlin/racingcar/model/RacingCarModel.kt
+++ src/main/kotlin/racingcar/model/RacingCarModel.kt
@@ -2,43 +2,42 @@ package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class RacingCarModel{
-    private lateinit var carList : List<String>
-    private lateinit var carFowards : MutableList<String>
+class RacingCarModel {
+    private lateinit var carList: List<String>
+    private lateinit var carForwards: MutableList<String>
+    private val lengthException = "자동차 이름은 0자 미만이거나 5자를 초과할 수 없습니다"
 
-    fun seperateCarNames(input: String) {
-        carList = input.split(",").map { it }
-        carFowards = MutableList(carList.size) {""}
+    private fun separateCarNames(input: String) {
+        carList = input.split(",").map { isLengthOverOrNull(it) }
+        carForwards = MutableList(carList.size) { "" }
     }
 
-    fun getRaceResult(): Boolean {
+    private fun getRaceResult(): Boolean {
         return Randoms.pickNumberInRange(0, 9) >= 4
     }
 
-    fun runRaceAttempt() : String {
-        var eachAttemptResult = ""
-        carList.indices.forEach{ index ->
-            if (getRaceResult())
-                carFowards[index] += "-"
-        }
-        for (i in carList.indices) {
-            eachAttemptResult += "${carList[i]} : ${carFowards[i]}\n"
-        }
-        return eachAttemptResult+"\n"
+    private fun runRaceAttempt(): String {
+        carForwards = carForwards.mapIndexed { _, forward -> if (getRaceResult()) "$forward-" else forward }.toMutableList()
+        return carList.indices.joinToString("\n") { "${carList[it]} : ${carForwards[it]}" } + "\n\n"
     }
-    fun findWinners(): String {
-        val max = carFowards.max().length
-        return carList.filterIndexed{index, _ -> carFowards[index].length == max}.joinToString(",")
+
+    private fun findWinners(): String {
+        val max = carForwards.max().length
+        return carList.filterIndexed { index, _ -> carForwards[index].length == max }.joinToString(",")
     }
 
     fun runRace(inputCarList: String, attempts: Int): Pair<String, String> {
         var attemptResult = ""
-        seperateCarNames(inputCarList)
-        repeat(attempts) {
-            attemptResult += runRaceAttempt()
-        }
+        separateCarNames(inputCarList)
+        repeat(attempts) { attemptResult += runRaceAttempt() }
         val winners = findWinners()
         return Pair(attemptResult, winners)
+    }
+
+    //Exception
+    private fun isLengthOverOrNull(name: String): String {
+        name.takeIf { it.length > 5 || it.isEmpty() }?.let { throw IllegalArgumentException("$lengthException: $name") }
+        return name
     }
 
 }
