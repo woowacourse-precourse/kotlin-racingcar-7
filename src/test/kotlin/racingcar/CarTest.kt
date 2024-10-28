c9c2dd86ba1bc3bd4d4514d1d14ef4705aa73dd9
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-10-28 17:50:36 +0900

    test: 자동차 객체 생성 위치 테스트 수정

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -8,12 +8,12 @@ import org.junit.jupiter.api.Test
 class CarTest {
 
     @Test
-    fun `자동차 이름을 넘겨줄 경우 전진 횟수가 0인 자동차 객체를 반환한다`() {
+    fun `자동차 이름을 넘겨줄 경우 초기 자동차 위치는 0이 된다`() {
         val carName = "loopy"
 
         val car = Car.from(carName)
 
-        assertEquals(Car("loopy"), car)
+        assertEquals(0, car.location)
     }
 
     @Test
