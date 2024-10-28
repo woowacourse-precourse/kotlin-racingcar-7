67c6b08ffcf8b3d553a2d2dca6fa5d21d0c679b9
Jieun <lje2000lje@naver.com>
2024-10-28 18:59:54 +0900

    feat(InputView.kt): 자동차 이름 빈값 유효성 검사 함수 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -7,9 +7,15 @@ class InputView {
     fun inputCarNames() {
         println(MESSAGE_ENTER_CAR_NAMES)
         val input = Console.readLine().orEmpty()
+        validateEmptyInput(input)
+    }
+
+    private fun validateEmptyInput(input: String) {
+        if (input.isBlank()) throw IllegalArgumentException(ERROR_EMPTY_INPUT)
     }
 
     companion object {
         const val MESSAGE_ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        const val ERROR_EMPTY_INPUT = "자동차 이름을 입력해야 합니다."
     }
 }
