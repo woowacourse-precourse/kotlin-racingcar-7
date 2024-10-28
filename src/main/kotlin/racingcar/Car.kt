d17c9d7a522bae8c668326d612d68a23ed576f08
eden <devfeijoa@gmail.com>
2024-10-28 18:14:55 +0900

    refactor: Result 클래스를 Car 클래스에 통합

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -3,36 +3,14 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 class Car(private val name: String) {
     private var position = 0
-    private var result: Result = Result()
 
-    fun getName(): String {
-        return name
-    }
-
-    fun getPosition(): Int {
-        return position
-    }
+    fun getName() = name
+    fun getPosition() = position
 
     fun moveForward() {
-        val randomNumber = Randoms.pickNumberInRange(0, 9)
-        if (randomNumber >= 4) {
-            position += 1
-            result.printResult()
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++
         }
     }
-    fun movingState(): String {
-        return "$name : ${result.getCurrentPosition()}"
-    }
-}
-
-class Result {
-    private var printPosition = ""
-
-    fun getCurrentPosition(): String {
-        return printPosition
-    }
-
-    fun printResult() {
-        printPosition += "-"
-    }
+    fun getStatus() = "$name : ${"-".repeat(position)}"
 }
