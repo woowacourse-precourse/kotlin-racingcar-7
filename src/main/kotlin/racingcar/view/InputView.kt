7d88d814229969774ab30873749707d44fabc85e
nuyhhyun <sarahs19991115@gmail.com>
2024-10-28 23:22:44 +0900

    feat($Exception): 예외 처리 기능 구현
    더 가시적인 예외 처리 위해 해당 오브젝트 별도 구현

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,17 +1,19 @@
 package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.Model.Exception
 
 class InputView {
     private val outputView = OutputView()
+    private val exception = Exception
 
-    fun getNameOfCars(): String {
+    fun getNameOfCars(): Any? {
         outputView.enterNameOfCars()
         val nameOfCars = Console.readLine()
         return if (isNameOfCarsValid(nameOfCars)) {
             nameOfCars
         } else {
-            throw IllegalArgumentException("Name of cars invalid")
+            exception.errorComesUpWith(INVAID_CAR_NAMES_INPUT)
         }
     }
 
@@ -24,17 +26,22 @@ class InputView {
             return false
     }
 
-    fun getTryCounts(): Int {
+    fun getTryCounts(): Any {
         outputView.enterTryCounts()
         val tryCounts = Console.readLine().toIntOrNull() ?: 0
         return if(isTryCountsValid(tryCounts)) {
             tryCounts
         } else {
-            throw IllegalArgumentException("Try count invalid")
+            exception.errorComesUpWith(INVAID_TRY_COUNTS_INPUT)
         }
     }
 
     private fun isTryCountsValid(tryCounts: Int): Boolean {
         return tryCounts > 0
+    }
+
+    companion object {
+        private const val INVAID_CAR_NAMES_INPUT = "두 대 이상의 자동차들을 쉼표(,)를 기준으로 구분하여 입력해주세요."
+        private const val INVAID_TRY_COUNTS_INPUT = "양의 정수인 시도 횟수를 입력해주세요."
     }
 }
