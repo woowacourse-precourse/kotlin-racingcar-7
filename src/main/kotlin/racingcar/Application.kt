54af12a81cb4a88d184fb8e0f42812cd7d8ac8b0
jooiss <jusong513@pusan.ac.kr>
2024-10-28 21:02:31 +0900

    feat: 자동차 이름 입력받기 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
+    val userCarName = getCarName()
+    val userCarList = parseCar(userCarName!!)
+}
+
+fun getCarName(): String? {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    return readlnOrNull()
+}
+
+fun parseCar(input: String): List<String> {
+    val userCarList: List<String> = input.split(",")
+    return userCarList
 }
