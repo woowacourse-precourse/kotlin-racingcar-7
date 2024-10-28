41e4ee4ad33d7702556f8d95c1659c4c4359d448
kimgwon <92065911+kimgwon@users.noreply.github.com>
2024-10-28 20:49:14 +0900

    feat(RaceController): Create start() function to initiate race and display results
    - Added start logic to prompt user input for cars and rounds
    - Implemented race with round results and final winner display

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -3,9 +3,11 @@ package racingcar.controller
 import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 import racingcar.model.Car
 import racingcar.model.Race
+import racingcar.view.Output
 
 class RaceController {
     private val inputController = InputController()
+    private val output = Output()
     private val race = Race()
     private val cars = race.cars
 
@@ -14,5 +16,19 @@ class RaceController {
     private fun getWinner(): List<Car> {
         val maxMoving = cars.maxOf { it.moving }
         return cars.filter { car -> car.moving == maxMoving }
+    }
+
+    fun start() {
+        output.infoGetCarNames()
+        makeCar()
+
+        output.infoGetRound()
+
+        repeat(inputController.getRaceRound()) {
+            moveCar()
+            output.showRoundResult(cars)
+        }
+
+        output.showRaceResult(getWinner())
     }
 }
