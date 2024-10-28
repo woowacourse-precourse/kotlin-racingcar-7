7c82a08b0476a1e808b775fc3a7ce27d0a36ff8f
ezhoon <david0275@naver.com>
2024-10-28 23:12:35 +0900

    feat(print): 경기 진행 결과 출력 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -13,6 +13,11 @@ fun main() {
     val cars = carNames.map { Car(it) }
     val game = CarRacingGame(cars, attempts)
 
+    game.play()
+
+    val winners = game.getWinners()
+
+    Output.printWinners(winners)
 }
 
 // # 입력 기능 추가
@@ -36,6 +41,7 @@ object Input {
 // # 자동차 경주 진행
 class CarRacingGame(private val cars: List<Car>, private val attempts: Int) {
     fun play() {
+        println("\n실행 결과")
         repeat(attempts) {
             cars.forEach { car ->
                 if (Randoms.pickNumberInRange(0, 9) >= 4) {
@@ -59,3 +65,13 @@ class CarRacingGame(private val cars: List<Car>, private val attempts: Int) {
     }
 }
 
+// # 출력 기능
+object Output {
+    fun printWinners(winners: List<String>) {
+        if (winners.size == 1) {
+            println("최종 우승자 : ${winners[0]}")
+        } else {
+            println("최종 우승자 : ${winners.joinToString(", ")}")
+        }
+    }
+}
