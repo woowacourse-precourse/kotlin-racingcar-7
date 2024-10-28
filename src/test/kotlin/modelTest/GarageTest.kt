5dd39f94e982075edc77c9d5b9be83acfd2e3a83
nuyhhyun <sarahs19991115@gmail.com>
2024-10-28 21:40:49 +0900

    refactor($GarageTest): 테스트 코드 형식 수정
    $ApplicationTest 테스트 코드 형식에 따라 테스트 코드 수정

--- src/test/kotlin/modelTest/GarageTest.kt
+++ src/test/kotlin/modelTest/GarageTest.kt
@@ -1,5 +1,6 @@
 package modelTest
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import racingcar.Model.Garage
@@ -7,14 +8,15 @@ import racingcar.Model.Garage
 class GarageTest {
     @Test
     fun `쉼표(,)를 기준으로 각 자동차 이름을 구분할 수 있다`() {
-        //given
-        val nameOfCars = "hyun, pobi, woni, jun"
+        assertSimpleTest {
+            //given
+            val nameOfCars = "hyun, pobi, woni, jun"
 
-        //when
-        val carsInGarage = Garage(nameOfCars)
+            //when
+            val carsInGarage = Garage(nameOfCars)
 
-        //then
-        assertThat(carsInGarage.carsInGarage.map { it.name }).isEqualTo(listOf("hyun", "pobi", "woni", "jun"))
-
+            //then
+            assertThat(carsInGarage.carsInGarage.map { it.name }).isEqualTo(listOf("hyun", "pobi", "woni", "jun"))
+        }
     }
 }
