53fb0971a389464df17b0b8f97ab82836582a4bc
Jieun <lje2000lje@naver.com>
2024-10-28 20:10:21 +0900

    feat(Game.kt): 랜덤값에 따라 전진을 결정하는 함수 구현
    랜덤값이 4 이상일 경우 car의 move 함수를 호출하여 거리 증가

--- src/main/kotlin/racingcar/model/Game.kt
+++ src/main/kotlin/racingcar/model/Game.kt
@@ -1,11 +1,25 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Game(carNames: List<String>) {
     private val cars = carNames.map { Car(it) }
 
     fun playRound() {
         cars.forEach { car ->
-
+            if (isMoveForward()) {
+                car.move()
+            }
         }
+    }
+
+    private fun isMoveForward(): Boolean {
+        return Randoms.pickNumberInRange(MIN_MOVE_THRESHOLD, MAX_MOVE_THRESHOLD) >= MOVE_THRESHOLD
+    }
+
+    companion object {
+        const val MOVE_THRESHOLD = 4
+        const val MIN_MOVE_THRESHOLD = 0
+        const val MAX_MOVE_THRESHOLD = 9
     }
 }
