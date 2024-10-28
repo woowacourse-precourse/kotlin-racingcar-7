375450ea7954347a3f71e8542e44e1f16099ef09
souk0712 <souk0712@gmail.com>
2024-10-28 22:41:26 +0900

    feat: Race 클래스에 유효성 검사 로직 추가
    - 시도 횟수의 제약사항 추가

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -1,3 +1,15 @@
 package racingcar
 
-class Race(private val tryCount: Int, private val cars: List<Car>)
+class Race(private val tryCount: Int, private val cars: List<Car>) {
+    init {
+        checkValidate()
+    }
+
+    private fun checkValidate() {
+        require(tryCount >= 0) { WRONG_TRY_COUNT_MESSAGE }
+    }
+
+    companion object {
+        private const val WRONG_TRY_COUNT_MESSAGE = "시도 횟수는 0이상이어야 합니다."
+    }
+}
